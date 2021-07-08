7b3340624b477243d963b175b505414eb58eb902
Fuxing Loh <git@fuxing.dev>
2021-07-09 02:37:53 +0800

    updated README.md and its health

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,7 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Head from 'next/head'
 
 export default function Home (): JSX.Element {
-  const [owner, setOwner] = useState('a1') // as hex
-  const [data, setData] = useState('b1') // as hex
-
   return (
     <div className='container mx-auto px-4 py-6'>
       <Head>
@@ -15,11 +12,8 @@ export default function Home (): JSX.Element {
 
       <main>
         <h1 className='text-2xl font-medium'>
-          DeFiChain MasterNode Voting
+          DeFiChain Explorer
         </h1>
-
-        <input type='text' value={owner} onChange={({ target: { value } }) => setOwner(value)} />
-        <input type='text' value={data} onChange={({ target: { value } }) => setData(value)} />
       </main>
 
       <footer />
