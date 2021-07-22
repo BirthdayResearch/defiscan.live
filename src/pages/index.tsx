46820581ca57bb0123fd9bf1bfa78d249e1b3699
Fuxing Loh <git@fuxing.dev>
2021-07-22 12:51:36 +0800

    added ts-standard rules into lint

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 export default function Home (): JSX.Element {
   return (
-    <div className={'container mx-auto px-4 py-6'}>
-      <h1 className={'text-2xl font-medium'}>
+    <div className='container mx-auto px-4 py-6'>
+      <h1 className='text-2xl font-medium'>
         DeFiChain Explorer
       </h1>
     </div>
