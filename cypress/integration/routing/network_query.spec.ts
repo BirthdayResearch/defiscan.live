b3ab0ed51c4383a345839aec33ebbcfbf4bd0060
Fuxing Loh <git@fuxing.dev>
2021-07-23 00:42:56 +0800

    added should('exist') for better test reliability

--- cypress/integration/routing/network_query.spec.ts
+++ cypress/integration/routing/network_query.spec.ts
@@ -10,7 +10,10 @@ describe('querystring network', () => {
   })
 
   it('should preserve network querystring when route to /prices', function () {
-    cy.get('footer').findAllByText('Prices').click()
+    cy.get('footer')
+      .findAllByText('Prices')
+      .should('exist')
+      .click()
 
     cy.location().should((loc) => {
       expect(loc.pathname).to.eq('/prices')
