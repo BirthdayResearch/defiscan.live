db02cc54876836bcf35955b43014bec08ea8e993
websterlcl <w.canonbrother@gmail.com>
2021-07-22 13:45:36 +0800

    added test

--- cypress/integration/pages/dex/index.ts
+++ cypress/integration/pages/dex/index.ts
@@ -5,12 +5,12 @@ context('dex', () => {
     cy.visit('/dex')
   })  
   
-  it('should contain pool pair list information', function () {
-    cy.get('table thead tr th:nth-child(1)').should('have.value', 'Name')
-    cy.get('table thead tr th:nth-child(2)').should('have.value', 'Total Liquidity USD')
-    cy.get('table thead tr th:nth-child(3)').should('have.value', 'Daily Volume USD (30d avg)')
-    cy.get('table thead tr th:nth-child(4)').should('have.value', 'Liquidity')
-    cy.get('table thead tr th:nth-child(5)').should('have.value', 'Price Ratio')
-    cy.get('table thead tr th:nth-child(6)').should('have.value', 'APR')
+  it('should list pool pair information', function () {
+    cy.get('div.table div.table-row-group:nth-child(1) div.table-row div.table-cell:nth-child(1)').should('have.text', 'Name')
+    cy.get('div.table div.table-row-group:nth-child(1) div.table-row div.table-cell:nth-child(2)').should('have.text', 'Total Liquidity USD')
+    cy.get('div.table div.table-row-group:nth-child(1) div.table-row div.table-cell:nth-child(3)').should('have.text', 'Daily Volume USD (30d avg)')
+    cy.get('div.table div.table-row-group:nth-child(1) div.table-row div.table-cell:nth-child(4)').should('have.text', 'Liquidity')
+    cy.get('div.table div.table-row-group:nth-child(1) div.table-row div.table-cell:nth-child(5)').should('have.text', 'Price Ratio')
+    cy.get('div.table div.table-row-group:nth-child(1) div.table-row div.table-cell:nth-child(6)').should('have.text', 'APR')
   })
 })
