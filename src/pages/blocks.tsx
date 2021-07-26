7ccf7f10cf753c5b22b9f7f79e16d8ce12116676
aikchun <iam@simaikchun.com>
2021-07-26 13:15:40 +0800

    Formatter changes from double quotes to single quotes.

--- src/pages/blocks.tsx
+++ src/pages/blocks.tsx
@@ -1,13 +1,12 @@
 export default function Blocks (): JSX.Element {
   return (
-    <div className="container mx-auto flex px-4 py-8">
-      <div className="grid w-screen md:w-2/3 grid-cols-4 gap-4">
+    <div className='container mx-auto flex px-4 py-8'>
+      <div className='grid w-screen md:w-2/3 grid-cols-4 gap-4'>
         <div>Height</div>
         <div>Age</div>
         <div>Transactions</div>
-        <div className="justify-self-end">Size</div>
+        <div className='justify-self-end'>Size</div>
       </div>
     </div>
   )
 }
-
