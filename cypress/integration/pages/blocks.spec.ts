a16e2985825a7bcd8b1f050e4766666d20a0e8a6
aikchun <iam@simaikchun.com>
2021-07-27 16:32:04 +0800

    Added cypress test spec file for blocks page.

--- cypress/integration/pages/blocks.spec.ts
+++ cypress/integration/pages/blocks.spec.ts
@@ -3,15 +3,10 @@ describe('blocks page', () => {
     cy.visit('/blocks')
   })
 
-  it('should have 6 column headers', function () {
-    cy.get('.table-row-group > .hidden > .table-cell').should('have.length', 5)
-  })
-
-  it('should contain the appropriate headers', function () {
-    cy.get('.table-row-group > .hidden > .table-cell:nth-child(1)').should('have.text', 'height')
-    cy.get('.table-row-group > .hidden > .table-cell:nth-child(2)').should('have.text', 'age')
-    cy.get('.table-row-group > .hidden > .table-cell:nth-child(3)').should('have.text', 'transactions')
-    cy.get('.table-row-group > .hidden > .table-cell:nth-child(4)').should('have.text', 'size')
-    cy.get('.table-row-group > .hidden > .table-cell:nth-child(5)').should('have.text', 'difficulty')
+  it('should contain block table header count', function () {
+    cy.get('.grid:nth-child(1) div:nth-child(1)').should('have.text', 'Height')
+    cy.get('.grid:nth-child(1) div:nth-child(2)').should('have.text', 'Age')
+    cy.get('.grid:nth-child(1) div:nth-child(3)').should('have.text', 'Transactions')
+    cy.get('.grid:nth-child(1) div:nth-child(4)').should('have.text', 'Size')
   })
 })
