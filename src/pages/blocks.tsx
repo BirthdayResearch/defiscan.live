3a08f360f353bf896d953d6834b684d3128ddec3
aikchun <iam@simaikchun.com>
2021-07-29 14:34:55 +0800

    Changed the table implementation to use flex instead of grid.

--- src/pages/blocks.tsx
+++ src/pages/blocks.tsx
@@ -1,7 +1,7 @@
-import { Fragment } from 'react'
 import { parseAge } from '../utils'
 
 export default function Blocks (): JSX.Element {
+  // dummy data
   const blocks = [
     { height: 100, time: parseAge(1627288589), transactions: ['', '', ''], size: 1 },
     { height: 99, time: parseAge(1627277303), transactions: ['', '', ''], size: 2 },
@@ -11,24 +11,24 @@ export default function Blocks (): JSX.Element {
 
   const renderBlocks = (): JSX.Element[] => {
     return blocks.map(block => (
-      <Fragment key={block.time}>
-        <div>{block.height}</div>
-        <div>{block.time}</div>
-        <div className='justify-self-end'>{block.transactions.length}</div>
-        <div className='justify-self-end'>{block.size}</div>
-      </Fragment>
+      <div className='blocks-table-row flex justify-between' key={block.time}>
+        <div className='flex-1'>{block.height}</div>
+        <div className='flex-1'>{block.time}</div>
+        <div className='flex-1 text-right'>{block.transactions.length}</div>
+        <div className='flex-1 text-right'>{block.size}</div>
+      </div>
     ))
   }
 
   return (
     <div className='container mx-auto px-4 py-8'>
-      <div className='grid w-screen md:w-2/3 grid-cols-4 gap-4'>
-        <div>Height</div>
-        <div>Age</div>
-        <div>Transactions</div>
-        <div className='justify-self-end'>Size</div>
+      <div className='blocks-table-header flex justify-between w-screen md:w-2/3 grid-cols-4 gap-4'>
+        <div className='flex-1'>Height</div>
+        <div className='flex-1'>Age</div>
+        <div className='flex-1 text-right'>Transactions</div>
+        <div className='flex-1 text-right'>Size</div>
       </div>
-      <div className='grid grid-flow-row w-screen md:w-2/3 grid-cols-4 gap-4'>
+      <div className='block-table-body flex flex-col w-screen md:w-2/3 grid-cols-4 gap-4'>
         {renderBlocks()}
       </div>
     </div>
