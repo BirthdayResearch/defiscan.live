afc25c838e418c3e265640f23ca5a79127f4a91e
aikchun <iam@simaikchun.com>
2021-07-29 16:24:09 +0800

    Refactored table header into a generic component and added a blocks transactions page.

--- cypress/integration/pages/blocks.spec.ts
+++ cypress/integration/pages/blocks.spec.ts
@@ -4,9 +4,9 @@ describe('blocks page', () => {
   })
 
   it('should contain block table header count', function () {
-    cy.get('.blocks-table-header div:nth-child(1)').should('have.text', 'Height')
-    cy.get('.blocks-table-header div:nth-child(2)').should('have.text', 'Age')
-    cy.get('.blocks-table-header div:nth-child(3)').should('have.text', 'Transactions')
-    cy.get('.blocks-table-header div:nth-child(4)').should('have.text', 'Size')
+    cy.get('.table-header div:nth-child(1)').should('have.text', 'Height')
+    cy.get('.table-header div:nth-child(2)').should('have.text', 'Age')
+    cy.get('.table-header div:nth-child(3)').should('have.text', 'Transactions')
+    cy.get('.table-header div:nth-child(4)').should('have.text', 'Size')
   })
 })
