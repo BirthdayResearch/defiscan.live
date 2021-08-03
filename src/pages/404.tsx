26f116fb54989478198c07c101c648bc20daed7e
Fuxing Loh <4266087+fuxingloh@users.noreply.github.com>
2021-08-04 01:28:37 +0800

    added 404 page (#105)

---
+++ src/pages/404.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+export default function NotFound (): JSX.Element {
+  return (
+    <>
+      <Head>
+        <title>Page Not Found - DeFi Scan</title>
+      </Head>
+
+      <div className='container mx-auto px-4 py-10'>
+        <h1 className='text-3xl font-semibold'>
+          404 - Page Not Found
+        </h1>
+      </div>
+    </>
+  )
+}
