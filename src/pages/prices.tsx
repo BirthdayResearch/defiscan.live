234a5603e76199d023cf6bf69adf60e9e9b5e9c9
Fuxing Loh <git@fuxing.dev>
2021-08-03 15:21:48 +0800

    Cypress test failure

--- src/pages/prices.tsx
+++ src/pages/prices.tsx
@@ -1,8 +1,8 @@
-export default function Home (): JSX.Element {
+export default function Prices (): JSX.Element {
   return (
     <div className='container mx-auto px-4 py-6'>
       <h1 className='text-2xl font-medium'>
-        DeFiChain Prices
+        Prices Feed
       </h1>
     </div>
   )
