28fd557215e89e30924ed61e76016af4f001128a
aikchun <iam@simaikchun.com>
2021-08-04 13:12:19 +0800

    Added whale api and fetchBlock calls using whale-api-client.

--- src/pages/blocks/index.tsx
+++ src/pages/blocks/index.tsx
@@ -1,26 +1,37 @@
+import { useEffect, useState } from 'react'
 import { parseAge } from '../../utils'
 import { TableHeader, TableBody } from '../../components/Table/index'
+import { useWhaleApiClient } from '@contexts'
 import Link from 'next/link'
 
-interface BlocksPageProps {
-  blocks: Block[]
-}
-
-interface Transaction {
+export interface Block {
   id: string
-}
+  hash: string
+  previousHash: string
 
-interface Block {
   height: number
-  id: string
-  time: string
-  transactions: Transaction[]
+  version: number
+  time: number
+  medianTime: number
+
+  transactionCount: number
+
+  difficulty: number
+
+  masternode: string
+  minter: string
+  minterBlockCount: number
+
+  stakeModifier: string
+  merkleroot: string
+
   size: number
+  sizeStripped: number
+  weight: number
 }
 
-export default function Blocks (props: BlocksPageProps): JSX.Element {
-  // dummy data
-  const { blocks } = props
+export default function Blocks (): JSX.Element {
+  const [blocks, setBlocks] = useState<Block[]>([])
 
   function renderBlocks (): JSX.Element[] {
     return blocks.map(block => (
@@ -28,12 +39,24 @@ export default function Blocks (props: BlocksPageProps): JSX.Element {
         <div className='flex-1'>
           <Link href={`/blocks/${block.id}/transactions`}><a>{block.height}</a></Link>
         </div>
-        <div className='flex-1'>{block.time}</div>
-        <div className='flex-1 text-right'>{block.transactions.length}</div>
+        <div className='flex-1'>{parseAge(block.time)}</div>
+        <div className='flex-1 text-right'>{block.transactionCount}</div>
         <div className='flex-1 text-right'>{block.size}</div>
       </div>
     ))
   }
+  const api = useWhaleApiClient()
+
+  const next = undefined
+
+  async function fetchBlocks (): Promise<void> {
+    const res = await api.blocks.list(30, next)
+    setBlocks(res)
+  }
+
+  useEffect(() => {
+    fetchBlocks().then(() => {}, () => {})
+  }, [])
 
   return (
     <div className='container mx-auto px-4 py-8'>
@@ -48,24 +71,4 @@ export default function Blocks (props: BlocksPageProps): JSX.Element {
       </TableBody>
     </div>
   )
-}
-
-// async operation to dummy data
-async function getBlocks (): Promise<Block[]> {
-  return await new Promise((resolve): void => {
-    const blocks = [
-      { id: '101', height: 100, time: parseAge(1627288589), transactions: [], size: 1 },
-      { id: '99', height: 99, time: parseAge(1627277303), transactions: [], size: 2 },
-      { id: '98', height: 98, time: parseAge(1626079179), transactions: [], size: 3 },
-      { id: '97', height: 97, time: parseAge(1595752871), transactions: [], size: 4 }
-    ]
-    setTimeout(() => {
-      resolve(blocks)
-    }, 200)
-  })
-}
-
-export async function getServerSideProps (): Promise<{ props: BlocksPageProps }> {
-  const blocks = await getBlocks()
-  return { props: { blocks } }
 }
