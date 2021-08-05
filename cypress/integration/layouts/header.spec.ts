73075e1b630b21a2504df3d1f6ff6fa8204ae367
Fuxing Loh <4266087+fuxingloh@users.noreply.github.com>
2021-08-05 21:08:23 +0800

    removed `useConnectedPlayground()` (#129)
    * added cms directory
    
    * with SSR, useConnectedPlayground() to find connected playground is invalid as network is now configured via querystring instead of state
    
    Intermediate network switching prove to be difficult via SSR rendering one network while SPA rendering on another.
    
    Also removed SetupPlayground is ci.yml

--- cypress/integration/layouts/header.spec.ts
+++ cypress/integration/layouts/header.spec.ts
@@ -4,6 +4,6 @@ describe('header', () => {
   })
 
   it('should contain block count', function () {
-    cy.get('header ul li:nth-child(1)').should('not.have.text', 'Blocks: 0')
+    cy.get('header ul li:nth-child(1)').should('not.have.text', 'Blocks: ...')
   })
 })
