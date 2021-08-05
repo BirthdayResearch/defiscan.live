fb8b5a21acee4fe2eb3c278ac87eb9b4b494720a
Ahmed Hilali <monstrobishi@gmail.com>
2021-08-05 18:21:26 +0800

    Added images and descriptions for oracles and symbols

--- src/cms/oracles/index.ts
+++ src/cms/oracles/index.ts
@@ -19,7 +19,32 @@ export interface OracleCopy {
   order: number
 }
 
-export const ORACLES: Record<string, OracleCopy> = {}
+export const ORACLES: Record<string, OracleCopy> = {
+  'nasdaq' : {
+    id: '',
+    name: 'Nasdaq',
+    image: 'cms/oracles/images/nasdaq.png',
+    order: 0
+  },
+  'finnhub' : {
+    id: '',
+    name: 'Finnhub',
+    image: 'cms/oracles/images/finnhub.png',
+    order: 1
+  },
+  'tiingo' : {
+    id: '',
+    name: 'Tiingo',
+    image: 'cms/oracles/images/tiingo.png',
+    order: 2
+  },
+  'iex' : {
+    id: '',
+    name: 'IEX Cloud',
+    image: 'cms/oracles/images/iex.png',
+    order: 3
+  }
+}
 
 export function getOracleCopy (id: string): OracleCopy | undefined {
   return ORACLES[id]
