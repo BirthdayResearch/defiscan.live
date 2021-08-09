2b5c06a125204ff30b7a7e839bb1dbb6a4a4114b
Suraj Auwal <siradjiawoual@gmail.com>
2021-08-09 21:00:22 +0100

    Tokens page redo

--- src/pages/tokens/index.tsx
+++ src/pages/tokens/index.tsx
@@ -1,85 +1,108 @@
-import Link from 'next/link'
-import { forwardRef, useState, useRef, useCallback, ForwardedRef, PropsWithChildren } from 'react'
-import { Loader } from '../../components/shared/Loader'
-import { useTokens } from '../../hooks/useTokens'
-import { tokens } from '@defichain/whale-api-client'
-import { Container } from '../../layouts/components/Container'
+import { GetServerSidePropsContext, GetServerSidePropsResult, InferGetServerSidePropsType } from 'next'
+import { MouseEvent, useState } from 'react'
+import { FaSearch, FaSort } from 'react-icons/fa'
+import { CgSortAz } from 'react-icons/cg'
 
-interface TokenItemProps extends PropsWithChildren<unknown> {
-  data: tokens.TokenData
+import { tokens } from '@defichain/whale-api-client'
+import { TokenData } from '@defichain/whale-api-client/dist/api/tokens'
+import { getWhaleApiClient } from '@contexts/WhaleContext'
+
+import { AdaptiveTable } from '@components/commons/AdaptiveTable'
+import { Head } from '@components/commons/Head'
+import { getTokenIcon } from '@components/icons/tokens'
+import { Link } from '@components/commons/Link'
+
+interface TokensPageData {
+  tokens: {
+    items: tokens.TokenData[]
+    nextToken: string | null
+  }
 }
 
-const TokenItem = forwardRef(function tokens ({ data }: TokenItemProps, ref: ForwardedRef<HTMLDivElement>) {
-  return (
-    <div
-      ref={ref} data-testid='tokens_listitem'
-      className='flex flex-col md:flex-row justify-center md:justify-between border-b border-gray-300'
-    >
-      <Link href={`tokens/${data.id}`}>
-        <a data-testid={`token_page_link_${data.id}`}>
-          <div className='p-4 text-center flex-none md:flex-1 md:text-left'>{data.symbol}</div>
-        </a>
-      </Link>
-      <div className='p-4 text-center flex-none md:flex-1 md:text-center'>{data.symbolKey}</div>
-      <div className='p-4 text-center flex-none md:flex-1 md:text-right'>{data.minted}</div>
-    </div>
-  )
-})
+export default function TokensPage ({ tokens }: InferGetServerSidePropsType<typeof getServerSideProps>): JSX.Element {
+  const [items, setItems] = useState(tokens.items)
 
-export default function TokensPage (): JSX.Element {
-  const [nextToken, setNextToken] = useState<string>('')
-  const observer = useRef<IntersectionObserver>()
-  const {
-    tokens,
-    loading,
-    error,
-    hasNext,
-    next
-  } = useTokens(5, nextToken)
-
-  const lastTokenRef = useCallback(node => {
-    if (loading) return
-    if (observer.current != null) {
-      observer.current.disconnect()
+  function handleSort (e: MouseEvent<HTMLButtonElement>): void {
+    const sortBy = e.currentTarget.dataset.sortBy
+    if (sortBy === 'tokenName') {
+      setItems([...(items.sort((a, b) => a.name.localeCompare(b.name)))])
+    } else {
+      setItems([...(items.sort((a, b) => parseInt(a.minted) - parseInt(b.minted)))])
     }
-
-    observer.current = new IntersectionObserver(entries => {
-      if (entries[0].isIntersecting && hasNext) {
-        setNextToken(next as string)
-      }
-    })
-    if (node !== null) observer.current.observe(node)
-  }, [loading, hasNext])
-
-  if (error) {
-    return <h1>Can not fetch tokens at this time.</h1>
   }
 
   return (
-    <Container>
-      <h1 className='text-2xl font-medium pb-8'>Tokens</h1>
-      <div className='w-full px-3 border-2 rounded-2xl shadow rounded-lg bg-white mt-3'>
-        <div
-          data-testid='tokens_table_head'
-          className='flex flex-col md:flex-row justify-center md:justify-between border-b-2 border-gray-300 py-2'
-        >
-          <div className='text-lg font-semibold text-lg py-4 px-2 text-center md:text-left '>name</div>
-          <div className='text-lg font-semibold text-lg py-4 px-2 text-center md:text-center '>symbol</div>
-          <div className='text-lg font-semibold text-lg py-4 px-2 text-center md:text-right'>minted</div>
-        </div>
-        {(tokens.length > 0) && tokens.map((token, index, array) => {
-          if (index === array.length - 1) {
-            return <TokenItem ref={lastTokenRef} data={token} key={token.id} />
-          } else {
-            return <TokenItem data={token} key={token.id} />
-          }
-        })}
-        {loading && (
-          <div className='my-5'>
-            <Loader />
+    <div className='container mx-auto px-4 pt-12 pb-20'>
+      <Head
+        title='Tokens'
+      />
+      <>
+        <div className='flex justify-between items-center'>
+          <h1 className='text-2xl font-semibold'>Tokens</h1>
+          <div className='space-x-2'>
+            <button className='p-3 rounded bg-gray-50 border border-grey-200 shadow-sm'>
+              <FaSearch height={40} />
+            </button>
+            <button className='p-3 rounded bg-gray-50 border border-grey-200 shadow-sm'>
+              <CgSortAz height={40} />
+            </button>
           </div>
-        )}
-      </div>
-    </Container>
+        </div>
+        <AdaptiveTable className='mt-6'>
+          <AdaptiveTable.Header>
+            <AdaptiveTable.Head className='flex items-center'>
+              TOKEN NAME <button data-sort-by='tokenName' onClick={(e) => handleSort(e)}><FaSort /></button>
+            </AdaptiveTable.Head>
+            <AdaptiveTable.Head className='text-right'>SYMBOL</AdaptiveTable.Head>
+            <AdaptiveTable.Head className='text-right'>CATEGORY</AdaptiveTable.Head>
+            <AdaptiveTable.Head className='text-right flex items-center'>
+              MINTED <button data-sort-by='minted' onClick={(e) => handleSort(e)}><FaSort /></button>
+            </AdaptiveTable.Head>
+          </AdaptiveTable.Header>
+          {items.map((data: TokenData) => (
+            <TokenRow data={data} key={data.id} />
+          ))}
+        </AdaptiveTable>
+      </>
+    </div>
   )
+}
+
+function TokenRow ({ data }: { data: TokenData }): JSX.Element {
+  const TokenIcon = getTokenIcon(data.symbol)
+  return (
+    <AdaptiveTable.Row>
+      <AdaptiveTable.Cell title='TOKEN NAME' className='align-middle'>
+        <div className='flex items-center space-x-2'>
+          <TokenIcon />
+          <Link href={{ pathname: `/tokens/${data.id}` }}>
+            <a className='ml-16 text-primary'>
+              {data.name}
+            </a>
+          </Link>
+        </div>
+      </AdaptiveTable.Cell>
+      <AdaptiveTable.Cell title='SYMBOL' className='align-middle lg:text-right'>
+        <div>{data.symbol}</div>
+      </AdaptiveTable.Cell>
+      <AdaptiveTable.Cell title='CATEGORIES' className='align-middle lg:text-right'>
+        <div>{data.isDAT ? 'DAT' : 'LPS'}</div>
+      </AdaptiveTable.Cell>
+      <AdaptiveTable.Cell title='MINTED' className='align-middle lg:text-right'>
+        <div>{data.minted}</div>
+      </AdaptiveTable.Cell>
+    </AdaptiveTable.Row>
+  )
+}
+
+export async function getServerSideProps (context: GetServerSidePropsContext): Promise<GetServerSidePropsResult<TokensPageData>> {
+  const items = await getWhaleApiClient(context).tokens.list()
+  return {
+    props: {
+      tokens: {
+        items,
+        nextToken: items.nextToken ?? null
+      }
+    }
+  }
 }
