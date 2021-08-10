7bd1ffc0a0992b71b26437832781fbcc0852803f
Fuxing Loh <git@fuxing.dev>
2021-08-10 21:52:53 +0800

    Removed getInitialProps from _app as we are using ssr now

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,23 +1,14 @@
-import App, { AppContext } from 'next/app'
 import NextNProgress from 'nextjs-progressbar'
 import { Default } from '../layouts/Default'
 import '../styles/globals.css'
 
-function ScanApp ({ Component, pageProps }): JSX.Element {
+function ScanApp ({ Component, pageProps }: any): JSX.Element {
   return (
     <Default>
       <NextNProgress color='#ff00af' height={2} />
       <Component {...pageProps} />
     </Default>
   )
-}
-
-/**
- * To load SSR for hydrating
- */
-ScanApp.getInitialProps = async (ctx: AppContext) => {
-  const appProps = await App.getInitialProps(ctx)
-  return { ...appProps }
 }
 
 export default ScanApp
