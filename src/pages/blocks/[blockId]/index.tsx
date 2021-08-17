2a4a6a4ac10630c1011dfe70ceaf89ee24e944a5
aikchun <iam@simaikchun.com>
2021-08-17 16:00:17 +0800

    Added copy button.

--- src/pages/blocks/[blockId]/index.tsx
+++ src/pages/blocks/[blockId]/index.tsx
@@ -1,15 +1,51 @@
+import { useState } from 'react'
 import { Breadcrumb } from '@components/commons/Breadcrumb'
 import { GetServerSidePropsContext, GetServerSidePropsResult, InferGetServerSidePropsType } from 'next'
 import { getWhaleApiClient } from '@contexts/WhaleContext'
 import { Block } from '@defichain/whale-api-client/dist/api/blocks'
+import { MdContentCopy } from 'react-icons/md'
 
 interface BlockDetailsPageProps {
   block: Block
 }
 
+function CopyButton ({ value }: { value: string }): JSX.Element {
+  const [open, setOpen] = useState<Boolean>(false)
+  function copy (): void {
+    const input = document.createElement('input')
+    input.value = value
+    document.body.appendChild(input)
+    input.select()
+    document.execCommand('copy')
+    document.body.removeChild(input)
+
+    setOpen(true)
+
+    setTimeout(() => {
+      setOpen(false)
+    }, 500)
+  }
+
+  return (
+    <div className='ml-1'>
+      {
+        open === true ? (
+          <span className='bg-gray-100 p-1 rounded'>
+            Copied!
+          </span>)
+          : (
+            <button className='p-2 rounded bg-gray-100' type='button' onClick={() => { copy() }}>
+              <MdContentCopy />
+            </button>
+          )
+      }
+    </div>
+  )
+}
+
 export default function BlockDetails ({ block }: InferGetServerSidePropsType<typeof getServerSideProps>): JSX.Element {
   console.log('block', block)
-  const { height } = block
+  const { height, hash } = block
   return (
     <div className='container mx-auto px-4 py-8'>
       <Breadcrumb items={[
@@ -18,9 +54,8 @@ export default function BlockDetails ({ block }: InferGetServerSidePropsType<typ
 
       ]}
       />
-
-      <div className='block-details-title'>Title #height</div>
-      <div className='block-details-hash'>Hash: __hash__ copy button</div>
+      <h1 className='font-semibold text-2xl'>Block #{height}</h1>
+      <div className='h-8 flex items-center'><span className='font-semibold'>Hash:&nbsp;</span> <span className='text-primary'> {hash} </span> <CopyButton value={hash} /></div>
       <div className='block-details-table-1'>
         <div className='block-details-table-1-block-reward'>xxxxx DFI</div>
         <div className='block-details-table-1-height'>height</div>
