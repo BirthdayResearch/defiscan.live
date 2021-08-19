4b9500706940fc2093955f04a22df95f17ad2d6e
aikchun <iam@simaikchun.com>
2021-08-19 22:24:55 +0800

    Added tests for mobile.

--- cypress/integration/pages/blocks/index.spec.ts
+++ cypress/integration/pages/blocks/index.spec.ts
@@ -15,6 +15,7 @@ context('/blocks on desktop', () => {
     cy.findByTestId('OverflowTable.Header').then(ele => {
       cy.wrap(ele).findByText('HEIGHT').should('be.visible')
       cy.wrap(ele).findByText('AGE').should('be.visible')
+      cy.wrap(ele).findByText('TRANSACTIONS').should('be.visible')
       cy.wrap(ele).findByText('MINTER').should('be.visible')
       cy.wrap(ele).findByText('SIZE (B)').should('be.visible')
       cy.wrap(ele).findByText('DIFFICULTY').should('be.visible')
@@ -40,23 +41,45 @@ context('/blocks on desktop', () => {
 
       expect(pages[1]).not.equals(pages[0])
     })
-
-    cy.findByTestId('CursorPagination.Prev').click()
-    cy.findByTestId('OverflowTable').should((ele) => {
-      expect(ele.text()).equals(pages[0])
-      expect(ele.text()).not.equals(pages[1])
-    })
   })
 })
 
-context('/tokens on mobile', () => {
+context('/blocks on mobile', () => {
 
   before(() => {
-    cy.viewport('iphone-x')
     cy.visit('/blocks?network=MainNet')
+  })
+
+  beforeEach(() => {
+    cy.viewport('iphone-x')
   })
 
   it('should have heading', () => {
     cy.get('h1').should('have.text', 'Blocks')
+  })
+
+  it('should have OverflowTable header information', function () {
+    cy.findByTestId('OverflowTable.Header').then(ele => {
+      cy.wrap(ele).findByText('HEIGHT').should('be.visible')
+      cy.wrap(ele).findByText('AGE').should('be.visible')
+      cy.wrap(ele).findByText('TRANSACTIONS').should('be.visible')
+      cy.wrap(ele).findByText('MINTER').should('not.be.visible')
+    })
+  })
+
+  it('should CursorPagination.Next and CursorPagination.Prev', function () {
+    const pages: Record<number, string> = {}
+
+    cy.findByTestId('OverflowTable').should((ele) => {
+      pages[0] = ele.text()
+    })
+
+    cy.findByTestId('CursorPagination.Next').click()
+    cy.wait(1000)
+    cy.findByTestId('OverflowTable').should((ele) => {
+      pages[1] = ele.text()
+
+      expect(pages[1]).not.equals(pages[0])
+    })
   })
 })
