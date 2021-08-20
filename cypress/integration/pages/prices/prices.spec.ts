204b7fdfad86541113b2be10ead4753610933af9
Fuxing Loh <4266087+fuxingloh@users.noreply.github.com>
2021-08-20 17:49:30 +0800

    refactor /prices tests (#205)

--- cypress/integration/pages/prices/prices.spec.ts
+++ cypress/integration/pages/prices/prices.spec.ts
@@ -12,18 +12,17 @@ context('/prices', () => {
   })
 
   it('should contain at least 20 price feed', function () {
-    cy.get('[data-testid="PriceFeed"]').should('have.length.above', 20)
+    cy.findAllByTestId('PriceFeed').should('have.length.above', 20)
   })
 
   it('should have common PriceFeed texts', () => {
-    cy.get('[data-testid="PriceFeed"]').first()
-      .should('contain.text','View')
-      .should('contain.text','USD')
+    cy.findAllByTestId('PriceFeed').eq(0)
+      .should('contain.text', 'View')
+      .should('contain.text', 'USD')
   })
 
   it('should be able to click on a PriceFeed', () => {
-    cy.get('[data-testid="PriceFeed"]').first().click()
-    cy.wait(5000)
+    cy.findAllByTestId('PriceFeed').eq(0).click()
 
     cy.location().should((loc) => {
       expect(loc.pathname).to.contains('/prices/')
