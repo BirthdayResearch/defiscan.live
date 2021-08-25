0aa9ceadf845dcae9b9082595a259e089dc30c58
aikchun <iam@simaikchun.com>
2021-08-25 15:01:00 +0800

    Added a test for the hash appear on the screen.

--- cypress/integration/pages/blocks/[blockId].spec.ts
+++ cypress/integration/pages/blocks/[blockId].spec.ts
@@ -140,4 +140,17 @@ context('/blocks/[blockId] on mobile', () => {
       })
     })
   })
+
+  // it('should CursorPagination.Next', function () {
+  //   cy.wait(500)
+  //   cy.findAllByTestId('OverflowTable.Cell').then((ele) => {
+  //     const pageOneFirstCell = ele[0].innerText
+  //     cy.interceptServerSideWait(() => {
+  //       cy.findByTestId('CursorPagination.Next').click()
+  //     })
+  //     cy.findAllByTestId('OverflowTable.Cell').then((pageTwoCells) => {
+  //       expect(pageTwoCells[0].innerText).not.equals(pageOneFirstCell)
+  //     })
+  //   })
+  // })
 })
