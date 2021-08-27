9acc23c50d126589d949ee3270fb3cf51747eb55
Fuxing Loh <git@fuxing.dev>
2021-08-27 15:26:00 +0800

    Updated MasternodesPage with v0.9 API

--- src/pages/masternodes/index.tsx
+++ src/pages/masternodes/index.tsx
@@ -1,10 +1,10 @@
-import { GetServerSidePropsContext, GetServerSidePropsResult, InferGetServerSidePropsType } from 'next'
 import { CursorPage, CursorPagination } from '@components/commons/CursorPagination'
-import { getWhaleApiClient } from '@contexts/WhaleContext'
-import { MasternodeData } from '@defichain/whale-api-client/dist/api/masternodes'
 import { Head } from '@components/commons/Head'
-import NumberFormat from 'react-number-format'
 import { OverflowTable } from '@components/commons/OverflowTable'
+import { getWhaleApiClient } from '@contexts/WhaleContext'
+import { MasternodeData, MasternodeState } from '@defichain/whale-api-client/dist/api/masternodes'
+import { GetServerSidePropsContext, GetServerSidePropsResult, InferGetServerSidePropsType } from 'next'
+import NumberFormat from 'react-number-format'
 
 interface MasternodesPageProps {
   masternodes: {
@@ -20,7 +20,8 @@ export default function MasternodesPage ({ masternodes }: InferGetServerSideProp
 
       <div className='flex items-center justify-center pb-6'>
         <div className='bg-pink-50 rounded p-3'>
-          🚧 Work in progress, this is an early iteration of defiscan.live/masternodes. Some features are not available and
+          🚧 Work in progress, this is an early iteration of defiscan.live/masternodes. Some features are not available
+          and
           may not work as expected.
         </div>
       </div>
@@ -34,6 +35,7 @@ export default function MasternodesPage ({ masternodes }: InferGetServerSideProp
           <OverflowTable.Head>RESIGN HEIGHT</OverflowTable.Head>
           <OverflowTable.Head>MINTED BLOCKS</OverflowTable.Head>
           <OverflowTable.Head>STATE</OverflowTable.Head>
+          <OverflowTable.Head>TIMELOCK</OverflowTable.Head>
         </OverflowTable.Header>
         {masternodes.items.map((mn) => (
           <MasternodeRow data={mn} key={mn.id} />
@@ -46,13 +48,13 @@ export default function MasternodesPage ({ masternodes }: InferGetServerSideProp
   )
 }
 
-function MasternodeRow ({ data }: {data: MasternodeData}): JSX.Element {
+function MasternodeRow ({ data }: { data: MasternodeData }): JSX.Element {
   return (
     <OverflowTable.Row>
-      <OverflowTable.Cell sticky className='break-all' width='w-72'>
+      <OverflowTable.Cell sticky className='break-all' width='w-64'>
         {data.owner.address}
       </OverflowTable.Cell>
-      <OverflowTable.Cell className='break-all' width='w-72'>
+      <OverflowTable.Cell className='break-all' width='w-64'>
         {data.operator.address}
       </OverflowTable.Cell>
       <OverflowTable.Cell>
@@ -65,7 +67,7 @@ function MasternodeRow ({ data }: {data: MasternodeData}): JSX.Element {
       </OverflowTable.Cell>
       <OverflowTable.Cell>
         <NumberFormat
-          value={data.resign.height}
+          value={data.resign?.height}
           fixedDecimalScale
           displayType='text'
           thousandSeparator=','
@@ -80,7 +82,38 @@ function MasternodeRow ({ data }: {data: MasternodeData}): JSX.Element {
         />
       </OverflowTable.Cell>
       <OverflowTable.Cell>
-        {data.state}
+        {(() => {
+          switch (data.state) {
+            case MasternodeState.PRE_ENABLED:
+              return 'Pre-Enabled'
+            case MasternodeState.ENABLED:
+              return 'Enabled'
+            case MasternodeState.PRE_RESIGNED:
+              return 'Pre-Resigned'
+            case MasternodeState.RESIGNED:
+              return 'Resigned'
+            case MasternodeState.PRE_BANNED:
+              return 'Pre-Banned'
+            case MasternodeState.BANNED:
+              return 'Banned'
+            default:
+              return data.state
+          }
+        })()}
+      </OverflowTable.Cell>
+      <OverflowTable.Cell>
+        {(() => {
+          switch (data.timelock) {
+            case 0:
+              return <div>0 year</div>
+            case 260:
+              return <div>5 years</div>
+            case 520:
+              return <div>10 years</div>
+            default:
+              return <div>{data.timelock} Weeks</div>
+          }
+        })()}
       </OverflowTable.Cell>
     </OverflowTable.Row>
   )
@@ -88,7 +121,7 @@ function MasternodeRow ({ data }: {data: MasternodeData}): JSX.Element {
 
 export async function getServerSideProps (context: GetServerSidePropsContext): Promise<GetServerSidePropsResult<MasternodesPageProps>> {
   const next = CursorPagination.getNext(context)
-  const items = await getWhaleApiClient(context).masternodes.list(20, next)
+  const items = await getWhaleApiClient(context).masternodes.list(30, next)
   return {
     props: {
       masternodes: {
