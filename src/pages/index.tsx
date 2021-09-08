aa8ae3d8408a300f6ebacce441615f7ab9f228da
aikchun <iam@simaikchun.com>
2021-09-08 16:57:55 +0800

    Added a bunch of summary cards.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
+import classnames from 'classnames'
+import { ReactNode, PropsWithChildren } from 'react'
 import { Container } from '@components/commons/Container'
 import { GetServerSidePropsResult } from 'next'
-import { IoSearchOutline } from 'react-icons/io5'
+import { IoCaretUp, IoSearchOutline } from 'react-icons/io5'
 
 function Banner (): JSX.Element {
   return (
@@ -22,10 +24,61 @@ function Banner (): JSX.Element {
   )
 }
 
+function SummaryCard ({ children }: PropsWithChildren<{ children: ReactNode }>): JSX.Element {
+  return (
+    <div className='border border-gray-50 rounded-lg shadow-lg flex-1 h-64 pt-6 px-7 pb-7'>
+      {children}
+    </div>
+  )
+}
+
+function SummaryCardTitle ({ children }: PropsWithChildren<{children: ReactNode}>): JSX.Element {
+  return (
+    <div className='text-lg text-gray-500 leading-6'>{children}</div>
+  )
+}
+
+function SummaryCardHeader ({ children, className }: PropsWithChildren<{children: ReactNode, className?: string}>): JSX.Element {
+  return (
+    <div className={classnames('text-2xl text-gray-900 leading-8 flex items-center', className)}>{children}</div>
+  )
+}
+
+function SummaryCardHeadModifier ({ children, className }: PropsWithChildren<{children: ReactNode, className?: string}>): JSX.Element {
+  return <span className={classnames('text-base ml-3', className)}>{children}</span>
+}
+
+function Summary (): JSX.Element {
+  return (
+    <div className='mt-12'>
+      <span className='text-xl font-semibold'>DeFiChain in numbers</span>
+      <div className='flex mt-4 gap-x-4'>
+        <SummaryCard>
+          <SummaryCardTitle>Price</SummaryCardTitle>
+          <SummaryCardHeader className='text-4xl leading-10'>$2.97<SummaryCardHeadModifier className='text-green-500'><IoCaretUp className='inline' /> 23.10%</SummaryCardHeadModifier></SummaryCardHeader>
+        </SummaryCard>
+        <SummaryCard>
+          <SummaryCardTitle>Total Value Locked</SummaryCardTitle>
+          <SummaryCardHeader>$999,999,999</SummaryCardHeader>
+        </SummaryCard>
+        <SummaryCard>
+          <SummaryCardTitle>Masternodes</SummaryCardTitle>
+          <SummaryCardHeader>222,397,682 DFI</SummaryCardHeader>
+        </SummaryCard>
+        <SummaryCard>
+          <SummaryCardTitle>Transaction Activity</SummaryCardTitle>
+          <SummaryCardHeader>11,106,158,292,201</SummaryCardHeader>
+        </SummaryCard>
+      </div>
+    </div>
+  )
+}
+
 export default function HomePage (): JSX.Element {
   return (
     <Container>
       <Banner />
+      <Summary />
     </Container>
   )
 }
