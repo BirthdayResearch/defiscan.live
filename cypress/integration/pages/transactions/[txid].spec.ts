eabc63b4b1d1adadf3df380486fa7e6e6dcb00c9
aikchun <iam@simaikchun.com>
2021-09-13 16:57:17 +0800

    Added a placeholders and updated the test for transaction detail page.

--- cypress/integration/pages/transactions/[txid].spec.ts
+++ cypress/integration/pages/transactions/[txid].spec.ts
@@ -15,42 +15,41 @@ context('/transactions/[txid] on desktop', () => {
     cy.findByTestId('transaction-hash').should('have.text', 'f5f619011d0c79747ae269b545156d2b3dcba2045dec05f5568181452bbcbe02')
   })
 
-  it('should have details subtitle', () => {
-    cy.findByTestId('block-reward').should('have.text', 'xxxx DFI')
+  it('should have transaction-detail-total-amount', () => {
+    cy.findByTestId('transaction-detail-total-amount').should('have.text', '')
   })
   
-  it('should have fee', () => {
-    cy.findByTestId('fee').should('have.text', 'xxxx DFI')
+  it('should have transaction-detail-fee', () => {
+    cy.findByTestId('transaction-detail-fee').should('have.text', '')
   })
 
-  it('should have confirmations', () => {
-    cy.findByTestId('confirmations').should('be.visible')
+  it('should have transaction-detail-confirmations', () => {
+    cy.findByTestId('transaction-detail-confirmations').contains(/\d+/)
   })
 
-  it('should have block height', () => {
-    cy.findByTestId('block-height').should('have.text', '1133915')
+  it('should have transaction-detail-block-height', () => {
+    cy.findByTestId('transaction-detail-block-height').should('have.text', '1133915')
   })
 
-  it('should have custom transaction', () => {
-    cy.findByTestId('custom-transaction').should('have.text', 'xxxx')
+  it('should have transaction-detail-custom-transaction', () => {
+    cy.findByTestId('transaction-detail-custom-transaction').should('have.text', '')
   })
 
-  it('should have fee rate', () => {
-    cy.findByTestId('fee-rate').should('have.text', 'xxxx DFI')
+  it('should have transaction-detail-fee-rate', () => {
+    cy.findByTestId('transaction-detail-fee-rate').should('have.text', '')
   })
 
-  it('should have size', () => {
-    cy.findByTestId('size').should('have.text', '208')
-  })
-  
-  it('should have received time', () => {
-    cy.findByTestId('received-time').should('have.text', '(received time)')
+  it('should have transaction-detail-size', () => {
+    cy.findByTestId('transaction-detail-size').should('have.text', '208')
   })
 
-  it('should have mined time', () => {
-    cy.findByTestId('mined-time').should('have.text', '(mined time)')
+  it('should have transaction-detail-received-time', () => {
+    cy.findByTestId('transaction-detail-received-time').should('have.text', '')
   })
 
+  it('should have transaction-detail-mined-time', () => {
+    cy.findByTestId('transaction-detail-mined-time').should('have.text', '')
+  })
 });
 
 context('/transactions/[txid] on mobile', () => {
@@ -70,41 +69,39 @@ context('/transactions/[txid] on mobile', () => {
     cy.findByTestId('transaction-hash').should('have.text', 'f5f619011d0c79747ae269b545156d2b3dcba2045dec05f5568181452bbcbe02')
   })
 
-  it('should have details subtitle', () => {
-    cy.findByTestId('block-reward').should('have.text', 'xxxx DFI')
+  it('should have transaction-detail-total-amount', () => {
+    cy.findByTestId('transaction-detail-total-amount').should('have.text', '')
   })
   
-  it('should have fee', () => {
-    cy.findByTestId('fee').should('have.text', 'xxxx DFI')
+  it('should have transaction-detail-fee', () => {
+    cy.findByTestId('transaction-detail-fee').should('have.text', '')
   })
 
-  it('should have confirmations', () => {
-    cy.findByTestId('confirmations').should('be.visible')
+  it('should have transaction-detail-confirmations', () => {
+    cy.findByTestId('transaction-detail-confirmations').contains(/\d+/)
   })
 
-  it('should have block height', () => {
-    cy.findByTestId('block-height').should('have.text', '1133915')
+  it('should have transaction-detail-block-height', () => {
+    cy.findByTestId('transaction-detail-block-height').should('have.text', '1133915')
   })
 
-  it('should have custom transaction', () => {
-    cy.findByTestId('custom-transaction').should('have.text', 'xxxx')
+  it('should have transaction-detail-custom-transaction', () => {
+    cy.findByTestId('transaction-detail-custom-transaction').should('have.text', '')
   })
 
-  it('should have fee rate', () => {
-    cy.findByTestId('fee-rate').should('have.text', 'xxxx DFI')
+  it('should have transaction-detail-fee-rate', () => {
+    cy.findByTestId('transaction-detail-fee-rate').should('have.text', '')
   })
 
-  it('should have size', () => {
-    cy.findByTestId('size').should('have.text', '208')
-  })
-  
-  it('should have received time', () => {
-    cy.findByTestId('received-time').should('have.text', '(received time)')
+  it('should have transaction-detail-size', () => {
+    cy.findByTestId('transaction-detail-size').should('have.text', '208')
   })
 
-  it('should have mined time', () => {
-    cy.findByTestId('mined-time').should('have.text', '(mined time)')
+  it('should have transaction-detail-received-time', () => {
+    cy.findByTestId('transaction-detail-received-time').should('have.text', '')
   })
 
-
+  it('should have transaction-detail-mined-time', () => {
+    cy.findByTestId('transaction-detail-mined-time').should('have.text', '')
+  })
 })
