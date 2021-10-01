70026e89863d740e04fdeb59ed39845718192a49
aikchun <iam@simaikchun.com>
2021-10-01 13:00:18 +0800

    Added testIds to components and written tests.

--- cypress/integration/pages/index.ts
+++ cypress/integration/pages/index.ts
@@ -10,4 +10,20 @@ context('/blocks on macbook-13', () => {
   it('should have summary-price', () => {
     cy.findByTestId('summary-price').should('be.visible')
   })
+  it('should have summary-tvl', () => {
+    cy.findByTestId('summary-tvl').should('be.visible')
+  })
+  it('should have summary-masternodes', () => {
+    cy.findByTestId('summary-masternodes').should('be.visible')
+  })
+
+  it('should have stat-blocks', () => {
+    cy.findByTestId('stat-blocks').should('be.visible')
+  })
+  it('should have stat-total-dfi-burned', () => {
+    cy.findByTestId('stat-total-dfi-burned').should('be.visible')
+  })
+  it('should have stat-difficulty', () => {
+    cy.findByTestId('stat-difficulty').should('be.visible')
+  })
 })
