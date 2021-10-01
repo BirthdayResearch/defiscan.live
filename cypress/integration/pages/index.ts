d910b619f566f3a494723278d8b20e2ee93128b2
aikchun <iam@simaikchun.com>
2021-10-01 15:07:55 +0800

    Added tests for the navigation to mainnet explorer.

--- cypress/integration/pages/index.ts
+++ cypress/integration/pages/index.ts
@@ -26,4 +26,21 @@ context('/blocks on macbook-13', () => {
   it('should have stat-difficulty', () => {
     cy.findByTestId('stat-difficulty').should('be.visible')
   })
+  it('should have view-all-blocks-link', () => {
+    cy.findByTestId('view-all-blocks-link').click()
+    cy.location('pathname').should('eq', '/blocks')
+    cy.go('back')
+  })
+  it('should have view-all-blocks-button', () => {
+    cy.findByTestId('view-all-blocks-button').click()
+    cy.location('pathname').should('eq', '/blocks')
+    cy.go('back')
+  })
+
+  it('should have latest-transactions-link', () => {
+    cy.findByTestId('latest-transactions-link').should('have.attr', 'href', 'https://mainnet.defichain.io/#/DFI/mainnet/home')
+  })
+  it('should have latest-transactions-button', () => {
+    cy.findByTestId('latest-transactions-button').should('have.attr', 'href', 'https://mainnet.defichain.io/#/DFI/mainnet/home')
+  })
 })
