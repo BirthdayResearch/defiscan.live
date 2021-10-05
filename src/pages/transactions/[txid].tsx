719acfd6f817ad647256467988bdfda3ca00a07a
Joel-David Wong <git@joeldavid.dev>
2021-10-05 22:50:34 +0800

    Added txn details with addresses and updated ui

--- src/pages/transactions/[txid].tsx
+++ src/pages/transactions/[txid].tsx
@@ -15,9 +15,9 @@ import { Link } from '@components/commons/Link'
 import { format, fromUnixTime } from 'date-fns'
 import BigNumber from 'bignumber.js'
 import { useNetwork } from '@contexts/NetworkContext'
-import { PropsWithChildren, ReactNode } from 'react'
 import { IoArrowForwardOutline } from 'react-icons/io5'
 import { fromScriptHex } from '@defichain/jellyfish-address'
+import { NetworkName } from '@defichain/jellyfish-network'
 
 interface TransactionPageProps {
   txid: string
@@ -168,17 +168,27 @@ function TransactionDetails (props: { transaction: Transaction, vins: Transactio
   const transaction = props.transaction
   const vins = props.vins
   const vouts = props.vouts
+  const network = useNetwork()
+
+  let networkName: NetworkName
+  if (network.toString() === 'MainNet') {
+    networkName = 'mainnet'
+  } else if (network.toString() === 'TestNet') {
+    networkName = 'testnet'
+  } else {
+    networkName = 'regtest'
+  }
 
   return (
     <>
       <h1 className='font-medium text-2xl mt-6' data-testid='details-subtitle'>Details</h1>
 
       <div className='mt-5 flex flex-col space-y-6 items-start lg:flex-row lg:space-x-8 lg:space-y-0'>
-        <TransactionDetailsLeft transaction={transaction} vins={vins} vouts={vouts} />
+        <TransactionDetailsLeft transaction={transaction} vins={vins} vouts={vouts} networkName={networkName} />
         <div className='flex items-center justify-center text-gray-600 w-full lg:w-auto lg:h-20'>
           <IoArrowForwardOutline className='transform rotate-90 lg:rotate-0' size={24} />
         </div>
-        <TransactionDetailsRight transaction={transaction} vins={vins} vouts={vouts} />
+        <TransactionDetailsRight transaction={transaction} vins={vins} vouts={vouts} networkName={networkName} />
       </div>
 
       <TransactionDetailsSummary transaction={transaction} vins={vins} />
@@ -186,41 +196,46 @@ function TransactionDetails (props: { transaction: Transaction, vins: Transactio
   )
 }
 
-function TransactionDetailsLeft (props: { transaction: Transaction, vins: TransactionVin[], vouts: TransactionVout[] }): JSX.Element {
+function TransactionDetailsLeft (props: { transaction: Transaction, vins: TransactionVin[], vouts: TransactionVout[], networkName: NetworkName }): JSX.Element {
   return (
-    <div className='w-full lg:w-1/2 max-h-screen overflow-y-auto'>
+    <div className='w-full lg:w-1/2 max-h-screen overflow-y-auto pr-1'>
       <div className='flex flex-col gap-y-1'>
-        {props.vins.map((vin) => {
-          let address: string | undefined
-          if (vin.script !== undefined) {
-            address = fromScriptHex(vin.script.hex, 'mainnet')?.address
-          }
+        {props.vins
+          .map((vin) => {
+            if (vin.vout === undefined) {
+              // Coinbase Txn
+              return (
+                <InputOutputBlock label='INPUT' address={undefined} value='Coinbase (Newly Generated Coins)' />
+              )
+            }
 
-          return (
-            <InputOutputBlock label='INPUT' key={vin.sequence}>
-              <span className='opacity-60'>{address}</span>
-              <span>{vin.vout?.value} DFI</span>
-            </InputOutputBlock>
-          )
-        })}
+            const txid = vin.vout.txid
+            const address = fromScriptHex(vin.vout.script.hex, props.networkName)?.address
+            const value = vin.vout.value
+
+            return (
+              <InputOutputBlock label='INPUT' address={address} value={`${value} DFI`} key={txid} />
+            )
+          })}
       </div>
     </div>
   )
 }
 
-function TransactionDetailsRight (props: { transaction: Transaction, vins: TransactionVin[], vouts: TransactionVout[] }): JSX.Element {
+function TransactionDetailsRight (props: { transaction: Transaction, vins: TransactionVin[], vouts: TransactionVout[], networkName: NetworkName }): JSX.Element {
   return (
     <div className='w-full lg:w-1/2'>
       <div className='flex flex-col gap-y-1'>
         {props.vouts.map((vout) => {
+          const address = fromScriptHex(vout.script.hex, props.networkName)?.address
+
           return (
             <InputOutputBlock
               label='OUTPUT'
-              key={vout.script.hex}
-            >
-              <span className='text-primary'>{vout.script.type}</span>
-              <span>{vout.value} DFI</span>
-            </InputOutputBlock>
+              address={address}
+              value={`${vout.value} DFI`}
+              key={vout.txid}
+            />
           )
         })}
       </div>
@@ -232,31 +247,31 @@ function TransactionDetailsSummary (props: { transaction: Transaction, vins: Tra
   const fee = props.vins[0].vout === undefined ? 'Coinbase' : `${(getTransactionFee(props.transaction, props.vins)).decimalPlaces(8).toString()} mDFI`
 
   return (
-    <div className='flex flex-col items-end justify-between h-16 mt-8'>
+    <div className='flex flex-col items-end justify-between mt-8'>
       <div className='flex justify-between gap-x-3'>
-        <div>Fees:</div>
-        <div>{fee}</div>
+        <span>Fees:</span>
+        <span>{fee}</span>
       </div>
-      <div className='flex justify-between gap-x-3'>
-        <div>Total:</div>
-        <div>{props.transaction.totalVoutValue} DFI</div>
+      <div className='flex justify-between gap-x-3 mt-2'>
+        <span>Total:</span>
+        <span>{props.transaction.totalVoutValue} DFI</span>
       </div>
     </div>
   )
 }
 
-function InputOutputBlock ({
-  label,
-  children
-}: PropsWithChildren<{ label: 'INPUT' | 'OUTPUT', children: ReactNode }>): JSX.Element {
+function InputOutputBlock (props: { label: 'INPUT' | 'OUTPUT', address: string | undefined, value: string | undefined }): JSX.Element {
   return (
     <div className='bg-gray-50 h-20 p-3 rounded flex justify-between'>
       <div className='flex flex-col justify-between h-full w-full'>
-        <span className='bg-gray-200 rounded text-xs px-2 py-1 font-medium w-min'>
-          {label}
+        <span className='bg-gray-200 rounded text-xs px-2 py-1 font-medium w-min mb-1.5'>
+          {props.label}
         </span>
-        <div className='flex justify-between'>
-          {children}
+        <div className='flex justify-between gap-x-2'>
+          <span className='opacity-60 overflow-ellipsis overflow-hidden'>
+            {props.address !== undefined ? props.address : 'N/A'}
+          </span>
+          <span className='min-w-max'>{props.value !== undefined && `${props.value}`}</span>
         </div>
       </div>
     </div>
