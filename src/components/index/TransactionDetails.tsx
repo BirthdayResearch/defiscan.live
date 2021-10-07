d9bc2de5922e97049f114b94a2f2a085ce3565e6
aikchun <iam@simaikchun.com>
2021-10-07 14:07:03 +0800

    fixed issue with age and value of the transaction in the TransactionDetails.

--- src/components/index/TransactionDetails.tsx
+++ src/components/index/TransactionDetails.tsx
@@ -4,26 +4,28 @@ import { IoTimeOutline } from 'react-icons/io5'
 export function TransactionDetails ({ hash, age, from, to, confirmations }: {hash: string, age: string, from: string, to: string, confirmations: number|undefined}): JSX.Element {
   return (
     <div className='h-40 p-4 border border-gray-200 '>
-      <div className='w-full'>
+      <div className='w-full flex justify-between'>
         <span
           className='w-5/12 inline-block leading-6 text-gray-900 font-semibold overflow-ellipsis overflow-hidden'
         >
           {hash}
         </span>
-        <span>
-          <span
-            className='text-xs text-opacity-40 text-black font-medium'
-          >
-            <IoTimeOutline size={15} className='inline' />
-            <span className='ml-1.5'>{age}</span>
+        <div className='flex items-center'>
+          <span>
+            <span
+              className='text-xs text-opacity-40 text-black font-medium'
+            >
+              <IoTimeOutline size={15} className='inline' />
+              <span className='ml-1.5'>{age}</span>
+            </span>
+            <NumberFormat
+              className='h-5 text-xs leading-4 font-medium px-2 py-0.5 rounded bg-gray-100'
+              value={24200.032}
+              decimalScale={3}
+              suffix=' DFI'
+            />
           </span>
-          <NumberFormat
-            className='h-5 text-xs leading-4 font-medium px-2 py-0.5 rounded bg-gray-100'
-            value={24200.032}
-            decimalScale={3}
-            suffix=' DFI'
-          />
-        </span>
+        </div>
       </div>
       <div className='mt-4'>
         <div className='flex gap-x-1.5 text-sm leading-5'>
