653c4d5064a9a35cbd02b4786d7bc1aacc79f92a
Suraj Auwal <siradjiawoual@gmail.com>
2021-10-10 16:04:27 +0100

    Fixed lint issue

--- cypress/integration/pages/transactions/DfTx/SetOracleData.spec.ts
+++ cypress/integration/pages/transactions/DfTx/SetOracleData.spec.ts
@@ -15,23 +15,22 @@ context('/transactions/[txid] - DfTx Set Oracle Data on desktop', () => {
     cy.findByTestId('DfTxHeader.Subtitle').contains('Type:Set Oracle Data')
   })
 
-  it('should have DfTxSetOracleData Oracle ID ',  ()  =>{
+  it('should have DfTxSetOracleData Oracle ID ', () => {
     cy.findByTestId('DfTxSetOracleData.OracleId').should('have.text', 'd96d04bd4b5b35d12d20d9c6b70809f8e83118ccffff307b82459dd42a6b5f07')
   })
 
-  it('should have DfTxSetOracleData Received Time',  ()  =>{
+  it('should have DfTxSetOracleData Received Time', () => {
     cy.findByTestId('DfTxSetOracleData.Timestamp').should('have.text', 'Oct 7, 2021, 5:43:25 PM')
   })
 
-  it('should have DfTxSetOracleData Token DFI Amount',  () => {
+  it('should have DfTxSetOracleData Token DFI Amount', () => {
     cy.findByTestId('DfTxSetOracleData.DFIAmount').should('have.text', '2.50350398 DFI')
   })
 
-  it('should have DfTxSetOracleData Token DFI Currency',  () => {
+  it('should have DfTxSetOracleData Token DFI Currency', () => {
     cy.findByTestId('DfTxSetOracleData.DFICurrency').should('have.text', 'USD')
   })
 })
-
 
 context('/transactions/[txid] - DfTx Set Oracle Data on mobile', () => {
   before(() => {
@@ -50,19 +49,19 @@ context('/transactions/[txid] - DfTx Set Oracle Data on mobile', () => {
     cy.findByTestId('DfTxHeader.Subtitle').contains('Type:Set Oracle Data')
   })
 
-  it('should have DfTxSetOracleData Oracle ID ',  ()  =>{
+  it('should have DfTxSetOracleData Oracle ID ', () => {
     cy.findByTestId('DfTxSetOracleData.OracleId').should('have.text', 'd96d04bd4b5b35d12d20d9c6b70809f8e83118ccffff307b82459dd42a6b5f07')
   })
 
-  it('should have DfTxSetOracleData Received Time',  ()  =>{
+  it('should have DfTxSetOracleData Received Time', () => {
     cy.findByTestId('DfTxSetOracleData.Timestamp').should('have.text', 'Oct 7, 2021, 5:43:25 PM')
   })
 
-  it('should have DfTxSetOracleData Token DFI Amount',  () => {
+  it('should have DfTxSetOracleData Token DFI Amount', () => {
     cy.findByTestId('DfTxSetOracleData.DFIAmount').should('have.text', '2.50350398 DFI')
   })
 
-  it('should have DfTxSetOracleData Token DFI Currency',  () => {
+  it('should have DfTxSetOracleData Token DFI Currency', () => {
     cy.findByTestId('DfTxSetOracleData.DFICurrency').should('have.text', 'USD')
   })
 })
