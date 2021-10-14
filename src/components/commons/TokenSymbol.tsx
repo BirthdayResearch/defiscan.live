d267ebfb2edc4cd91aa3bdb3820a8760ba55af8d
Joel-David Wong <4586209+joeldavidw@users.noreply.github.com>
2021-10-15 00:45:33 +0800

    Component to retrieve token symbol asynchronously (#401)
    * Added common component to retrieve token symbol asynchronously
    
    * Changed to darker colour and added left margin
    
    * Added token type check and catch on token not found
    
    * Made changes according to suggestion
    
    * Update src/components/commons/TokenSymbol.tsx
    
    Co-authored-by: Fuxing Loh <4266087+fuxingloh@users.noreply.github.com>
    
    Co-authored-by: Fuxing Loh <4266087+fuxingloh@users.noreply.github.com>

---
+++ src/components/commons/TokenSymbol.tsx
@@ -0,0 +1,54 @@
+import { useWhaleApiClient } from '@contexts/WhaleContext'
+import { useEffect, useState } from 'react'
+import { TokenData } from '@defichain/whale-api-client/dist/api/tokens'
+import classNames from 'classnames'
+import { getAssetIcon, getTokenIcon } from '@components/icons/assets'
+
+interface TokenSymbolProps {
+  tokenId: number
+  className?: string
+  testId: string
+}
+
+export function TokenSymbol (props: TokenSymbolProps): JSX.Element {
+  const api = useWhaleApiClient()
+  const [tokenData, setTokenData] = useState<TokenData | undefined>(undefined)
+
+  useEffect(() => {
+    if (typeof props.tokenId !== 'number') {
+      return
+    }
+
+    const response = api.tokens.get(props.tokenId.toString())
+    void response.then(data => {
+      setTokenData(data)
+    }).catch(() => {
+      setTokenData(undefined)
+    })
+  }, [props.tokenId])
+
+  if (tokenData === undefined) {
+    return (
+      <div className='animate-pulse py-2.5 w-10 rounded-md bg-gray-200 inline ml-1' />
+    )
+  }
+
+  return (
+    <div className='flex gap-x-1'>
+      <div className={classNames(props.className)} data-testid={props.testId}>
+        {tokenData.symbol}{!tokenData.isDAT && `#${tokenData.id}`}
+      </div>
+      <div className='my-auto'>
+        {(() => {
+          if (tokenData.isDAT) {
+            const AssetIcon = getAssetIcon(tokenData.symbol)
+            return <AssetIcon className='h-6 w-6' />
+          }
+
+          const TokenIcon = getTokenIcon(tokenData.symbol)
+          return <TokenIcon className='h-6 w-6' />
+        })()}
+      </div>
+    </div>
+  )
+}
