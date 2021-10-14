209bdd4650ad549b4142110f44170975f3d6c039
Fuxing Loh <4266087+fuxingloh@users.noreply.github.com>
2021-10-14 20:08:58 +0800

    disable all DfTx parsing temporary (#397)

--- src/components/transactions/[txid]/TransactionDfTx.tsx
+++ src/components/transactions/[txid]/TransactionDfTx.tsx
@@ -1,34 +1,8 @@
 import { SmartBuffer } from 'smart-buffer'
-import {
-  CAccountToAccount,
-  CAnyAccountToAccount,
-  CPoolAddLiquidity,
-  CPoolRemoveLiquidity,
-  CPoolSwap,
-  CSetOracleData,
-  CUtxosToAccount,
-  CPoolCreatePair,
-  CTokenCreate,
-  OP_DEFI_TX,
-  CAccountToUtxos,
-  CCreateMasternode,
-  CResignMasternode,
-  toOPCodes
-} from '@defichain/jellyfish-transaction'
+import { CPoolSwap, OP_DEFI_TX, toOPCodes } from '@defichain/jellyfish-transaction'
 import { Transaction, TransactionVin, TransactionVout } from '@defichain/whale-api-client/dist/api/transactions'
 import { DfTxPoolSwap } from '@components/transactions/[txid]/DfTx/DfTxPoolSwap'
-import { DfTxUtxosToAccount } from '@components/transactions/[txid]/DfTx/DfTxUtxosToAccount'
-import { DfTxAccountToAccount } from '@components/transactions/[txid]/DfTx/DfTxAccountToAccount'
-import { DfTxAccountToUtxos } from '@components/transactions/[txid]/DfTx/DfTxAccountToUtxos'
-import { DfTxAnyAccountToAccount } from '@components/transactions/[txid]/DfTx/DfTxAnyAccountToAccount'
-import { DfTxPoolAddLiquidity } from '@components/transactions/[txid]/DfTx/DfTxPoolAddLiquidity'
-import { DfTxCreateMasternode } from '@components/transactions/[txid]/DfTx/DfTxCreateMasternode'
-import { DfTxResignMasternode } from '@components/transactions/[txid]/DfTx/DfTxResignMasternode'
 import { DfTxUnmapped } from '@components/transactions/[txid]/DfTx/DfTxUnmapped'
-import { DfTxPoolRemoveLiquidity } from '@components/transactions/[txid]/DfTx/DfTxPoolRemoveLiquidity'
-import { DfTxSetOracleData } from '@components/transactions/[txid]/DfTx/DfTxSetOracleData'
-import { DfTxPoolCreatePair } from '@components/transactions/[txid]/DfTx/DfTxPoolCreatePair'
-import { DfTxTokenCreate } from '@components/transactions/[txid]/DfTx/DfTxTokenCreate'
 
 interface TransactionDfTxProps {
   transaction: Transaction
@@ -49,28 +23,28 @@ export function TransactionDfTx (props: TransactionDfTxProps): JSX.Element | nul
   switch (tx.type) {
     case CPoolSwap.OP_CODE:
       return <DfTxPoolSwap dftx={tx} />
-    case CPoolAddLiquidity.OP_CODE:
-      return <DfTxPoolAddLiquidity dftx={tx} />
-    case CPoolRemoveLiquidity.OP_CODE:
-      return <DfTxPoolRemoveLiquidity dftx={tx} />
-    case CSetOracleData.OP_CODE:
-      return <DfTxSetOracleData dftx={tx} />
-    case CUtxosToAccount.OP_CODE:
-      return <DfTxUtxosToAccount dftx={tx} />
-    case CAccountToAccount.OP_CODE:
-      return <DfTxAccountToAccount dftx={tx} />
-    case CAnyAccountToAccount.OP_CODE:
-      return <DfTxAnyAccountToAccount dftx={tx} />
-    case CAccountToUtxos.OP_CODE:
-      return <DfTxAccountToUtxos dftx={tx} />
-    case CCreateMasternode.OP_CODE:
-      return <DfTxCreateMasternode dftx={tx} />
-    case CResignMasternode.OP_CODE:
-      return <DfTxResignMasternode dftx={tx} />
-    case CPoolCreatePair.OP_CODE:
-      return <DfTxPoolCreatePair dftx={tx} />
-    case CTokenCreate.OP_CODE:
-      return <DfTxTokenCreate dftx={tx} />
+    // case CPoolAddLiquidity.OP_CODE:
+    //   return <DfTxPoolAddLiquidity dftx={tx} />
+    // case CPoolRemoveLiquidity.OP_CODE:
+    //   return <DfTxPoolRemoveLiquidity dftx={tx} />
+    // case CSetOracleData.OP_CODE:
+    //   return <DfTxSetOracleData dftx={tx} />
+    // case CUtxosToAccount.OP_CODE:
+    //   return <DfTxUtxosToAccount dftx={tx} />
+    // case CAccountToAccount.OP_CODE:
+    //   return <DfTxAccountToAccount dftx={tx} />
+    // case CAnyAccountToAccount.OP_CODE:
+    //   return <DfTxAnyAccountToAccount dftx={tx} />
+    // case CAccountToUtxos.OP_CODE:
+    //   return <DfTxAccountToUtxos dftx={tx} />
+    // case CCreateMasternode.OP_CODE:
+    //   return <DfTxCreateMasternode dftx={tx} />
+    // case CResignMasternode.OP_CODE:
+    //   return <DfTxResignMasternode dftx={tx} />
+    // case CPoolCreatePair.OP_CODE:
+    //   return <DfTxPoolCreatePair dftx={tx} />
+    // case CTokenCreate.OP_CODE:
+    //   return <DfTxTokenCreate dftx={tx} />
     default:
       return <DfTxUnmapped dftx={tx} />
   }
