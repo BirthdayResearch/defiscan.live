e95f23ac75d9d96fc9569cce2072ffac16f7aac2
Joel-David Wong <git@joeldavid.dev>
2021-10-15 01:23:06 +0800

    Fixed DfTx AccountToUtxos

---
+++ cypress/integration/pages/transactions/DfTx/AccountToUtxos.ts
@@ -0,0 +1,67 @@
+context('/transactions/[txid] - DfTx account to utxos on desktop', () => {
+  before(() => {
+    cy.visit('/transactions/68440d960df49256d122110e6635edcfeda7658ddc9d1c5f53afd090c70536d9?network=MainNet')
+  })
+
+  beforeEach(() => {
+    cy.viewport('macbook-13')
+  })
+
+  it('should have heading', () => {
+    cy.findByTestId('DfTxHeader.Title').contains('DeFi Transaction')
+  })
+
+  it('should have DfTx type', () => {
+    cy.findByTestId('DfTxHeader.Subtitle').contains('Type:Account To Utxos')
+  })
+
+  it('should have DfTxAccountToUtxos fromAddress', () => {
+    cy.findByTestId('DfTxAccountToUtxos.fromAddress').should('have.text', 'dYpwUmq6z8jdYdpXecNGsN84iyyQyw5Syq')
+  })
+
+  it('should have DfTxAccountToUtxos balances', () => {
+    cy.findByTestId('DfTxAccountToUtxos.balances').should('have.text', '361.65527311')
+  })
+
+  it('should have DfTxAccountToUtxos symbol', () => {
+    cy.findByTestId('DfTxAccountToUtxos.symbol').should('have.text', 'DFI')
+  })
+
+  it('should have DfTxAccountToUtxos mintingOutputsStart', () => {
+    cy.findByTestId('DfTxAccountToUtxos.mintingOutputsStart').should('have.text', '2')
+  })
+})
+
+context('/transactions/[txid] - DfTx account to utxos on mobile', () => {
+  before(() => {
+    cy.visit('/transactions/68440d960df49256d122110e6635edcfeda7658ddc9d1c5f53afd090c70536d9?network=MainNet')
+  })
+
+  beforeEach(() => {
+    cy.viewport('iphone-x')
+  })
+
+  it('should have heading', () => {
+    cy.findByTestId('DfTxHeader.Title').contains('DeFi Transaction')
+  })
+
+  it('should have DfTx type', () => {
+    cy.findByTestId('DfTxHeader.Subtitle').contains('Type:Account To Utxos')
+  })
+
+  it('should have DfTxAccountToUtxos fromAddress', () => {
+    cy.findByTestId('DfTxAccountToUtxos.fromAddress').should('have.text', 'dYpwUmq6z8jdYdpXecNGsN84iyyQyw5Syq')
+  })
+
+  it('should have DfTxAccountToUtxos balances', () => {
+    cy.findByTestId('DfTxAccountToUtxos.balances').should('have.text', '361.65527311')
+  })
+
+  it('should have DfTxAccountToUtxos symbol', () => {
+    cy.findByTestId('DfTxAccountToUtxos.symbol').should('have.text', 'DFI')
+  })
+
+  it('should have DfTxAccountToUtxos mintingOutputsStart', () => {
+    cy.findByTestId('DfTxAccountToUtxos.mintingOutputsStart').should('have.text', '2')
+  })
+})
