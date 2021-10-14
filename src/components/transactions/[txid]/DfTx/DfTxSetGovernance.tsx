8861cbaffd357e5ee53fa1d1e1694ed648c9b0bf
Joel-David Wong <git@joeldavid.dev>
2021-10-14 19:09:27 +0800

    Updated SetGovernance

--- src/components/transactions/[txid]/DfTx/DfTxSetGovernance.tsx
+++ src/components/transactions/[txid]/DfTx/DfTxSetGovernance.tsx
@@ -1,6 +1,11 @@
-import { DfTx, GovernanceVar, LiqPoolSplit, SetGovernance } from '@defichain/jellyfish-transaction'
+import { DfTx, SetGovernance } from '@defichain/jellyfish-transaction'
 import { AdaptiveList } from '@components/commons/AdaptiveList'
 import { DfTxHeader } from '@components/transactions/[txid]/DfTx/DfTxHeader'
+import { OverflowTable } from '@components/commons/OverflowTable'
+import {
+  GovernanceLpDailyReward,
+  GovernanceLpSplits, GovernanceUnmapped
+} from '@defichain/jellyfish-transaction/dist/script/dftx/dftx_governance'
 
 interface DfTxSetGovernanceProps {
   dftx: DfTx<SetGovernance>
@@ -10,40 +15,86 @@ export function DfTxSetGovernance (props: DfTxSetGovernanceProps): JSX.Element {
   return (
     <div>
       <DfTxHeader name='Set Governance' />
-      <div className='mt-5 flex flex-col space-y-6 items-start lg:flex-row lg:space-x-8 lg:space-y-0'>
-        {props.dftx.data.governanceVars.map((gov, index) => (
-          <SetGovernanceTable governance={gov} key={index} />
-        ))}
+      <div className='mt-5'>
+        {props.dftx.data.governanceVars.map((gov, index) => {
+          switch (gov.key) {
+            case 'LP_DAILY_DFI_REWARD':
+              return (
+                <GovernanceLpDailyRewardTable
+                  governanceLpDailyReward={gov as GovernanceLpDailyReward}
+                  key={index}
+                />
+              )
+            case 'LP_SPLITS':
+              return <GovernanceLpSplitsTable governanceLpSplits={gov as GovernanceLpSplits} key={index} />
+            default:
+              return <GovernanceUnmappedTable governanceUnmapped={gov as GovernanceUnmapped} key={index} />
+          }
+        })}
       </div>
     </div>
   )
 }
 
-function SetGovernanceTable ({ governance }: { governance: GovernanceVar }): JSX.Element {
-  const liqPoolSplit = governance.value as LiqPoolSplit[]
-  const isLiqPoolSplit = typeof governance.value === 'object'
+function GovernanceLpDailyRewardTable (props: { governanceLpDailyReward: GovernanceLpDailyReward }): JSX.Element {
+  return (
+    <div className='flex flex-col space-y-6 items-start lg:flex-row lg:space-x-8 lg:space-y-0'>
+      <AdaptiveList className='w-full lg:w-1/2'>
+        <AdaptiveList.Row name='Governance Key' testId='DfTxSetGovernance.Key'>
+          {props.governanceLpDailyReward.key}
+        </AdaptiveList.Row>
+        <AdaptiveList.Row name='Value' testId='DfTxSetGovernance.Value'>
+          {props.governanceLpDailyReward.value.toFixed(8)}
+        </AdaptiveList.Row>
+      </AdaptiveList>
+    </div>
+  )
+}
+
+function GovernanceUnmappedTable (props: { governanceUnmapped: GovernanceUnmapped }): JSX.Element {
+  return (
+    <div className='flex flex-col space-y-6 items-start lg:flex-row lg:space-x-8 lg:space-y-0'>
+      <AdaptiveList className='w-full lg:w-1/2'>
+        <AdaptiveList.Row name='Governance Key' testId='DfTxSetGovernance.Key'>
+          {props.governanceUnmapped.key}
+        </AdaptiveList.Row>
+        <AdaptiveList.Row name='Value' testId='DfTxSetGovernance.Value'>
+          {props.governanceUnmapped.value}
+        </AdaptiveList.Row>
+      </AdaptiveList>
+    </div>
+  )
+}
+
+function GovernanceLpSplitsTable (props: { governanceLpSplits: GovernanceLpSplits }): JSX.Element {
   return (
     <>
-      <AdaptiveList className='w-full lg:w-1/2'>
-        <AdaptiveList.Row name=' Governance Key' testId='DfTxSetGovernance.Key'>
-          {governance.key}
-        </AdaptiveList.Row>
-        {isLiqPoolSplit && liqPoolSplit.map(value => (
-          <AdaptiveList.Row key={value.tokenId} name='Governance Token ID' testId={`DfTxSetGovernance.Token${value.tokenId}`}>
-            {value.tokenId}
+      <div className='flex flex-col space-y-6 items-start lg:flex-row lg:space-x-8 lg:space-y-0'>
+        <AdaptiveList className='w-full lg:w-1/2'>
+          <AdaptiveList.Row name='Governance Key' testId='DfTxSetGovernance.Key'>
+            {props.governanceLpSplits.key}
           </AdaptiveList.Row>
-        ))}
-      </AdaptiveList>
-      <AdaptiveList className='w-full lg:w-1/2'>
-        <AdaptiveList.Row name='Governance Value'>
-          {isLiqPoolSplit ? 'N/A' : governance.value.toString()}
-        </AdaptiveList.Row>
-        {isLiqPoolSplit && liqPoolSplit.map(value => (
-          <AdaptiveList.Row key={value.tokenId} name={`Governance Token ${value.tokenId} Value`} testId={`DfTxSetGovernance.Token${value.tokenId}Value`}>
-            {value.value.toFixed(8)}
-          </AdaptiveList.Row>
-        ))}
-      </AdaptiveList>
+        </AdaptiveList>
+      </div>
+      <div className='w-full lg:w-1/2 mt-4'>
+        <OverflowTable>
+          <OverflowTable.Header>
+            <OverflowTable.Head>Governance Token ID</OverflowTable.Head>
+            <OverflowTable.Head>Value</OverflowTable.Head>
+          </OverflowTable.Header>
+          {props.governanceLpSplits.value.map(value => (
+            <OverflowTable.Row key={value.tokenId}>
+              <OverflowTable.Cell>
+                <span data-testId={`DfTxSetGovernance.Token${value.tokenId}`}>{value.tokenId}</span>
+              </OverflowTable.Cell>
+              <OverflowTable.Cell>
+                <span data-testId={`DfTxSetGovernance.Token${value.tokenId}Value`}>{value.value.toFixed(8)}</span>
+              </OverflowTable.Cell>
+            </OverflowTable.Row>
+          )
+          )}
+        </OverflowTable>
+      </div>
     </>
   )
 }
