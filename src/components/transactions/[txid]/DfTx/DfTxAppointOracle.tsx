26c130c75d1a16e85e8a6c23b4d8f91d76cfc01e
Joel-David Wong <git@joeldavid.dev>
2021-10-14 19:16:50 +0800

    Updated AppointOracle UI

--- src/components/transactions/[txid]/DfTx/DfTxAppointOracle.tsx
+++ src/components/transactions/[txid]/DfTx/DfTxAppointOracle.tsx
@@ -4,6 +4,7 @@ import { useNetworkObject } from '@contexts/NetworkContext'
 
 import { DfTxHeader } from '@components/transactions/[txid]/DfTx/DfTxHeader'
 import { AdaptiveList } from '@components/commons/AdaptiveList'
+import { OverflowTable } from '@components/commons/OverflowTable'
 
 interface DfTxAppointOracleProps {
   dftx: DfTx<AppointOracle>
@@ -21,7 +22,7 @@ export function DfTxAppointOracle (props: DfTxAppointOracleProps): JSX.Element {
   return (
     <div>
       <DfTxHeader name='Appoint Oracle' />
-      <div className='mt-5 flex flex-col space-y-6 items-start lg:flex-row lg:space-x-8 lg:space-y-0'>
+      <div className='mt-5'>
         <AppointOracleTable
           address={script?.address}
           weightage={props.dftx.data.weightage}
@@ -32,29 +33,44 @@ export function DfTxAppointOracle (props: DfTxAppointOracleProps): JSX.Element {
   )
 }
 
-function AppointOracleTable ({ address, priceFeeds, weightage }: AppointOracleProps): JSX.Element {
+function AppointOracleTable ({
+  address,
+  priceFeeds,
+  weightage
+}: AppointOracleProps): JSX.Element {
   return (
     <>
-      <AdaptiveList className='w-full lg:w-1/2'>
-        <AdaptiveList.Row name='Address' testId='DfTxAppointOracle.Address' className='break-all'>
-          {address ?? 'N/A'}
-        </AdaptiveList.Row>
-        {priceFeeds.map((feed, index) => (
-          <AdaptiveList.Row name='PriceFeed Token' key={index} testId={`DfTxAppointOracle.${feed.token}Token`}>
-            {feed.token}
+      <div className='flex flex-col space-y-6 items-start lg:flex-row lg:space-x-8 lg:space-y-0'>
+        <AdaptiveList className='w-full lg:w-1/2'>
+          <AdaptiveList.Row name='Address' testId='DfTxAppointOracle.Address' className='break-all'>
+            {address ?? 'N/A'}
           </AdaptiveList.Row>
-        ))}
-      </AdaptiveList>
-      <AdaptiveList className='w-full lg:w-1/2'>
-        <AdaptiveList.Row name='Weightage' testId='DfTxAppointOracle.Weightage'>
-          {weightage}
-        </AdaptiveList.Row>
-        {priceFeeds.map((feed, index) => (
-          <AdaptiveList.Row name='PriceFeed Currency' key={index} testId={`DfTxAppointOracle.${feed.token}Currency`}>
-            {feed.currency}
+        </AdaptiveList>
+        <AdaptiveList className='w-full lg:w-1/2'>
+          <AdaptiveList.Row name='Weightage' testId='DfTxAppointOracle.Weightage'>
+            {weightage}
           </AdaptiveList.Row>
-        ))}
-      </AdaptiveList>
+        </AdaptiveList>
+      </div>
+      <div className='w-full lg:w-1/2 mt-4'>
+        <OverflowTable>
+          <OverflowTable.Header>
+            <OverflowTable.Head>Token</OverflowTable.Head>
+            <OverflowTable.Head>Currency</OverflowTable.Head>
+          </OverflowTable.Header>
+          {priceFeeds.map((feed, index) => (
+            <OverflowTable.Row key={index}>
+              <OverflowTable.Cell>
+                <span data-testId={`DfTxAppointOracle.${feed.token}Token`}>{feed.token}</span>
+              </OverflowTable.Cell>
+              <OverflowTable.Cell>
+                <span data-testId={`DfTxAppointOracle.${feed.token}Currency`}>{feed.currency}</span>
+              </OverflowTable.Cell>
+            </OverflowTable.Row>
+          )
+          )}
+        </OverflowTable>
+      </div>
     </>
   )
 }
