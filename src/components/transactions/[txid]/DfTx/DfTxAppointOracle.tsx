1cc66c5cb9eedc42f7174b51af1035453c59dd5d
Joel-David Wong <git@joeldavid.dev>
2021-10-18 19:03:02 +0800

    Updated Appoint Oracle

--- src/components/transactions/[txid]/DfTx/DfTxAppointOracle.tsx
+++ src/components/transactions/[txid]/DfTx/DfTxAppointOracle.tsx
@@ -10,12 +10,6 @@ interface DfTxAppointOracleProps {
   dftx: DfTx<AppointOracle>
 }
 
-interface AppointOracleProps {
-  address: string | undefined
-  weightage: number
-  priceFeeds: CurrencyPair[]
-}
-
 export function DfTxAppointOracle (props: DfTxAppointOracleProps): JSX.Element {
   const network = useNetworkObject().name
   const script = fromScript(props.dftx.data.script, network)
@@ -23,54 +17,42 @@ export function DfTxAppointOracle (props: DfTxAppointOracleProps): JSX.Element {
     <div>
       <DfTxHeader name='Appoint Oracle' />
       <div className='mt-5'>
-        <AppointOracleTable
-          address={script?.address}
-          weightage={props.dftx.data.weightage}
-          priceFeeds={props.dftx.data.priceFeeds}
-        />
+        <div className='flex flex-col space-y-6 items-start lg:flex-row lg:space-x-8 lg:space-y-0'>
+          <AdaptiveList className='w-full lg:w-1/2'>
+            <AdaptiveList.Row name='Address' testId='DfTxAppointOracle.Address' className='break-all'>
+              {script?.address ?? 'N/A'}
+            </AdaptiveList.Row>
+            <AdaptiveList.Row name='Weightage' testId='DfTxAppointOracle.Weightage'>
+              {props.dftx.data.weightage}
+            </AdaptiveList.Row>
+          </AdaptiveList>
+          <div className='w-full lg:w-1/2'>
+            <OverflowTable>
+              <OverflowTable.Header>
+                <OverflowTable.Head>Token</OverflowTable.Head>
+                <OverflowTable.Head>Currency</OverflowTable.Head>
+              </OverflowTable.Header>
+              {props.dftx.data.priceFeeds.map((priceFeed) => (
+                <AppointOracleRow priceFeed={priceFeed} key={`${priceFeed.token}-${priceFeed.currency}`} />
+              )
+              )}
+            </OverflowTable>
+          </div>
+        </div>
       </div>
     </div>
   )
 }
 
-function AppointOracleTable ({
-  address,
-  priceFeeds,
-  weightage
-}: AppointOracleProps): JSX.Element {
+function AppointOracleRow (props: { priceFeed: CurrencyPair }): JSX.Element {
   return (
-    <>
-      <div className='flex flex-col space-y-6 items-start lg:flex-row lg:space-x-8 lg:space-y-0'>
-        <AdaptiveList className='w-full lg:w-1/2'>
-          <AdaptiveList.Row name='Address' testId='DfTxAppointOracle.Address' className='break-all'>
-            {address ?? 'N/A'}
-          </AdaptiveList.Row>
-        </AdaptiveList>
-        <AdaptiveList className='w-full lg:w-1/2'>
-          <AdaptiveList.Row name='Weightage' testId='DfTxAppointOracle.Weightage'>
-            {weightage}
-          </AdaptiveList.Row>
-        </AdaptiveList>
-      </div>
-      <div className='w-full lg:w-1/2 mt-4'>
-        <OverflowTable>
-          <OverflowTable.Header>
-            <OverflowTable.Head>Token</OverflowTable.Head>
-            <OverflowTable.Head>Currency</OverflowTable.Head>
-          </OverflowTable.Header>
-          {priceFeeds.map((feed, index) => (
-            <OverflowTable.Row key={index}>
-              <OverflowTable.Cell>
-                <span data-testId={`DfTxAppointOracle.${feed.token}Token`}>{feed.token}</span>
-              </OverflowTable.Cell>
-              <OverflowTable.Cell>
-                <span data-testId={`DfTxAppointOracle.${feed.token}Currency`}>{feed.currency}</span>
-              </OverflowTable.Cell>
-            </OverflowTable.Row>
-          )
-          )}
-        </OverflowTable>
-      </div>
-    </>
+    <OverflowTable.Row>
+      <OverflowTable.Cell>
+        <span data-testId={`DfTxAppointOracle.${props.priceFeed.token}Token`}>{props.priceFeed.token}</span>
+      </OverflowTable.Cell>
+      <OverflowTable.Cell>
+        <span data-testId={`DfTxAppointOracle.${props.priceFeed.token}Currency`}>{props.priceFeed.currency}</span>
+      </OverflowTable.Cell>
+    </OverflowTable.Row>
   )
 }
