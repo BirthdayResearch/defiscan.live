5ee536e33bc4ff2ae0b523b803a1cefad768a50f
Joel-David Wong <git@joeldavid.dev>
2021-10-19 21:35:39 +0800

    Updated ICXMakeOffer

--- cypress/integration/pages/transactions/DfTx/ICXMakeOffer.spec.ts
+++ cypress/integration/pages/transactions/DfTx/ICXMakeOffer.spec.ts
@@ -15,20 +15,28 @@ context('/transactions/[txid] - DfTx ICX Make Offer on desktop', () => {
     cy.findByTestId('DfTxHeader.Subtitle').contains('Type:ICX Make Offer')
   })
 
+  it('should have DfTxICXMakeOffer OrderTx', () => {
+    cy.findByTestId('DfTxICXMakeOffer.OrderTx').should('have.text', '17bf3dacb6bb5227b3393e63eeadd89a855a99d67efe9d3b4f99ed47c221ae54')
+  })
+
   it('should have DfTxICXMakeOffer amount', () => {
     cy.findByTestId('DfTxICXMakeOffer.Amount').should('have.text', '0.10000000')
   })
 
-  it('should have DfTxICXMakeOffer OrderTx', () => {
-    cy.findByTestId('DfTxICXMakeOffer.OrderTx').should('have.text', '17bf3dacb6bb5227b3393e63eeadd89a855a99d67efe9d3b4f99ed47c221ae54')
+  it('should have DfTxICXMakeOffer TakerFee', () => {
+    cy.findByTestId('DfTxICXMakeOffer.TakerFee').should('have.text', '0.00000000')
+  })
+
+  it('should have DfTxICXMakeOffer Expiry', () => {
+    cy.findByTestId('DfTxICXMakeOffer.Expiry').should('have.text', '20 Blocks')
   })
 
   it('should have DfTxICXMakeOffer OwnerAddress', () => {
     cy.findByTestId('DfTxICXMakeOffer.OwnerAddress').should('have.text', 'dHWPd6DtartruFfEugTgWc8FaB1syM4MD9')
   })
 
-  it('should have DfTxICXMakeOffer TakerFee', () => {
-    cy.findByTestId('DfTxICXMakeOffer.TakerFee').should('have.text', '0')
+  it('should have DfTxICXMakeOffer Pubkey', () => {
+    cy.findByTestId('DfTxICXMakeOffer.Pubkey').should('have.text', 'N/A')
   })
 })
 
@@ -49,19 +57,27 @@ context('/transactions/[txid] - DfTx ICX Make Offer on mobile', () => {
     cy.findByTestId('DfTxHeader.Subtitle').contains('Type:ICX Make Offer')
   })
 
+  it('should have DfTxICXMakeOffer OrderTx', () => {
+    cy.findByTestId('DfTxICXMakeOffer.OrderTx').should('have.text', '17bf3dacb6bb5227b3393e63eeadd89a855a99d67efe9d3b4f99ed47c221ae54')
+  })
+
   it('should have DfTxICXMakeOffer amount', () => {
     cy.findByTestId('DfTxICXMakeOffer.Amount').should('have.text', '0.10000000')
   })
 
-  it('should have DfTxICXMakeOffer OrderTx', () => {
-    cy.findByTestId('DfTxICXMakeOffer.OrderTx').should('have.text', '17bf3dacb6bb5227b3393e63eeadd89a855a99d67efe9d3b4f99ed47c221ae54')
+  it('should have DfTxICXMakeOffer TakerFee', () => {
+    cy.findByTestId('DfTxICXMakeOffer.TakerFee').should('have.text', '0.00000000')
+  })
+
+  it('should have DfTxICXMakeOffer Expiry', () => {
+    cy.findByTestId('DfTxICXMakeOffer.Expiry').should('have.text', '20 Blocks')
   })
 
   it('should have DfTxICXMakeOffer OwnerAddress', () => {
     cy.findByTestId('DfTxICXMakeOffer.OwnerAddress').should('have.text', 'dHWPd6DtartruFfEugTgWc8FaB1syM4MD9')
   })
 
-  it('should have DfTxICXMakeOffer TakerFee', () => {
-    cy.findByTestId('DfTxICXMakeOffer.TakerFee').should('have.text', '0')
+  it('should have DfTxICXMakeOffer Pubkey', () => {
+    cy.findByTestId('DfTxICXMakeOffer.Pubkey').should('have.text', 'N/A')
   })
 })
--- src/components/transactions/[txid]/DfTx/DfTxICXMakeOffer.tsx
+++ src/components/transactions/[txid]/DfTx/DfTxICXMakeOffer.tsx
@@ -16,21 +16,32 @@ export function DfTxICXMakeOffer (props: DfTxICXMakeOfferProps): JSX.Element {
     <div>
       <DfTxHeader name='ICX Make Offer' />
       <div className='mt-5 flex flex-col space-y-6 items-start lg:flex-row lg:space-x-8 lg:space-y-0'>
-        <AdaptiveList>
-          <AdaptiveList.Row name='Amount'>
-            <span data-testid='DfTxICXMakeOffer.Amount'>{props.dftx.data.amount.toFixed(8)}</span>
-          </AdaptiveList.Row>
-          <AdaptiveList.Row name='Order Tx'>
-            <span data-testid='DfTxICXMakeOffer.OrderTx'>{props.dftx.data.orderTx}</span>
-          </AdaptiveList.Row>
-          <AdaptiveList.Row name='Owner Address'>
-            <span data-testid='DfTxICXMakeOffer.OwnerAddress'>{address?.address}</span>
-          </AdaptiveList.Row>
-          <AdaptiveList.Row name='Taker Fee'>
-            <span data-testid='DfTxICXMakeOffer.TakerFee'>{props.dftx.data.takerFee.toString()}</span>
-          </AdaptiveList.Row>
-        </AdaptiveList>
-
+        <div className='w-full lg:w-1/2'>
+          <AdaptiveList>
+            <AdaptiveList.Row name='Order Tx'>
+              <span data-testid='DfTxICXMakeOffer.OrderTx'>{props.dftx.data.orderTx}</span>
+            </AdaptiveList.Row>
+            <AdaptiveList.Row name='Amount'>
+              <span data-testid='DfTxICXMakeOffer.Amount'>{props.dftx.data.amount.toFixed(8)}</span>
+            </AdaptiveList.Row>
+            <AdaptiveList.Row name='Taker Fee'>
+              <span data-testid='DfTxICXMakeOffer.TakerFee'>{props.dftx.data.takerFee.toFixed(8)}</span>
+            </AdaptiveList.Row>
+            <AdaptiveList.Row name='Expiry'>
+              <span data-testid='DfTxICXMakeOffer.Expiry'>{`${props.dftx.data.expiry} Blocks`}</span>
+            </AdaptiveList.Row>
+          </AdaptiveList>
+        </div>
+        <div className='w-full lg:w-1/2'>
+          <AdaptiveList>
+            <AdaptiveList.Row name='Owner Address'>
+              <span data-testid='DfTxICXMakeOffer.OwnerAddress'>{address?.address ?? 'N/A'}</span>
+            </AdaptiveList.Row>
+            <AdaptiveList.Row name='Pubkey'>
+              <span data-testid='DfTxICXMakeOffer.Pubkey'>{props.dftx.data.receivePubkey ?? 'N/A'}</span>
+            </AdaptiveList.Row>
+          </AdaptiveList>
+        </div>
       </div>
     </div>
   )
