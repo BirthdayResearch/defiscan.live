a3e519daa894d4443c86d15147976205677d9702
Joel-David Wong <4586209+joeldavidw@users.noreply.github.com>
2021-10-20 22:16:48 +0800

    Added search bar to `homepage` (#430)
    * Added searchbar, server sided redirection and tests
    
    * Renamed searchTerm to query, move SearchBar into a function and resolved other suggestions

---
+++ cypress/integration/pages/search/[searchTerm].spec.ts
@@ -0,0 +1,67 @@
+context('search macbook-13', () => {
+  before(function () {
+    cy.visit('/?network=MainNet')
+  })
+
+  beforeEach(() => {
+    cy.viewport('macbook-13')
+  })
+
+  it('should have IndexHeader.SearchInput', () => {
+    cy.findByTestId('IndexHeader.SearchInput').should('be.visible')
+  })
+
+  it('should show no result alert', () => {
+    cy.findByTestId('IndexHeader.SearchInput').type('asd123asd321').type('{enter}')
+    cy.findByTestId('SearchPage.NoResults').should('contain.text', 'The requested search term asd123asd321 could not be found')
+  })
+
+  it('should redirect to transaction', () => {
+    cy.visit('/search/2b260d10f92a7416168112ffcf74942c13ac80894a5d4bde391def174bf97dac')
+    cy.url().should('contain', '/transactions/2b260d10f92a7416168112ffcf74942c13ac80894a5d4bde391def174bf97dac')
+  })
+
+  it('should redirect to block - hash', () => {
+    cy.visit('/search/def78eabef69c249ffac8d4fe5bbd18f8d74c6fd95e856794e0bd5052e2286b4')
+    cy.url().should('contain', '/blocks/def78eabef69c249ffac8d4fe5bbd18f8d74c6fd95e856794e0bd5052e2286b4')
+  })
+
+  it('should redirect to block - height', () => {
+    cy.visit('/search/1292821')
+    cy.url().should('contain', '/blocks/def78eabef69c249ffac8d4fe5bbd18f8d74c6fd95e856794e0bd5052e2286b4')
+  })
+})
+
+context('/prices/TSLA-USD iphone-x', () => {
+  before(function () {
+    cy.visit('/?network=MainNet')
+  })
+
+  beforeEach(() => {
+    cy.viewport('iphone-x')
+  })
+
+  it('should have IndexHeader.SearchInput', () => {
+    cy.findByTestId('IndexHeader.SearchInput').should('be.visible')
+  })
+
+  it('should show no result alert', () => {
+    cy.findByTestId('IndexHeader.SearchInput').type('asd123asd321').type('{enter}')
+    cy.findByTestId('SearchPage.NoResults').should('contain.text', 'The requested search term asd123asd321 could not be found')
+  })
+
+  it('should redirect to transaction', () => {
+    cy.visit('/search/2b260d10f92a7416168112ffcf74942c13ac80894a5d4bde391def174bf97dac')
+    cy.url().should('contain', '/transactions/2b260d10f92a7416168112ffcf74942c13ac80894a5d4bde391def174bf97dac')
+  })
+
+  it('should redirect to block - hash', () => {
+    cy.visit('/search/def78eabef69c249ffac8d4fe5bbd18f8d74c6fd95e856794e0bd5052e2286b4')
+    cy.url().should('contain', '/blocks/def78eabef69c249ffac8d4fe5bbd18f8d74c6fd95e856794e0bd5052e2286b4')
+  })
+
+  it('should redirect to block - height', () => {
+    cy.visit('/search/1292821')
+    cy.url().should('contain', '/blocks/def78eabef69c249ffac8d4fe5bbd18f8d74c6fd95e856794e0bd5052e2286b4')
+  })
+})
---
+++ src/pages/search/[query].tsx
@@ -0,0 +1,77 @@
+import { GetServerSidePropsContext, GetServerSidePropsResult, InferGetServerSidePropsType } from 'next'
+import { Container } from '@components/commons/Container'
+import { getWhaleApiClient } from '@contexts/WhaleContext'
+import { WhaleApiClient } from '@defichain/whale-api-client'
+
+interface SearchProps {
+  query: string
+}
+
+export default function SearchPage (props: InferGetServerSidePropsType<typeof getServerSideProps>): JSX.Element {
+  return (
+    <Container className='pt-12 pb-20'>
+      <div className='flex flex-wrap justify-center pt-24 pb-36'>
+        <div className='rounded text-center w-auto bg-red-100 px-20 py-4' data-testid='SearchPage.NoResults'>
+          The requested search term <code className='break-all'>{props.query}</code> could not be found
+        </div>
+      </div>
+    </Container>
+  )
+}
+
+export async function getServerSideProps (context: GetServerSidePropsContext): Promise<GetServerSidePropsResult<SearchProps>> {
+  const api = getWhaleApiClient(context)
+  const query = context.params?.query?.toString().trim() as string
+
+  const results = await search(api, query)
+
+  if (results === undefined) {
+    return {
+      props: {
+        query: query
+      }
+    }
+  }
+
+  return {
+    redirect: {
+      statusCode: 302,
+      destination: `${results.url}`
+    }
+  }
+}
+
+async function search (api: WhaleApiClient, query: string): Promise<{ id: string, url: string } | undefined> {
+  query = query.trim()
+
+  const txnData = await api.transactions.get(query)
+    .then(data => {
+      return {
+        url: `/transactions/${data.txid}`,
+        id: data.txid
+      }
+    })
+    .catch(() => {
+      return undefined
+    })
+
+  if (txnData !== undefined) {
+    return txnData
+  }
+
+  const blocksData = await api.blocks.get(query)
+    .then(data => {
+      return {
+        url: `/blocks/${data.id}`,
+        id: data.id
+      }
+    }).catch(() => {
+      return undefined
+    })
+
+  if (blocksData !== undefined) {
+    return blocksData
+  }
+
+  return undefined
+}
