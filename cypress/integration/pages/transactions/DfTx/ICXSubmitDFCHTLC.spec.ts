6a101b6582ddc34487b0dd835de34d74e5daf094
Joel-David Wong <git@joeldavid.dev>
2021-10-20 22:27:02 +0800

    Updated ICXSubmitDFCHTLC

--- cypress/integration/pages/transactions/DfTx/ICXSubmitDFCHTLC.spec.ts
+++ cypress/integration/pages/transactions/DfTx/ICXSubmitDFCHTLC.spec.ts
@@ -19,12 +19,16 @@ context('/transactions/[txid] - DfTx ICX Submit DFCHTLC  on desktop', () => {
     cy.findByTestId('DfTxICXSubmitDFCHTLC.OfferTx').should('have.text', '3e0726e7c5a970f8670559d51f9e4ca119d5a22bc2405ae2ff092596929290ac')
   })
 
-  it('should have DfTxICXSubmitDFCHTLC Amount', () => {
-    cy.findByTestId('DfTxICXSubmitDFCHTLC.Amount').should('have.text', '0.1')
-  })
-
   it('should have DfTxICXSubmitDFCHTLC Hash', () => {
     cy.findByTestId('DfTxICXSubmitDFCHTLC.Hash').should('have.text', '664e63c049e09db655a280069f668ccd992d44d44b271b79cd586b6ed0e75f17')
+  })
+
+  it('should have DfTxICXSubmitDFCHTLC Amount', () => {
+    cy.findByTestId('DfTxICXSubmitDFCHTLC.Amount').should('have.text', '0.10000000')
+  })
+
+  it('should have DfTxICXSubmitDFCHTLC Timeout', () => {
+    cy.findByTestId('DfTxICXSubmitDFCHTLC.Timeout').should('have.text', '1440 Blocks')
   })
 })
 
@@ -49,11 +53,15 @@ context('/transactions/[txid] - DfTx ICX Submit DFCHTLC  on mobile', () => {
     cy.findByTestId('DfTxICXSubmitDFCHTLC.OfferTx').should('have.text', '3e0726e7c5a970f8670559d51f9e4ca119d5a22bc2405ae2ff092596929290ac')
   })
 
-  it('should have DfTxICXSubmitDFCHTLC Amount', () => {
-    cy.findByTestId('DfTxICXSubmitDFCHTLC.Amount').should('have.text', '0.1')
-  })
-
   it('should have DfTxICXSubmitDFCHTLC Hash', () => {
     cy.findByTestId('DfTxICXSubmitDFCHTLC.Hash').should('have.text', '664e63c049e09db655a280069f668ccd992d44d44b271b79cd586b6ed0e75f17')
+  })
+
+  it('should have DfTxICXSubmitDFCHTLC Amount', () => {
+    cy.findByTestId('DfTxICXSubmitDFCHTLC.Amount').should('have.text', '0.10000000')
+  })
+
+  it('should have DfTxICXSubmitDFCHTLC Timeout', () => {
+    cy.findByTestId('DfTxICXSubmitDFCHTLC.Timeout').should('have.text', '1440 Blocks')
   })
 })
