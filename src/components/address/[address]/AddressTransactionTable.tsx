aeedf121a39d735b5c4c9db560a1fe503f657b68
Joel-David Wong <git@joeldavid.dev>
2021-10-29 23:01:32 +0800

    AddressTransactionTable clean up

--- src/components/address/[address]/AddressTransactionTable.tsx
+++ src/components/address/[address]/AddressTransactionTable.tsx
@@ -48,15 +48,15 @@ export function AddressTransactionTable (props: AddressTransactionTableProps): J
   return (
     <div className='mt-6 flex flex-wrap' data-testid='Transactions'>
       <span className='font-medium text-xl mb-2 text-gray-800' data-testid='Transactions.title'>Transactions</span>
-      <OverflowTable className='w-full'>
-        <OverflowTable.Header>
-          <OverflowTable.Head>TX ID</OverflowTable.Head>
-          <OverflowTable.Head>BLOCK</OverflowTable.Head>
-          <OverflowTable.Head>AGE</OverflowTable.Head>
-          <OverflowTable.Head className='text-right'>AMOUNT</OverflowTable.Head>
-        </OverflowTable.Header>
-        {transactionData.length > 0 ? (
-          transactionData.map((addressActivity) => {
+      {transactionData.length > 0 ? (
+        <OverflowTable className='w-full'>
+          <OverflowTable.Header>
+            <OverflowTable.Head>TX ID</OverflowTable.Head>
+            <OverflowTable.Head>BLOCK</OverflowTable.Head>
+            <OverflowTable.Head>AGE</OverflowTable.Head>
+            <OverflowTable.Head className='text-right'>AMOUNT</OverflowTable.Head>
+          </OverflowTable.Header>
+          {transactionData.map((addressActivity) => {
             return (
               <AddressTransactionTableRow
                 address={props.address}
@@ -64,40 +64,49 @@ export function AddressTransactionTable (props: AddressTransactionTableProps): J
                 key={`${addressActivity.id}-${addressActivity.type}`}
               />
             )
-          }))
-          : (<NoTransactionsRow />)}
-      </OverflowTable>
-      {next !== undefined && (
-        !isLoading ? (
-          <div
-            className='flex w-full justify-center mt-4' onClick={getTransactions}
-            data-testid='AddressTransactionTable.showMoreBtn'
-          >
-            <button
-              type='button'
-              className='w-full md:w-1/3 py-2.5 text-primary-400 hover:text-primary-500 border border-primary-200 hover:border-primary-500'
-            >
-              SHOW MORE
-            </button>
-          </div>
-        ) : (
-          <div className='flex w-full justify-center mt-4'>
-            <div className='flex justify-center pt-2 pb-4'>
-              <CgSpinner size={32} className='animate-spin text-gray-600' />
-            </div>
-          </div>
-        )
-      )}
+          })}
+        </OverflowTable>
+      )
+        : (<NoTransactionsInfo />)}
+      <ShowMoreButton isLoading={isLoading} next={next} handleGetTransactions={getTransactions} />
     </div>
   )
 }
 
-function NoTransactionsRow (): JSX.Element {
-  return (
-    <td colSpan={4}>
-      <div className='flex justify-center p-4'>
-        <span>No Transactions</span>
+function ShowMoreButton (props: { isLoading: boolean, next?: string, handleGetTransactions: () => void }): JSX.Element {
+  if (props.next === undefined) {
+    return <></>
+  }
+
+  if (props.isLoading) {
+    return (
+      <div className='flex w-full justify-center mt-4'>
+        <div className='flex justify-center pt-2 pb-4'>
+          <CgSpinner size={32} className='animate-spin text-gray-600' />
+        </div>
       </div>
-    </td>
+    )
+  }
+
+  return (
+    <div
+      className='flex w-full justify-center mt-4' onClick={props.handleGetTransactions}
+      data-testid='AddressTransactionTable.showMoreBtn'
+    >
+      <button
+        type='button'
+        className='w-full md:w-1/3 py-2.5 text-primary-300 hover:text-primary-500 border border-primary-200 hover:border-primary-500 rounded'
+      >
+        SHOW MORE
+      </button>
+    </div>
+  )
+}
+
+function NoTransactionsInfo (): JSX.Element {
+  return (
+    <div className='flex w-full h-40 items-center justify-center rounded p-4 border border-gray-200'>
+      <span>No Transactions</span>
+    </div>
   )
 }
