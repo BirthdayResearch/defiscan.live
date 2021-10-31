eda79cfb9e05575b17e9963d14c5b4b059be6ecf
Suraj Auwal <siradjiawoual@gmail.com>
2021-10-31 23:15:06 +0100

    TxIdLink component

---
+++ src/components/commons/transactions/TxIdLink.tsx
@@ -0,0 +1,12 @@
+import { Link } from '@components/commons/Link'
+import classnames from 'classnames'
+
+export function TxIdLink ({ txid, className }: {txid: string, className?: string}): JSX.Element {
+  return (
+    <div className={classnames('hover:text-primary-500 cursor-pointer', className ?? '')}>
+      <Link href={{ pathname: `/transactions/${txid}` }}>
+        {txid}
+      </Link>
+    </div>
+  )
+}
