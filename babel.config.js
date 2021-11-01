de9da50daefa36e3a052070abd2c3f1db8f2dceb
Joel-David Wong <git@joeldavid.dev>
2021-11-01 14:47:13 +0800

    Strip data-testid from prod build

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,13 @@
 module.exports = function (api) {
   api.cache(true)
 
-  const plugins = []
-  if (process.env.ISTANBUL === 'true') {
+  let plugins = ['babel-plugin-jsx-remove-data-test-id']
+  if (process.env.CYPRESS === 'true') {
+    let removePlugins = ['babel-plugin-jsx-remove-data-test-id']
     console.log('Instrumenting for Cypress E2E')
     plugins.push('istanbul')
+
+    plugins = plugins.filter(plugin => !removePlugins.includes(plugin))
   }
 
   return {
