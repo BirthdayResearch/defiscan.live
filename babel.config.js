4a87b10b2af78010538d330f2a9f02194625d6de
Joel-David Wong <git@joeldavid.dev>
2021-11-01 20:32:33 +0800

    Updated babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,17 +1,17 @@
 module.exports = function (api) {
   api.cache(true)
 
-  let plugins = ['babel-plugin-jsx-remove-data-test-id']
   if (process.env.CYPRESS === 'true') {
-    let removePlugins = ['babel-plugin-jsx-remove-data-test-id']
     console.log('Instrumenting for Cypress E2E')
-    plugins.push('istanbul')
 
-    plugins = plugins.filter(plugin => !removePlugins.includes(plugin))
+    return {
+      presets: ['next/babel'],
+      plugins: ['istanbul']
+    }
   }
 
   return {
     presets: ['next/babel'],
-    plugins: plugins
+    plugins: ['babel-plugin-jsx-remove-data-test-id']
   }
 }
