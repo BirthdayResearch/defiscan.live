c4874fdca6285340813981ae857f33f660666428
Joel-David Wong <git@joeldavid.dev>
2021-11-03 12:05:15 +0800

    Fixed babel config for dev

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,15 @@
 module.exports = function (api) {
   api.cache(true)
 
+  if (process.env.NODE_ENV === 'development') {
+    console.log('Instrumenting for Development')
+
+    return {
+      presets: ['next/babel'],
+      plugins: []
+    }
+  }
+
   if (process.env.CYPRESS === 'true') {
     console.log('Instrumenting for Cypress E2E')
 
