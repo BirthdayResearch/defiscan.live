960adb8d8448c8c96cd327f29367400d4b040846
Fuxing Loh <git@fuxing.dev>
2021-11-05 07:28:27 +0800

    added direct connection to RPC within cypress testing for scan

--- cypress/integration/pages/vaults/index.spec.ts
+++ cypress/integration/pages/vaults/index.spec.ts
@@ -1,6 +1,5 @@
 import { PlaygroundApiClient, PlaygroundRpcClient } from '@defichain/playground-api-client'
-import BigNumber from 'bignumber.js'
-import { RegTestGenesisKeys } from '@defichain/jellyfish-network/dist/RegTestFoundationKeys'
+import { wait } from 'next/dist/build/output/log'
 
 const api = new PlaygroundApiClient({
   url: 'http://localhost:19553'
@@ -8,35 +7,23 @@ const api = new PlaygroundApiClient({
 const rpc = new PlaygroundRpcClient(api)
 
 context('/vaults', () => {
-  before(async () => {
-    await rpc.loan.setCollateralToken({
-      token: 'DFI',
-      factor: new BigNumber(1),
-      fixedIntervalPriceId: 'DFI/USD'
-    })
-
-    const sourceAddress = RegTestGenesisKeys[0].operator.address
-    const address = await rpc.wallet.getNewAddress()
-
-    const vaultId = await rpc.loan.createVault({
-      loanSchemeId: '1',
-      ownerAddress: address
-    })
-    await rpc.call('waitfornewblock', [], 'number')
-
-    await rpc.loan.depositToVault({
-      vaultId: vaultId,
-      from: sourceAddress,
-      amount: '10@DFI'
-    })
-
-  })
-
   beforeEach(() => {
     cy.viewport('macbook-13')
   })
 
   it('should have vaultId', async () => {
+    let vaultId
+    cy.wrap(null).then(async () => {
+      const address = await rpc.wallet.getNewAddress()
+      vaultId = await rpc.loan.createVault({
+        loanSchemeId: '1',
+        ownerAddress: address
+      })
+      // this method isn't mapped yet, so we are calling it raw via `rpc.call`
+      await rpc.call('waitfornewblock', [], 'number')
+      return vaultId
+    })
+
     cy.visit('/vaults?network=Local')
     // cy.findByTestId('TEST_JSON').contains('ownerAddress')
   })
--- src/pages/vaults/index.tsx
+++ src/pages/vaults/index.tsx
@@ -1,165 +1,29 @@
-import { Head } from '@components/commons/Head'
-import { OverflowTable } from '@components/commons/OverflowTable'
 import { GetServerSidePropsContext, GetServerSidePropsResult, InferGetServerSidePropsType } from 'next'
-import { Container } from '@components/commons/Container'
-import { getAssetIcon } from '@components/icons/assets'
-import NumberFormat from 'react-number-format'
-import { TextMiddleTruncate } from '@components/commons/TextMiddleTruncate'
-import {
-  LoanVaultActive,
-  LoanVaultLiquidated,
-  LoanVaultState,
-  LoanVaultTokenAmount
-} from '@defichain/whale-api-client/dist/api/loan'
 import { getWhaleApiClient } from '@contexts/WhaleContext'
-import { CursorPage, CursorPagination } from '@components/commons/CursorPagination'
+import { LoanVaultActive, LoanVaultLiquidated } from '@defichain/whale-api-client/dist/api/loan'
+import { Container } from '@components/commons/Container'
 
-interface VaultsPageData {
-  vaults: {
-    items: Array<LoanVaultActive | LoanVaultLiquidated>
-    pages: CursorPage[]
-  }
+interface VaultPageProps {
+  vaults: Array<LoanVaultActive | LoanVaultLiquidated>
 }
 
-export default function Vaults ({ vaults }: InferGetServerSidePropsType<typeof getServerSideProps>): JSX.Element {
+export default function VaultPage (props: InferGetServerSidePropsType<typeof getServerSideProps>): JSX.Element {
   return (
     <Container className='pt-12 pb-20'>
-      <Head title='Vaults' />
-
-      <h1 className='text-2xl font-medium'>Vaults</h1>
-
-      <div className='my-6'>
-        <OverflowTable>
-          <OverflowTable.Header>
-            <OverflowTable.Head sticky>VAULT ID</OverflowTable.Head>
-            <OverflowTable.Head>STATUS</OverflowTable.Head>
-            <OverflowTable.Head alignRight>LOANS VALUE (USD)</OverflowTable.Head>
-            <OverflowTable.Head alignRight>COLLATERAL VALUE (USD)</OverflowTable.Head>
-            <OverflowTable.Head alignRight>COLLATERAL RATIO</OverflowTable.Head>
-          </OverflowTable.Header>
-
-          {vaults.items.map(vault => {
-            if (vault.state === LoanVaultState.IN_LIQUIDATION) {
-              return <LiquidatedVaultRow vault={vault} />
-            }
-
-            return <ActiveVaultRow vault={vault} key={vault.vaultId} />
-          })}
-        </OverflowTable>
-      </div>
-      <div className='flex justify-end mt-8'>
-        <CursorPagination pages={vaults.pages} path='/vaults' />
+      <div data-testid='TEST_JSON'>
+        <pre>{JSON.stringify(props.vaults, null, 2)}</pre>
       </div>
     </Container>
   )
 }
 
-function ActiveVaultRow ({ vault }: { vault: LoanVaultActive }): JSX.Element {
-  return (
-    <OverflowTable.Row key={vault.vaultId}>
-      <OverflowTable.Cell>
-        <TextMiddleTruncate textLength={6} text={vault.vaultId} />
-      </OverflowTable.Cell>
-      <OverflowTable.Cell>
-        <VaultStatus state={vault.state} />
-      </OverflowTable.Cell>
-      <OverflowTable.Cell alignRight>
-        <div className='flex gap-x-6 justify-end'>
-          <LoanSymbols tokens={vault.loanAmounts} />
-          <NumberFormat
-            value={vault.loanValue}
-            displayType='text'
-            decimalScale={2}
-            fixedDecimalScale
-            thousandSeparator
-          />
-        </div>
-      </OverflowTable.Cell>
-      <OverflowTable.Cell alignRight>
-        <div className='flex gap-x-6 justify-end'>
-          <LoanSymbols tokens={vault.collateralAmounts} />
-          <NumberFormat
-            value={vault.collateralValue}
-            displayType='text'
-            decimalScale={2}
-            fixedDecimalScale
-            thousandSeparator
-          />
-        </div>
-      </OverflowTable.Cell>
-      <OverflowTable.Cell alignRight>
-        {`${vault.collateralRatio}%`}
-      </OverflowTable.Cell>
-    </OverflowTable.Row>
-  )
-}
-
-function LiquidatedVaultRow ({ vault }: { vault: LoanVaultLiquidated }): JSX.Element {
-  return (
-    <OverflowTable.Row key={vault.vaultId}>
-      <OverflowTable.Cell>
-        <TextMiddleTruncate textLength={6} text={vault.vaultId} />
-      </OverflowTable.Cell>
-      <OverflowTable.Cell>
-        <VaultStatus state={vault.state} />
-      </OverflowTable.Cell>
-      <OverflowTable.Cell alignRight>N/A</OverflowTable.Cell>
-      <OverflowTable.Cell alignRight>N/A</OverflowTable.Cell>
-      <OverflowTable.Cell alignRight>N/A</OverflowTable.Cell>
-    </OverflowTable.Row>
-  )
-}
-
-function LoanSymbols (props: { tokens: LoanVaultTokenAmount[] }): JSX.Element {
-  const remainingTokens = props.tokens.length - 3
-  return (
-    <div className='flex items-center gap-x-1'>
-      <div className='flex'>
-        {props.tokens.map((loan, index) => {
-          const TokenIcon = getAssetIcon(loan.symbol)
-          if (index < 3) {
-            if (index >= 1) {
-              return <TokenIcon className='h-6 w-6 -ml-2' />
-            }
-            return <TokenIcon className='h-6 w-6' />
-          }
-          return null
-        })}
-      </div>
-
-      {remainingTokens > 0 && (
-        <span className='text-xs text-gray-500'>{`+${remainingTokens}`}</span>
-      )}
-    </div>
-  )
-}
-
-function VaultStatus (props: { state: LoanVaultState.ACTIVE | LoanVaultState.FROZEN | LoanVaultState.MAY_LIQUIDATE | LoanVaultState.UNKNOWN | LoanVaultState.IN_LIQUIDATION }): JSX.Element {
-  switch (props.state) {
-    case LoanVaultState.ACTIVE:
-      return <span className='px-2 py-1 inline-block text-xs text-blue-500 text-center bg-blue-100'>ACTIVE</span>
-    case LoanVaultState.FROZEN:
-      return <span className='px-2 py-1 inline-block text-xs text-red-300 text-center bg-red-100'>HALTED</span>
-    case LoanVaultState.MAY_LIQUIDATE:
-      return <span className='px-2 py-1 inline-block text-xs text-orange-500 text-center bg-orange-100'>AT RISK</span>
-    case LoanVaultState.IN_LIQUIDATION:
-      return <span className='px-2 py-1 inline-block text-xs text-gray-500 text-center bg-gray-100'>LIQUIDATED</span>
-    case LoanVaultState.UNKNOWN:
-      return <span className='px-2 py-1 inline-block text-xs text-white text-center bg-gray-400'>UNKNOWN</span>
-  }
-}
-
-export async function getServerSideProps (context: GetServerSidePropsContext): Promise<GetServerSidePropsResult<VaultsPageData>> {
-  const next = CursorPagination.getNext(context)
+export async function getServerSideProps (context: GetServerSidePropsContext): Promise<GetServerSidePropsResult<VaultPageProps>> {
   const api = getWhaleApiClient(context)
-  const vaults = await api.loan.listVault(10, next)
+  const vaults = await api.loan.listVault()
 
   return {
     props: {
-      vaults: {
-        items: vaults,
-        pages: CursorPagination.getPages(context, vaults)
-      }
+      vaults: [...vaults]
     }
   }
 }
