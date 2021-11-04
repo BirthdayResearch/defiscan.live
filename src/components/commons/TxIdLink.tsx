c6f32b9c9bb98c71708ff02f938874f50f4bbbf6
Suraj Auwal <siradjiawoual@gmail.com>
2021-11-04 15:29:27 +0100

    Improved TxLinkId Component

--- src/components/commons/TxIdLink.tsx
+++ src/components/commons/TxIdLink.tsx
@@ -1,16 +1,23 @@
 import { Link } from '@components/commons/Link'
 import classnames from 'classnames'
+import { PropsWithChildren } from 'react'
 
 interface TxIdLinkProps {
   txid: string
   className?: string
   testId?: string
 }
-export function TxIdLink (props: TxIdLinkProps): JSX.Element {
+
+export function TxIdLink (props: PropsWithChildren<TxIdLinkProps>): JSX.Element {
   return (
     <div data-testid={props.testId} className={classnames('hover:underline text-primary-500 cursor-pointer', props.className)}>
       <Link href={{ pathname: `/transactions/${props.txid}` }}>
-        {props.txid}
+        {(() => {
+          if (props.children !== undefined) {
+            return props.children
+          }
+          return props.txid
+        })()}
       </Link>
     </div>
   )
