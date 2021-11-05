32ab40774a2b4d18e9b7efb3f53107f8180a2b3e
Joel-David Wong <git@joeldavid.dev>
2021-11-05 20:34:04 +0800

    Added mocked data and vaults/:id skeleton page

--- src/pages/vaults/[vaultid].tsx
+++ src/pages/vaults/[vaultid].tsx
@@ -1,52 +1,18 @@
 import { GetServerSidePropsContext, GetServerSidePropsResult, InferGetServerSidePropsType } from 'next'
-import React, { useCallback } from 'react'
-import {
-  LoanVaultActive,
-  LoanVaultLiquidated, LoanVaultLiquidationBatch,
-  LoanVaultState
-} from '@defichain/whale-api-client/dist/api/loan'
-
 import { Container } from '@components/commons/Container'
-import { VaultHeading } from '@components/vaults/[vaultid]/VaultHeading'
-import { VaultDetailsTable } from '@components/vaults/[vaultid]/VaultDetailsTable'
-import { CollateralDetails } from '@components/vaults/[vaultid]/CollateralDetailsTable'
-import { VaultLoansTable } from '@components/vaults/[vaultid]/VaultLoansTable'
+import { LoanVaultActive, LoanVaultLiquidated, LoanVaultState } from '@defichain/whale-api-client/dist/api/loan'
+// import { getWhaleApiClient } from '@contexts/WhaleContext'
 
 interface VaultsPageData {
   vault: LoanVaultActive | LoanVaultLiquidated
 }
 
-export default function VaultIdPage ({ vault }: InferGetServerSidePropsType<typeof getServerSideProps>): JSX.Element {
-  const getCollaterals = useCallback((batches: LoanVaultLiquidationBatch[]) => {
-    return batches.flatMap(v => v.collaterals)
-  }, [vault])
-
-  const getLoans = useCallback((batches: LoanVaultLiquidationBatch[]) => {
-    return batches.map(v => v.loan)
-  }, [vault])
-
+export default function VaultDetails ({ vault }: InferGetServerSidePropsType<typeof getServerSideProps>): JSX.Element {
   return (
     <Container className='pt-12 pb-20'>
-      <VaultHeading vaultId={vault.vaultId} vaultState={vault.state} />
-      <VaultDetailsTable vault={vault} />
-      {(() => {
-        switch (vault.state) {
-          case LoanVaultState.ACTIVE:
-            return (
-              <>
-                <CollateralDetails collaterals={vault.collateralAmounts} />
-                <VaultLoansTable loans={vault.loanAmounts} />
-              </>
-            )
-          case LoanVaultState.IN_LIQUIDATION:
-            return (
-              <>
-                <CollateralDetails collaterals={getCollaterals(vault.batches)} />
-                <VaultLoansTable loans={getLoans(vault.batches)} />
-              </>
-            )
-        }
-      })()}
+      <div>
+        <pre>{JSON.stringify(vault, null, 2)}</pre>
+      </div>
     </Container>
   )
 }
@@ -54,10 +20,10 @@ export default function VaultIdPage ({ vault }: InferGetServerSidePropsType<type
 export async function getServerSideProps (context: GetServerSidePropsContext): Promise<GetServerSidePropsResult<VaultsPageData>> {
   // const api = getWhaleApiClient(context)
   // const vaults = await api.loan.getVault("")
-  const vaultsActive: LoanVaultActive = {
-    vaultId: 'c9b19726d6ce42beec137f1fe85614ec3341aff83f797ccd51f6494e21ac9df4',
+  const vaults_active = {
+    vaultId: "c9b19726d6ce42beec137f1fe85614ec3341aff83f797ccd51f6494e21ac9df4",
     loanSchemeId: 'scheme',
-    ownerAddress: '8MR5RWXEDdy9CpFdN5CG5WBe41EQJZ9ZJ8',
+    ownerAddress: "8MR5RWXEDdy9CpFdN5CG5WBe41EQJZ9ZJ8",
     state: LoanVaultState.ACTIVE,
     collateralRatio: '16667',
     collateralValue: '10000',
@@ -72,30 +38,6 @@ export async function getServerSideProps (context: GetServerSidePropsContext): P
         name: 'Default Defi token',
         symbol: 'DFI',
         symbolKey: 'DFI'
-      },
-      {
-        amount: '10000.00000000',
-        displaySymbol: 'DFI',
-        id: '0',
-        name: 'Default Defi token',
-        symbol: 'DFI',
-        symbolKey: 'DFI'
-      },
-      {
-        amount: '450.344',
-        displaySymbol: 'dBTC',
-        id: '1',
-        name: 'BTC',
-        symbol: 'BTC',
-        symbolKey: 'BTC'
-      },
-      {
-        amount: '0.345000.521',
-        displaySymbol: 'dETH',
-        id: '2',
-        name: 'ETHEREUM',
-        symbol: 'ETH',
-        symbolKey: 'ETH'
       }
     ],
     loanAmounts: [
@@ -103,7 +45,7 @@ export async function getServerSideProps (context: GetServerSidePropsContext): P
         amount: '30.00009120',
         displaySymbol: 'dTSLA',
         id: '1',
-        name: 'TSLA',
+        name: '',
         symbol: 'TSLA',
         symbolKey: 'TSLA'
       }
@@ -113,73 +55,49 @@ export async function getServerSideProps (context: GetServerSidePropsContext): P
         amount: '0.00009120',
         displaySymbol: 'dTSLA',
         id: '1',
-        name: 'dTSLA',
+        name: '',
         symbol: 'TSLA',
         symbolKey: 'TSLA'
       }
     ]
-  }
+  } as LoanVaultActive
 
-  // const vaultsLiquidated: LoanVaultLiquidated = {
-  //   vaultId: 'c9b19726d6ce42beec137f1fe85614ec3341aff83f797ccd51f6494e21ac9df4',
-  //   loanSchemeId: 'scheme',
-  //   ownerAddress: '8MR5RWXEDdy9CpFdN5CG5WBe41EQJZ9ZJ8',
-  //   state: LoanVaultState.IN_LIQUIDATION,
-  //   batchCount: 1,
-  //   liquidationHeight: 162,
-  //   liquidationPenalty: 5,
-  //   batches: [
-  //     {
-  //       index: 0,
-  //       collaterals: [
-  //         {
-  //           amount: '10000.00000000',
-  //           displaySymbol: 'DFI',
-  //           id: '0',
-  //           name: 'Default Defi token',
-  //           symbol: 'DFI',
-  //           symbolKey: 'DFI'
-  //         },
-  //         {
-  //           amount: '10000.00000000',
-  //           displaySymbol: 'DFI',
-  //           id: '0',
-  //           name: 'Default Defi token',
-  //           symbol: 'DFI',
-  //           symbolKey: 'DFI'
-  //         },
-  //         {
-  //           amount: '450.344',
-  //           displaySymbol: 'dBTC',
-  //           id: '1',
-  //           name: 'BTC',
-  //           symbol: 'BTC',
-  //           symbolKey: 'BTC'
-  //         },
-  //         {
-  //           amount: '0.345000.521',
-  //           displaySymbol: 'dETH',
-  //           id: '2',
-  //           name: 'ETHEREUM',
-  //           symbol: 'ETH',
-  //           symbolKey: 'ETH'
-  //         }
-  //       ],
-  //       loan: {
-  //         amount: '30.00005130',
-  //         displaySymbol: 'dAAPL',
-  //         id: '2',
-  //         name: 'APPLE',
-  //         symbol: 'AAPL',
-  //         symbolKey: 'AAPL'
-  //       }
-  //     }
-  //   ]
-  // }
+  const vaults_liquidated = {
+    vaultId: "c9b19726d6ce42beec137f1fe85614ec3341aff83f797ccd51f6494e21ac9df4",
+    loanSchemeId: 'scheme',
+    ownerAddress: "8MR5RWXEDdy9CpFdN5CG5WBe41EQJZ9ZJ8",
+    state: LoanVaultState.IN_LIQUIDATION,
+    batchCount: 1,
+    liquidationHeight: 162,
+    liquidationPenalty: 5,
+    batches: [
+      {
+        index: 0,
+        collaterals: [
+          {
+            amount: '10000.00000000',
+            displaySymbol: 'DFI',
+            id: '0',
+            name: 'Default Defi token',
+            symbol: 'DFI',
+            symbolKey: 'DFI'
+          }
+        ],
+        loan: {
+          amount: '30.00005130',
+          displaySymbol: 'dAAPL',
+          id: '2',
+          name: '',
+          symbol: 'AAPL',
+          symbolKey: 'AAPL'
+        }
+      }
+    ]
+  } as LoanVaultLiquidated
 
   return {
     props: {
-      vault: vaultsActive
+      vault: vaults_active
     }
   }
 }
