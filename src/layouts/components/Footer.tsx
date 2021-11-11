7cf66bac67d2ae9906816f5bc1321d91ff505071
Joel-David Wong <git@joeldavid.dev>
2021-11-12 02:25:40 +0800

    Updated roundness to match netlify logo

--- src/layouts/components/Footer.tsx
+++ src/layouts/components/Footer.tsx
@@ -149,7 +149,7 @@ function FooterSectionAbout (): JSX.Element {
 
         {net !== undefined && (
           <HoverPopover popover={<NetworkStatus />} placement='top' className='inline-block float-right'>
-            <div className='text-sm text-gray-900 p-2 bg-white rounded-lg cursor-help'>
+            <div className='text-sm text-gray-900 p-2 bg-white rounded cursor-help'>
               <div>Protocol<span className='ml-2.5 font-medium'>{net.protocolversion}</span></div>
             </div>
           </HoverPopover>
