678cc2294a1c84e4025685a8ef7d185868008321
Joel-David Wong <git@joeldavid.dev>
2021-11-17 17:22:51 +0800

    Added new assets icons

--- src/components/icons/assets/index.ts
+++ src/components/icons/assets/index.ts
@@ -1,19 +1,45 @@
 import { _Default } from '@components/icons/assets/_Default'
-import { BTC } from '@components/icons/assets/BTC'
-import { dBCH } from '@components/icons/assets/dBCH'
-import { dBTC } from '@components/icons/assets/dBTC'
-import { dDFI } from '@components/icons/assets/dDFI'
-import { dDOGE } from '@components/icons/assets/dDOGE'
-import { dETH } from '@components/icons/assets/dETH'
-import { DFI } from '@components/icons/assets/DFI'
-import { dLTC } from '@components/icons/assets/dLTC'
-import { dUSDC } from '@components/icons/assets/dUSDC'
-import { dUSDT } from '@components/icons/assets/dUSDT'
+import { dBCH } from './dBCH'
+import { dBTC } from './dBTC'
+import { BTC } from './BTC'
+import { dDFI } from './dDFI'
+import { dDOGE } from './dDOGE'
+import { dETH } from './dETH'
+import { DFI } from './DFI'
+import { dLTC } from './dLTC'
+import { dUSDC } from './dUSDC'
+import { dUSDT } from './dUSDT'
+import { dTSLA } from './dTSLA'
+import { dUSD } from './dUSD'
+import { dAAPL } from './dAAPL'
+import { dAMD } from './dAMD'
+import { dGME } from './dGME'
+import { dBABA } from './dBABA'
+import { dGOOG } from './dGOOG'
+import { dPLTR } from './dPLTR'
+import { dARKK } from './dARKK'
+import { dAMZN } from './dAMZN'
+import { dCOIN } from './dCOIN'
+import { dFB } from './dFB'
+import { dTWTR } from './dTWTR'
+import { dNVDA } from './dNVDA'
+import { dMSFT } from './dMSFT'
+import { dSPY } from './dSPY'
+import { dQQQ } from './dQQQ'
+import { dGLD } from './dGLD'
+import { dSLV } from './dSLV'
+import { dPDBC } from './dPDBC'
+import { dVNQ } from './dVNQ'
+import { dURTH } from './dURTH'
+import { dTLT } from './dTLT'
 import { SVGProps } from 'react'
 
 const mapping: Record<string, (props: SVGProps<SVGSVGElement>) => JSX.Element> = {
+  _UTXO: DFI,
+  DFI: dDFI,
+  'DFI (UTXO)': DFI,
+  'DFI (Token)': dDFI,
   BTC: BTC,
-  DFI: DFI,
   dBCH: dBCH,
   dBTC: dBTC,
   dDFI: dDFI,
@@ -21,7 +47,31 @@ const mapping: Record<string, (props: SVGProps<SVGSVGElement>) => JSX.Element> =
   dETH: dETH,
   dLTC: dLTC,
   dUSDT: dUSDT,
-  dUSDC: dUSDC
+  dUSDC: dUSDC,
+  dTSLA: dTSLA,
+  dAAPL: dAAPL,
+  dAMD: dAMD,
+  dGME: dGME,
+  dBABA: dBABA,
+  dGOOG: dGOOG,
+  dDUSD: dUSD,
+  DUSD: dUSD,
+  dPLTR: dPLTR,
+  dARKK: dARKK,
+  dAMZN: dAMZN,
+  dCOIN: dCOIN,
+  dFB: dFB,
+  dTWTR: dTWTR,
+  dNVDA: dNVDA,
+  dMSFT: dMSFT,
+  dSPY: dSPY,
+  dQQQ: dQQQ,
+  dGLD: dGLD,
+  dSLV: dSLV,
+  dPDBC: dPDBC,
+  dVNQ: dVNQ,
+  dURTH: dURTH,
+  dTLT: dTLT
 }
 
 // TODO(@defich): move assets into it's own repo where anyone can create pull request into.
