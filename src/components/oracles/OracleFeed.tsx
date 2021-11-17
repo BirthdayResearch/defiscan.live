85f466375c1f29f41d69b83616ad93fb6b7edf65
Joel-David Wong <git@joeldavid.dev>
2021-11-17 21:34:15 +0800

    Fixed link

--- src/components/oracles/OracleFeed.tsx
+++ src/components/oracles/OracleFeed.tsx
@@ -21,60 +21,62 @@ export function OracleFeed (props: PriceFeedProps): JSX.Element {
   return (
     <div className='w-full md:w-1/2 lg:w-1/3 xl:w-1/4 p-4' data-testid='OracleFeed'>
       <Link href={{ pathname: `/oracles/${id}` }}>
-        <div className='bg-gray-50 rounded p-6 cursor-pointer'>
-          <div className='flex gap-x-2'>
-            {(() => {
-              if (copy !== undefined && copy.type === 'CRYPTO') {
-                const NativeIcon = getNativeIcon(price.token)
-                return (<NativeIcon width={24} height={24} />)
-              }
-            })()}
-            <h4>
-              {price.token} / {price.currency}
-            </h4>
-          </div>
+        <a>
+          <div className='bg-gray-50 rounded p-6 cursor-pointer'>
+            <div className='flex gap-x-2'>
+              {(() => {
+                if (copy !== undefined && copy.type === 'CRYPTO') {
+                  const NativeIcon = getNativeIcon(price.token)
+                  return (<NativeIcon width={24} height={24} />)
+                }
+              })()}
+              <h4>
+                {price.token} / {price.currency}
+              </h4>
+            </div>
 
-          <div className='flex items-center mt-2'>
-            <h3 className='text-2xl font-medium'>
-              <NumberFormat
-                value={price.aggregated.amount}
-                displayType='text'
-                thousandSeparator
-                decimalScale={2}
-                suffix={` ${price.currency}`}
-              />
-            </h3>
-            <div>
-              {isActive(price.block) ? (
-                <HoverPopover popover='Verified by oracles'>
-                  <div className='p-1 cursor-help'>
-                    <IoCheckmarkCircle className='h-4 w-4 text-green-500' />
-                  </div>
-                </HoverPopover>
-              ) : (
-                <HoverPopover
-                  popover={`Inactive since ${format(price.block.medianTime * 1000, 'MMM dd, hh:mm:ss aa')}`}
-                >
-                  <div className='p-1 cursor-help'>
-                    <IoAlertCircleOutline className='h-4 w-4 text-gray-500' />
-                  </div>
-                </HoverPopover>
+            <div className='flex items-center mt-2'>
+              <h3 className='text-2xl font-medium'>
+                <NumberFormat
+                  value={price.aggregated.amount}
+                  displayType='text'
+                  thousandSeparator
+                  decimalScale={2}
+                  suffix={` ${price.currency}`}
+                />
+              </h3>
+              <div>
+                {isActive(price.block) ? (
+                  <HoverPopover popover='Verified by oracles'>
+                    <div className='p-1 cursor-help'>
+                      <IoCheckmarkCircle className='h-4 w-4 text-green-500' />
+                    </div>
+                  </HoverPopover>
+                ) : (
+                  <HoverPopover
+                    popover={`Inactive since ${format(price.block.medianTime * 1000, 'MMM dd, hh:mm:ss aa')}`}
+                  >
+                    <div className='p-1 cursor-help'>
+                      <IoAlertCircleOutline className='h-4 w-4 text-gray-500' />
+                    </div>
+                  </HoverPopover>
+                )}
+              </div>
+            </div>
+
+            <div className='flex justify-between items-center mt-5'>
+              {copy !== undefined && (
+                <div className='bg-gray-200 p-1 rounded'>
+                  <div className='text-xs font-medium text-gray-600'>{copy.type}</div>
+                </div>
               )}
+              <div className='flex-grow' />
+              <button className='text-primary-500'>
+                View
+              </button>
             </div>
           </div>
-
-          <div className='flex justify-between items-center mt-5'>
-            {copy !== undefined && (
-              <div className='bg-gray-200 p-1 rounded'>
-                <div className='text-xs font-medium text-gray-600'>{copy.type}</div>
-              </div>
-            )}
-            <div className='flex-grow' />
-            <button className='text-primary-500'>
-              View
-            </button>
-          </div>
-        </div>
+        </a>
       </Link>
     </div>
   )
