e7746b4364a6202f5e05a0a4ad33f941258d6763
Suraj Auwal <siradjiawoual@gmail.com>
2021-11-22 07:31:25 +0100

    `DfTxUpdatevault` (#603)
    * DfTxUpdateVault.tsx component
    
    * Added DfTxUpdateVault.tsx component
    
    * Added DfTxUpdateVault.tsx Tests
    
    * Added Updated vault ID
    
    * Updated vault id casing
    
    Co-authored-by: Joel-David Wong <git@joeldavid.dev>

---
+++ src/components/transactions/[txid]/DfTx/DfTxUpdateVault.tsx
@@ -0,0 +1,53 @@
+import { UpdateVault, DfTx } from '@defichain/jellyfish-transaction'
+import { DfTxHeader } from '@components/transactions/[txid]/DfTx/DfTxHeader'
+import { useNetwork } from '@contexts/NetworkContext'
+import { fromScript } from '@defichain/jellyfish-address'
+import { AdaptiveList } from '@components/commons/AdaptiveList'
+import { AddressLink } from '@components/commons/link/AddressLink'
+import { Link } from '@components/commons/link/Link'
+import { TextMiddleTruncate } from '@components/commons/TextMiddleTruncate'
+
+interface DfTxUpdateVaultProps {
+  dftx: DfTx<UpdateVault>
+}
+
+export function DfTxUpdateVault (props: DfTxUpdateVaultProps): JSX.Element {
+  const network = useNetwork().name
+  const ownerAddress = fromScript(props.dftx.data.ownerAddress, network)
+
+  return (
+    <div>
+      <DfTxHeader name='Update Vault' />
+      <div className='mt-5 flex flex-col space-y-6 items-start lg:flex-row lg:space-x-8 lg:space-y-0'>
+        <AdaptiveList className='w-full lg:w-1/2'>
+          <AdaptiveList.Row name='Vault ID'>
+            <Link href={{ pathname: `/vaults/${props.dftx.data.vaultId}` }}>
+              <a className='content'>
+                <TextMiddleTruncate
+                  textLength={12} text={props.dftx.data.vaultId} className='text-primary-500 hover:underline'
+                  testId='DfTxUpdateVault.VaultID'
+                />
+              </a>
+            </Link>
+          </AdaptiveList.Row>
+          <AdaptiveList.Row name={'Owner\'s Address'}>
+            {(() => {
+              if (ownerAddress != null) {
+                return <AddressLink address={ownerAddress.address} testId='DfTxUpdateVault.OwnersAddress' className='break-all' />
+              }
+              return 'N/A'
+            })()}
+          </AdaptiveList.Row>
+          <AdaptiveList.Row name='Scheme ID' testId='DfTxUpdateVault.SchemeId'>
+            {(() => {
+              if (props.dftx.data.schemeId !== '') {
+                return props.dftx.data.schemeId
+              }
+              return 'N/A'
+            })()}
+          </AdaptiveList.Row>
+        </AdaptiveList>
+      </div>
+    </div>
+  )
+}
