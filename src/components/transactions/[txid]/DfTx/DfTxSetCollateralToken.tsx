1034ae4dc4d1234cceee198de357bb1d299e86eb
Suraj Auwal <siradjiawoual@gmail.com>
2021-11-22 06:03:59 +0100

    `DfTxSetCollateralToken` (#599)
    * Added DfTx Set Collateral Token
    
    * DfTxSetCollateralToken Component
    
    * DfTxSetCollateralToken Component Test
    
    * Fixed linting error
    
    * Fixed failing tests
    
    * Rebase main onto brance

---
+++ src/components/transactions/[txid]/DfTx/DfTxSetCollateralToken.tsx
@@ -0,0 +1,34 @@
+import { DfTx, SetCollateralToken } from '@defichain/jellyfish-transaction'
+import { Link } from '@components/commons/link/Link'
+import { DfTxHeader } from '@components/transactions/[txid]/DfTx/DfTxHeader'
+import { AdaptiveList } from '@components/commons/AdaptiveList'
+
+interface DfTxSetCollateralTokenProps {
+  dftx: DfTx<SetCollateralToken>
+}
+
+export function DfTxSetCollateralToken (props: DfTxSetCollateralTokenProps): JSX.Element {
+  return (
+    <div>
+      <DfTxHeader name='Set Collateral Token' />
+      <div className='mt-5 flex flex-col space-y-6 items-start lg:flex-row lg:space-x-8 lg:space-y-0'>
+        <AdaptiveList className='w-full lg:w-1/2'>
+          <AdaptiveList.Row name='Collateral Token' testId='DfTxSetCollateralToken.CollateralToken'>
+            <Link href={{ pathname: `/tokens/${props.dftx.data.token}` }}>
+              <a className='text-primary-500 hover:underline '>{props.dftx.data.token}</a>
+            </Link>
+          </AdaptiveList.Row>
+          <AdaptiveList.Row name='Currency Pair' testId='DfTxSetCollateralToken.CurrencyPair'>
+            {`${props.dftx.data.currencyPair.token}-${props.dftx.data.currencyPair.currency}`}
+          </AdaptiveList.Row>
+          <AdaptiveList.Row name='Factor' testId='DfTxSetCollateralToken.Factor'>
+            {props.dftx.data.factor.toFixed(8)}
+          </AdaptiveList.Row>
+          <AdaptiveList.Row name='Actived After Block' testId='DfTxSetCollateralToken.Block'>
+            {props.dftx.data.activateAfterBlock}
+          </AdaptiveList.Row>
+        </AdaptiveList>
+      </div>
+    </div>
+  )
+}
