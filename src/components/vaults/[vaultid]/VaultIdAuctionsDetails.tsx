29690ce7c84add4982900e8c54300751ee53af20
Joel-David Wong <git@joeldavid.dev>
2021-11-26 15:37:56 +0800

    Added auction active prices

--- src/components/vaults/[vaultid]/VaultIdAuctionsDetails.tsx
+++ src/components/vaults/[vaultid]/VaultIdAuctionsDetails.tsx
@@ -13,10 +13,12 @@ export function VaultAuctions (props: { batches: LoanVaultLiquidationBatch[] }):
     <>
       <div className='hidden md:block mt-10' data-testid='VaultLoansDesktop'>
         <h2 className='text-xl font-semibold' data-testid='VaultLoansDesktop.Heading'>In Auction</h2>
-        <div className='mt-4 mb-8 items-start grid gap-2 grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4'>
+        <div
+          className='mt-4 mb-8 items-start grid gap-2 grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4'
+        >
           {props.batches.map((batch) => (
             <VaultAuctionsDetailsCard
-              batchIndex={batch.index} loan={batch.loan} collaterals={batch.collaterals}
+              batch={batch}
               key={batch.index}
             />
           ))}
@@ -27,7 +29,7 @@ export function VaultAuctions (props: { batches: LoanVaultLiquidationBatch[] }):
         <div className='flex flex-col items-center space-y-2'>
           {props.batches.map((batch) => (
             <VaultAuctionsDetailsCard
-              batchIndex={batch.index} loan={batch.loan} collaterals={batch.collaterals}
+              batch={batch}
               key={batch.index}
             />
           ))}
@@ -37,9 +39,16 @@ export function VaultAuctions (props: { batches: LoanVaultLiquidationBatch[] }):
   )
 }
 
-function VaultAuctionsDetailsCard (props: { batchIndex: number, loan: LoanVaultTokenAmount, collaterals: LoanVaultTokenAmount[] }): JSX.Element {
-  const LoanSymbol = getAssetIcon(props.loan.symbol)
+function VaultAuctionsDetailsCard (props: { batch: LoanVaultLiquidationBatch }): JSX.Element {
+  const LoanSymbol = getAssetIcon(props.batch.loan.symbol)
   const [isOpen, setIsOpen] = useState<boolean>(true)
+
+  const minStartingBid = new BigNumber(props.batch.loan.amount).multipliedBy(1.05)
+  let minStartingBidValue: BigNumber | undefined
+
+  if (props.batch.loan.activePrice?.active != null) {
+    minStartingBidValue = new BigNumber(props.batch.loan.activePrice.active.amount).multipliedBy(minStartingBid)
+  }
 
   return (
     <div
@@ -49,7 +58,7 @@ function VaultAuctionsDetailsCard (props: { batchIndex: number, loan: LoanVaultT
       <div className='flex items-center justify-between'>
         <div className='flex items-center'>
           <LoanSymbol className='h-6 w-6' />
-          <span className='ml-1.5 font-medium text-gray-900'>{props.loan.displaySymbol}</span>
+          <span className='ml-1.5 font-medium text-gray-900'>{props.batch.loan.displaySymbol}</span>
         </div>
         <div className='flex items-center text-primary-500 cursor-pointer' onClick={() => setIsOpen(!isOpen)}>
           {!isOpen
@@ -59,14 +68,48 @@ function VaultAuctionsDetailsCard (props: { batchIndex: number, loan: LoanVaultT
       </div>
 
       <div className='flex items-center justify-between mt-2'>
-        <span className='font-medium text-gray-500 text-xs'>{`BATCH ${props.batchIndex + 1}`}</span>
+        <span className='font-medium text-gray-500 text-xs'>{`BATCH ${props.batch.index + 1}`}</span>
       </div>
 
-      <div className='flex items-center justify-between mt-6'>
-        <span className='text-gray-500 text-sm'>Amount</span>
+      <div className='flex justify-between mt-6'>
+        <span className='text-gray-500 text-sm'>Current Highest Bid</span>
         <div>
-          <span>{new BigNumber(props.loan.amount).toFixed(8)}</span>
-          <span className='ml-1'>{props.loan.displaySymbol}</span>
+          {
+            props.batch.highestBid === undefined ? (
+              <>N/A</>
+            )
+              : (
+                <>
+                  <span>{new BigNumber(props.batch.highestBid.amount.amount).toFixed(8)}</span>
+                  <span className='ml-1'>{props.batch.loan.displaySymbol}</span>
+                </>
+                )
+          }
+        </div>
+      </div>
+
+      <div className='flex justify-between mt-6'>
+        <span className='text-gray-500 text-sm'>Min. Starting Bid</span>
+        <div>
+          <div>
+            <span>{minStartingBid.toFixed(8)}</span>
+            <span className='ml-1'>{props.batch.loan.displaySymbol}</span>
+          </div>
+          <div className='text-sm text-gray-500 text-right'>
+            {
+              (minStartingBidValue != null) && (
+                <ReactNumberFormat
+                  value={minStartingBidValue.toFixed(2, BigNumber.ROUND_HALF_UP)}
+                  displayType='text'
+                  decimalScale={2}
+                  prefix='$'
+                  suffix=' USD'
+                  fixedDecimalScale
+                  thousandSeparator
+                />
+              )
+            }
+          </div>
         </div>
       </div>
 
@@ -86,7 +129,7 @@ function VaultAuctionsDetailsCard (props: { batchIndex: number, loan: LoanVaultT
             <InfoHoverPopover className='ml-1' description='Collateral token(s) for auction.' />
           </div>
           {
-            props.collaterals.map(collateral => (
+            props.batch.collaterals.map(collateral => (
               <CollateralListItem collateral={collateral} key={collateral.id} />
             ))
           }
@@ -98,21 +141,44 @@ function VaultAuctionsDetailsCard (props: { batchIndex: number, loan: LoanVaultT
 
 function CollateralListItem (props: { collateral: LoanVaultTokenAmount }): JSX.Element {
   const CollateralSymbol = getAssetIcon(props.collateral.symbol)
+  let collateralValue: BigNumber | undefined
+
+  if (props.collateral.activePrice?.active != null) {
+    const price = new BigNumber(props.collateral.activePrice.active.amount)
+    collateralValue = price.multipliedBy(props.collateral.amount)
+  }
 
   return (
     <div className='flex justify-between'>
-      <div className='flex items-center'>
+      <div className='flex'>
         <CollateralSymbol className='h-6 w-6' />
         <span className='ml-1.5 font-medium text-gray-900'>{props.collateral.displaySymbol}</span>
       </div>
       <div>
-        <ReactNumberFormat
-          value={props.collateral.amount}
-          displayType='text'
-          decimalScale={8}
-          fixedDecimalScale
-          thousandSeparator
-        />
+        <div>
+          <ReactNumberFormat
+            value={props.collateral.amount}
+            displayType='text'
+            decimalScale={8}
+            fixedDecimalScale
+            thousandSeparator
+          />
+        </div>
+        <div className='text-sm text-gray-500 text-right'>
+          {
+            (collateralValue != null) && (
+              <ReactNumberFormat
+                value={collateralValue.toFixed(2, BigNumber.ROUND_HALF_UP)}
+                displayType='text'
+                decimalScale={2}
+                prefix='$'
+                suffix=' USD'
+                fixedDecimalScale
+                thousandSeparator
+              />
+            )
+          }
+        </div>
       </div>
     </div>
   )
