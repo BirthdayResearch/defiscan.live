d244f7820d4a01479fc51dc3c72a14e2768a209b
Suraj Auwal <siradjiawoual@gmail.com>
2021-12-01 21:50:28 +0100

    Updated Code based on review

--- src/pages/auctions/index.tsx
+++ src/pages/auctions/index.tsx
@@ -1,19 +1,17 @@
 import { GetServerSidePropsContext, GetServerSidePropsResult, InferGetServerSidePropsType } from 'next'
 import { CursorPage, CursorPagination } from '@components/commons/CursorPagination'
 import { getWhaleApiClient } from '@contexts/WhaleContext'
-import { AuctionsStatsBar } from '@components/auctions/AuctionsStatsBar'
 import { Head } from '@components/commons/Head'
 import { Container } from '@components/commons/Container'
-import React, { useState } from 'react'
 import { InfoHoverPopover } from '@components/commons/popover/InfoHoverPopover'
 import { OverflowTable } from '@components/commons/OverflowTable'
 import { LoanVaultLiquidationBatch } from '@defichain/whale-api-client/dist/api/loan'
-import classNames from 'classnames'
 import { Link } from '@components/commons/link/Link'
 import { MdChevronRight } from 'react-icons/md'
 import ReactNumberFormat from 'react-number-format'
 import { VaultTokenSymbols } from '@components/vaults/common/VaultTokenSymbols'
 import { CalculateCollateralsValue } from '../../utils/vaults/CalculateCollateralsValue'
+import BigNumber from 'bignumber.js'
 
 interface ActionsPageProps {
   auctions: {
@@ -23,38 +21,19 @@ interface ActionsPageProps {
 }
 
 export default function AuctionsPage ({ auctions }: InferGetServerSidePropsType<typeof getServerSideProps>): JSX.Element {
-  const types = ['All', 'Ongoing', 'Ended']
-  const [typeSelection, setTypeCurrentSelection] = useState<string>('All')
-
-  console.log(auctions.items)
   return (
     <>
-      <AuctionsStatsBar />
       <Container className='pt-12 pb-20'>
         <Head title='Auctions' />
         {auctions.items.length === 0 ? (
           <div className='text-gray-400 flex w-full justify-center p-12'>
-            There are no Collaterals at this time
+            There are no active auctions at this time.
           </div>
         ) : (
           <>
-            <div className='flex justify-between items-center'>
-              <div className='flex items-center'>
-                <h1 className='text-2xl font-medium'>Auctions</h1>
-                <InfoHoverPopover className='ml-1' description='Auctions' />
-              </div>
-              <div className='flex w-full lg:max-w-max flex-wrap -mx-0.5' data-testid='FeedFilter.Types'>
-                {types.map(type => (
-                  <div
-                    onClick={() => setTypeCurrentSelection(type)}
-                    className={classNames('rounded p-2 border cursor-pointer mx-0.5 mt-1 lg:mt-0', typeSelection === type ? 'text-white bg-primary-500 border-primary-500' : 'border-gray-300 text-gray-900 hover:bg-primary-50')}
-                    key={type}
-                    data-testid='FeedFilter.Types'
-                  >
-                    {type}
-                  </div>
-                ))}
-              </div>
+            <div className='flex items-center'>
+              <h1 className='text-2xl font-medium'>Auctions</h1>
+              <InfoHoverPopover className='ml-1' description='Auctions' />
             </div>
             <div className='my-6 hidden md:block'>
               <OverflowTable>
@@ -137,7 +116,7 @@ function AuctionsTableRow ({ batch }: { batch: LoanVaultLiquidationBatch }): JSX
       <OverflowTable.Cell alignRight>
         <div className='text-right'>
           <ReactNumberFormat
-            value={CalculateCollateralsValue(batch.collaterals).value.toFixed(0)}
+            value={CalculateCollateralsValue(batch.collaterals).value.toFixed(2, BigNumber.ROUND_HALF_UP)}
             thousandSeparator
             decimalScale={2}
             prefix='$'
