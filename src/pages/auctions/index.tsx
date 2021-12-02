e0af7bf279c22ca71296f0f19baa1647660e1a22
Suraj Auwal <siradjiawoual@gmail.com>
2021-12-02 09:29:18 +0100

    Mobile View, Major updates

--- src/pages/auctions/index.tsx
+++ src/pages/auctions/index.tsx
@@ -5,22 +5,37 @@ import { Head } from '@components/commons/Head'
 import { Container } from '@components/commons/Container'
 import { InfoHoverPopover } from '@components/commons/popover/InfoHoverPopover'
 import { OverflowTable } from '@components/commons/OverflowTable'
-import { LoanVaultLiquidationBatch } from '@defichain/whale-api-client/dist/api/loan'
+import { LoanVaultLiquidated, LoanVaultLiquidationBatch } from '@defichain/whale-api-client/dist/api/loan'
 import { Link } from '@components/commons/link/Link'
-import { MdChevronRight } from 'react-icons/md'
+import { MdChevronRight, MdOutlineKeyboardArrowDown, MdOutlineKeyboardArrowUp } from 'react-icons/md'
 import ReactNumberFormat from 'react-number-format'
 import { VaultTokenSymbols } from '@components/vaults/common/VaultTokenSymbols'
 import { CalculateCollateralsValue } from '../../utils/vaults/CalculateCollateralsValue'
 import BigNumber from 'bignumber.js'
+import { useAuctionTimeLeft } from '../../hooks/useAuctionTimeLeft'
+import { useState } from 'react'
+import { Transition } from '@headlessui/react'
+import { useSelector } from 'react-redux'
+import { RootState } from '@store/index'
+import { getAssetIcon } from '@components/icons/assets'
+import { VaultDetailsListItem } from '@components/vaults/common/VaultDetailsListItem'
 
 interface ActionsPageProps {
   auctions: {
-    items: LoanVaultLiquidationBatch[]
+    items: LoanVaultLiquidated[]
     pages: CursorPage[]
   }
 }
 
+interface AuctionDetailProps {
+  batch: LoanVaultLiquidationBatch
+  vault: LoanVaultLiquidated
+  blockCount: number | undefined
+}
+
 export default function AuctionsPage ({ auctions }: InferGetServerSidePropsType<typeof getServerSideProps>): JSX.Element {
+  const { count: { blocks } } = useSelector((state: RootState) => state.stats)
+
   return (
     <>
       <Container className='pt-12 pb-20'>
@@ -38,6 +53,11 @@ export default function AuctionsPage ({ auctions }: InferGetServerSidePropsType<
             <div className='my-6 hidden md:block'>
               <OverflowTable>
                 <OverflowTable.Header>
+                  <OverflowTable.Head
+                    title='Time Left'
+                    infoDesc=''
+                    testId='AuctionPage.TimeLeft'
+                  />
                   <OverflowTable.Head
                     title='Loan Token'
                     infoDesc=''
@@ -67,16 +87,31 @@ export default function AuctionsPage ({ auctions }: InferGetServerSidePropsType<
                     alignRight
                   />
                 </OverflowTable.Header>
-                {(() => {
-                  const sorted = sortByType(auctions.items)
-                  return (
-                    sorted.map(batch => (
-                      <AuctionsTableRow batch={batch} key={batch.index} />
-                    ))
-                  )
-                })()}
-
+                {auctions.items.map(auction => {
+                  return auction.batches.map(batch => (
+                    <AuctionsTableRow
+                      batch={batch}
+                      key={batch.index}
+                      vault={auction}
+                      blockCount={blocks}
+                    />
+                  ))
+                })}
               </OverflowTable>
+            </div>
+            <div className='my-6 block md:hidden'>
+              <div className='flex flex-wrap space-y-2'>
+                {auctions.items.map(auction => {
+                  return auction.batches.map(batch => (
+                    <AuctionsMobileCard
+                      batch={batch}
+                      key={batch.index}
+                      vault={auction}
+                      blockCount={blocks}
+                    />
+                  ))
+                })}
+              </div>
             </div>
           </>
         )}
@@ -85,24 +120,32 @@ export default function AuctionsPage ({ auctions }: InferGetServerSidePropsType<
   )
 }
 
-function AuctionsTableRow ({ batch }: { batch: LoanVaultLiquidationBatch }): JSX.Element {
+function AuctionsTableRow (props: AuctionDetailProps): JSX.Element {
+  const totalCollateral = CalculateCollateralsValue(props.batch.collaterals).value
+    .toFixed(2, BigNumber.ROUND_HALF_UP)
+
+  const timeLeft = useAuctionTimeLeft(props.vault.liquidationHeight, props.blockCount ?? 0)
+
   return (
     <OverflowTable.Row>
       <OverflowTable.Cell>
-        {batch.loan.displaySymbol}
+        {timeLeft.timeRemaining}
+      </OverflowTable.Cell>
+      <OverflowTable.Cell>
+        {props.batch.loan.displaySymbol}
       </OverflowTable.Cell>
       <OverflowTable.Cell alignRight>
         {(() => {
-          if (batch.highestBid?.amount !== undefined) {
+          if (props.batch.highestBid?.amount !== undefined) {
             return (
               <span>
                 <ReactNumberFormat
-                  value={batch.highestBid.amount.amount}
+                  value={props.batch.highestBid.amount.amount}
                   thousandSeparator
                   decimalScale={2}
                   displayType='text'
                 />
-                {batch.highestBid.amount.displaySymbol}
+                {props.batch.highestBid.amount.displaySymbol}
               </span>
 
             )
@@ -111,12 +154,12 @@ function AuctionsTableRow ({ batch }: { batch: LoanVaultLiquidationBatch }): JSX
         })()}
       </OverflowTable.Cell>
       <OverflowTable.Cell>
-        <VaultTokenSymbols className='justify-end' tokens={batch.collaterals} />
+        <VaultTokenSymbols className='justify-end' tokens={props.batch.collaterals} />
       </OverflowTable.Cell>
       <OverflowTable.Cell alignRight>
         <div className='text-right'>
           <ReactNumberFormat
-            value={CalculateCollateralsValue(batch.collaterals).value.toFixed(2, BigNumber.ROUND_HALF_UP)}
+            value={totalCollateral}
             thousandSeparator
             decimalScale={2}
             prefix='$'
@@ -125,7 +168,7 @@ function AuctionsTableRow ({ batch }: { batch: LoanVaultLiquidationBatch }): JSX
         </div>
       </OverflowTable.Cell>
       <OverflowTable.Cell>
-        <Link href={{ pathname: '/auctions' }}>
+        <Link href={{ pathname: `/auctions/${props.batch.index}` }}>
           <a className='contents'>
             <div className='flex justify-end'>
               <MdChevronRight className='h-6  w-6' />
@@ -137,40 +180,116 @@ function AuctionsTableRow ({ batch }: { batch: LoanVaultLiquidationBatch }): JSX
   )
 }
 
-function sortByType (batch: LoanVaultLiquidationBatch[]): LoanVaultLiquidationBatch[] {
-  const typeOrder = ['Ongoing', 'Ended']
-  let sortedArr: LoanVaultLiquidationBatch[] = []
+function AuctionsMobileCard (props: AuctionDetailProps): JSX.Element {
+  const [isOpen, setIsOpen] = useState<boolean>(false)
+  const { timeRemaining } = useAuctionTimeLeft(props.vault.liquidationHeight, props.blockCount ?? 0)
 
-  const groups = batch.reduce((groups, item) => {
-    const key = item.loan.activePrice?.isLive === true ? 'Ongoing' : 'Ended'
-    if (key in groups) {
-      groups[key].push(item)
-    } else {
-      groups[key] = [item]
-    }
-    return groups
-  }, {})
+  const LoanSymbol = getAssetIcon(props.batch.loan.displaySymbol)
 
-  typeOrder.forEach(type => {
-    if (type in groups) {
-      sortedArr = sortedArr.concat(groups[type])
-    }
-  })
+  const totalCollateral = CalculateCollateralsValue(props.batch.collaterals).value
+    .toFixed(2, BigNumber.ROUND_HALF_UP)
 
-  return sortedArr
+  return (
+    <div
+      className='w-full flex flex-col rounded border border-gray-200 p-4 text-gray-500'
+      data-testid='VaultMobileCard'
+    >
+      <div className='w-full flex justify-between'>
+        <div className='flex items-center space-x-1 font-medium'>
+          <LoanSymbol className='w-6 h-6' />
+          <span>{props.batch.loan.displaySymbol}</span>
+        </div>
+        <div
+          className='flex items-center px-2  cursor-pointer'
+          onClick={() => setIsOpen(!isOpen)}
+          data-testid='VaultMobileCard.Toggle'
+        >
+          {(!isOpen)
+            ? <MdOutlineKeyboardArrowDown size={28} />
+            : <MdOutlineKeyboardArrowUp size={28} />}
+        </div>
+      </div>
+      <div className='text-xs mt-1 mb-4'>
+        {(timeRemaining !== undefined) ? <span>{timeRemaining} left</span> : ''}
+      </div>
+
+      <VaultDetailsListItem
+        title='Current Highest Bid'
+        infoDesc='Current Highest Bid'
+      >
+        {(() => {
+          if (props.batch.highestBid?.amount !== undefined) {
+            return (
+              <span>
+                <ReactNumberFormat
+                  value={props.batch.highestBid.amount.amount}
+                  thousandSeparator
+                  decimalScale={2}
+                  displayType='text'
+                />
+                {props.batch.highestBid.amount.displaySymbol}
+              </span>
+
+            )
+          }
+          return 'N/A'
+        })()}
+      </VaultDetailsListItem>
+
+      <Transition
+        enter='transition ease-out duration-200'
+        enterFrom='opacity-0 translate-y-0'
+        enterTo='opacity-100 translate-y-1'
+        leave='transition ease-in duration-150'
+        leaveFrom='opacity-100 translate-y-1'
+        leaveTo='opacity-100 translate-y-0'
+        className='w-full'
+        show={isOpen}
+      >
+        <div className='w-full mt-2 space-y-2'>
+          <VaultDetailsListItem
+            title='Collateral For Auction'
+            infoDesc='Collateral For Auction'
+          >
+            <VaultTokenSymbols className='justify-end' tokens={props.batch.collaterals} />
+          </VaultDetailsListItem>
+          <VaultDetailsListItem
+            title='Collateral Value (USD)'
+            infoDesc='Collateral Value (USD)'
+          >
+            <ReactNumberFormat
+              value={totalCollateral}
+              thousandSeparator
+              decimalScale={2}
+              prefix='$'
+              displayType='text'
+            />
+          </VaultDetailsListItem>
+        </div>
+        <div className='flex justify-center mt-5'>
+          <div className='text-primary-500 p-2 text-center border border-gray-200 rounded-sm'>
+            <Link href={{ pathname: `/auctions/${props.batch.index}` }}>
+              <a className='contents'>
+                VIEW BATCH DETAILS
+              </a>
+            </Link>
+          </div>
+        </div>
+      </Transition>
+
+    </div>
+  )
 }
 
 export async function getServerSideProps (context: GetServerSidePropsContext): Promise<GetServerSidePropsResult<ActionsPageProps>> {
   const next = CursorPagination.getNext(context)
-  const items: LoanVaultLiquidationBatch[] = []
   try {
-    const response = await getWhaleApiClient(context).loan.listAuction(30, next)
-    response.map(result => items.push(...result.batches))
+    const items = await getWhaleApiClient(context).loan.listAuction(30, next)
     return {
       props: {
         auctions: {
           items,
-          pages: CursorPagination.getPages(context, response)
+          pages: CursorPagination.getPages(context, items)
         }
       }
     }
