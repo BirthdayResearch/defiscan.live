906c5ee02208aa5082dbaa2fb9bfa9d91372a361
Leonardo Venoso <leonardovenoso@Leonardos-MacBook-Pro.local>
2021-12-07 17:07:20 +0800

    Refactor DfTxTakeLoan. Changes styles and content.

--- cypress/integration/pages/transactions/DfTx/TakeLoan.spec.ts
+++ cypress/integration/pages/transactions/DfTx/TakeLoan.spec.ts
@@ -15,36 +15,17 @@ context('/transactions/[txid] - Take Loan on desktop', () => {
     cy.findByTestId('DfTxHeader.Subtitle').contains('OP_DEFI_TX_TAKE_LOAN')
   })
 
-  it('should have type number', () => {
-    cy.findByTestId('transaction-take-loan-type-number').contains('88')
-  })
-
   it('should have a vaultId', () => {
-    cy.findByTestId('transaction-take-loan-vaultId').contains('27a8209d6b851d9aa4b1402aa5e50368c40aaf360eb547a905e1d8b4c5a2e2d9')
+    cy.findByTestId('DfTxTakeLoan.VaultId').contains('27a8209d6b851d9aa4b1402aa5e50368c40aaf360eb547a905e1d8b4c5a2e2d9')
   })
 
-  it('should have a clickable vaultId', () => {
-    cy.findByTestId('transaction-take-loan-vaultId').children('a').contains('27a8209d6b851d9aa4b1402aa5e50368c40aaf360eb547a905e1d8b4c5a2e2d9')
-  })
-
-  it('should have a signature', () => {
-    cy.findByTestId('transaction-take-loan-signature').contains('1147556984')
-  })
-
-  it('should have data stack to', () => {
-    cy.findByTestId('transaction-take-loan-data-to-stack-type-0').contains('OP_HASH160')
-    cy.findByTestId('transaction-take-loan-data-to-stack-code-0').contains('169')
-
-    cy.findByTestId('transaction-take-loan-data-to-stack-type-1').contains('OP_PUSHDATA')
-    cy.findByTestId('transaction-take-loan-data-to-stack-code-1').contains('18c2a5b07a220de462e46aa32d704fc849bdb71d')
-
-    cy.findByTestId('transaction-take-loan-data-to-stack-type-2').contains('OP_EQUAL')
-    cy.findByTestId('transaction-take-loan-data-to-stack-code-2').contains('135')
+  it('should have an address to', () => {
+    cy.findByTestId('DfTxTakeLoan.Address').contains('dFgD1hmyZ2KitHzNLzvGoFLn548fqxZE7E')
   })
 
   it('should have a token amounts', () => {
-    cy.findByTestId('transaction-take-loan-data-tokenAmounts-token-0').contains('15')
-    cy.findByTestId('transaction-take-loan-data-tokenAmounts-amount-0').contains('25')
+    cy.findByTestId('DfTxTakeLoan.toAmount-0').contains('25.00000000')
+    cy.findByTestId('DfTxTakeLoan.toSymbol-0').contains('DUSD')
   })
 })
 
@@ -65,35 +46,16 @@ context('/transactions/[txid] - DfTx Token Create on mobile', () => {
     cy.findByTestId('DfTxHeader.Subtitle').contains('OP_DEFI_TX_TAKE_LOAN')
   })
 
-  it('should have type number', () => {
-    cy.findByTestId('transaction-take-loan-type-number').contains('88')
-  })
-
   it('should have a vaultId', () => {
-    cy.findByTestId('transaction-take-loan-vaultId').contains('27a8209d6b851d9aa4b1402aa5e50368c40aaf360eb547a905e1d8b4c5a2e2d9')
+    cy.findByTestId('DfTxTakeLoan.VaultId').contains('27a8209d6b851d9aa4b1402aa5e50368c40aaf360eb547a905e1d8b4c5a2e2d9')
   })
 
-  it('should have a clickable vaultId', () => {
-    cy.findByTestId('transaction-take-loan-vaultId').children('a')
-  })
-
-  it('should have a signature', () => {
-    cy.findByTestId('transaction-take-loan-signature').contains('1147556984')
-  })
-
-  it('should have data stack to', () => {
-    cy.findByTestId('transaction-take-loan-data-to-stack-type-0').contains('OP_HASH160')
-    cy.findByTestId('transaction-take-loan-data-to-stack-code-0').contains('169')
-
-    cy.findByTestId('transaction-take-loan-data-to-stack-type-1').contains('OP_PUSHDATA')
-    cy.findByTestId('transaction-take-loan-data-to-stack-code-1').contains('18c2a5b07a220de462e46aa32d704fc849bdb71d')
-
-    cy.findByTestId('transaction-take-loan-data-to-stack-type-2').contains('OP_EQUAL')
-    cy.findByTestId('transaction-take-loan-data-to-stack-code-2').contains('135')
+  it('should have an address to', () => {
+    cy.findByTestId('DfTxTakeLoan.Address').contains('dFgD1hmyZ2KitHzNLzvGoFLn548fqxZE7E')
   })
 
   it('should have a token amounts', () => {
-    cy.findByTestId('transaction-take-loan-data-tokenAmounts-token-0').contains('15')
-    cy.findByTestId('transaction-take-loan-data-tokenAmounts-amount-0').contains('25')
+    cy.findByTestId('DfTxTakeLoan.toAmount-0').contains('25.00000000')
+    cy.findByTestId('DfTxTakeLoan.toSymbol-0').contains('DUSD')
   })
 })
--- src/components/transactions/[txid]/DfTx/DfTxTakeLoan.tsx
+++ src/components/transactions/[txid]/DfTx/DfTxTakeLoan.tsx
@@ -1,84 +1,53 @@
 import { DfTx, TakeLoan } from '@defichain/jellyfish-transaction'
 import { AdaptiveList } from '@components/commons/AdaptiveList'
-import { Link } from '@components/commons/link/Link'
-import { DfTxHierarchicalSubtitle } from '@components/transactions/[txid]/DfTx/DfTxHierarchicalSubtitle'
-import { AdaptiveTable } from '@components/commons/AdaptiveTable'
-import { DfTxNotAvailableMessage } from '@components/transactions/[txid]/DfTx/DfTxNotAvailableMessage'
 import { DfTxHeader } from '@components/transactions/[txid]/DfTx/DfTxHeader'
+import { fromScript } from '@defichain/jellyfish-address'
+import { useNetwork } from '@contexts/NetworkContext'
+import { VaultLink } from '@components/commons/link/VaultLink'
+import { AddressLink } from '@components/commons/link/AddressLink'
+import { TokenSymbol } from '@components/commons/TokenSymbol'
 
 interface DfTxTakeLoanProps {
   dftx: DfTx<TakeLoan>
 }
 
-interface Stack {
-  type: string
-  code?: string
-  hex?: string
-}
-
 export function DfTxTakeLoan (props: DfTxTakeLoanProps): JSX.Element {
+  const address = fromScript(props.dftx.data.to, useNetwork().name)
+
   return (
     <>
       <DfTxHeader name={props.dftx.name} />
-      <AdaptiveList className='w-full mt-6'>
-        <AdaptiveList.Row name='Type number' testId='transaction-take-loan-type-number'>
-          {props.dftx.type}
+      <AdaptiveList className='mt-5 w-full lg:w-1/2'>
+        <AdaptiveList.Row name='Vault ID'>
+          <VaultLink
+            className='break-all'
+            vault={props.dftx.data.vaultId}
+            testId='DfTxTakeLoan.VaultId'
+          />
         </AdaptiveList.Row>
-        <AdaptiveList.Row name='Vault ID' testId='transaction-take-loan-vaultId'>
-          <Link href={{ pathname: `/vaults/${props.dftx.data.vaultId}` }}>
-            <a className='hover:text-primary-500 overflow-ellipsis overflow-hidden'>
-              {props.dftx.data.vaultId}
-            </a>
-          </Link>
+        <AdaptiveList.Row name='Address To'>
+          {(() => {
+            if (address?.address !== undefined) {
+              return (
+                <AddressLink
+                  address={address.address}
+                  className='break-all'
+                  testId='DfTxTakeLoan.Address'
+                />
+              )
+            }
+            return 'NA'
+          })()}
         </AdaptiveList.Row>
-        <AdaptiveList.Row name='Signature' testId='transaction-take-loan-signature'>
-          {props.dftx.signature}
-        </AdaptiveList.Row>
+        {props.dftx.data.tokenAmounts.map((tokenData, i) => (
+          <AdaptiveList.Row name='Amount' key={`token-amounts-${i}`}>
+            <div className='flex flex-row'>
+              <span data-testid={`DfTxTakeLoan.toAmount-${i}`}>{tokenData.amount.toFixed(8)}</span>
+              <TokenSymbol tokenId={tokenData.token} className='ml-1' testId={`DfTxTakeLoan.toSymbol-${i}`} />
+            </div>
+          </AdaptiveList.Row>
+        ))}
       </AdaptiveList>
-      <div className='ml-10'>
-        <DfTxHierarchicalSubtitle title='Stack' />
-        {props.dftx.data.to.stack.length > 0 && (
-          <AdaptiveTable className='lg:mt-6'>
-            <AdaptiveTable.Header>
-              <AdaptiveTable.Head>Type</AdaptiveTable.Head>
-              <AdaptiveTable.Head>Code/Hex</AdaptiveTable.Head>
-            </AdaptiveTable.Header>
-            {props.dftx.data.to.stack.map((stack: Stack, i) => (
-              <AdaptiveTable.Row key={`stack-to-row-${i}`}>
-                <AdaptiveTable.Cell title='Type' testId={`transaction-take-loan-data-to-stack-type-${i}`}>{stack.type}</AdaptiveTable.Cell>
-                <AdaptiveTable.Cell title='Code/Hex' testId={`transaction-take-loan-data-to-stack-code-${i}`}>{stack.code !== undefined ? stack.code : stack.hex}</AdaptiveTable.Cell>
-              </AdaptiveTable.Row>
-            ))}
-          </AdaptiveTable>
-        )}
-        {props.dftx.data.to.stack.length === 0 && (
-          <DfTxNotAvailableMessage notAvailableAttr='Stack' />
-        )}
-        <DfTxHierarchicalSubtitle title='Token Amounts' />
-        {props.dftx.data.tokenAmounts.length > 0 && (
-          <AdaptiveTable className='lg:mt-6'>
-            <AdaptiveTable.Header>
-              <AdaptiveTable.Head>Token</AdaptiveTable.Head>
-              <AdaptiveTable.Head>Amount</AdaptiveTable.Head>
-            </AdaptiveTable.Header>
-            {props.dftx.data.tokenAmounts.map((tokenData, i) => (
-              <AdaptiveTable.Row key={`tokenAmounts-row-${i}`}>
-                <AdaptiveTable.Cell title='Token number' testId={`transaction-take-loan-data-tokenAmounts-token-${i}`}>
-                  <Link href={{ pathname: `/tokens/${tokenData.token}` }}>
-                    <a>{tokenData.token.toString()}</a>
-                  </Link>
-                </AdaptiveTable.Cell>
-                <AdaptiveTable.Cell title='Amount' testId={`transaction-take-loan-data-tokenAmounts-amount-${i}`}>
-                  {tokenData.amount.toNumber()}
-                </AdaptiveTable.Cell>
-              </AdaptiveTable.Row>
-            ))}
-          </AdaptiveTable>
-        )}
-        {props.dftx.data.tokenAmounts.length === 0 && (
-          <DfTxNotAvailableMessage notAvailableAttr='Token' />
-        )}
-      </div>
     </>
   )
 }
