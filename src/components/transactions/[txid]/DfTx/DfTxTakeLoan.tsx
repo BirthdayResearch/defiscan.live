f53556fb157b8951757f44d6cad5b34a14d8e97a
Leonardo V <leonardo.venoso@live.com>
2021-12-07 22:33:49 +0800

    Leo/take loan (#686)
    * Display take loan attributes in transactions page. Removed unmapped json answer.
    
    * Fixed lint message: stack.code and stack.hex properties do not exist
    
    * Fixed lint message on CI: better written in dot notation
    
    * Added Stack interface to solve lint error: better code and hex in dot notation
    
    * Fixed lint message: cypress command return should not be assigned
    
    * Added title to cells for Stack and Token tables
    
    * Refactor DfTxTakeLoan. Changes styles and content.
    
    * Removed DfTxHierarchicalSubtitle.tsx
    
    Co-authored-by: Leonardo Venoso <leonardovenoso@Leonardos-MacBook-Pro.local>
    Co-authored-by: Joel-David Wong <git@joeldavid.dev>

---
+++ src/components/transactions/[txid]/DfTx/DfTxTakeLoan.tsx
@@ -0,0 +1,53 @@
+import { DfTx, TakeLoan } from '@defichain/jellyfish-transaction'
+import { AdaptiveList } from '@components/commons/AdaptiveList'
+import { DfTxHeader } from '@components/transactions/[txid]/DfTx/DfTxHeader'
+import { fromScript } from '@defichain/jellyfish-address'
+import { useNetwork } from '@contexts/NetworkContext'
+import { VaultLink } from '@components/commons/link/VaultLink'
+import { AddressLink } from '@components/commons/link/AddressLink'
+import { TokenSymbol } from '@components/commons/TokenSymbol'
+
+interface DfTxTakeLoanProps {
+  dftx: DfTx<TakeLoan>
+}
+
+export function DfTxTakeLoan (props: DfTxTakeLoanProps): JSX.Element {
+  const address = fromScript(props.dftx.data.to, useNetwork().name)
+
+  return (
+    <>
+      <DfTxHeader name={props.dftx.name} />
+      <AdaptiveList className='mt-5 w-full lg:w-1/2'>
+        <AdaptiveList.Row name='Vault ID'>
+          <VaultLink
+            className='break-all'
+            vault={props.dftx.data.vaultId}
+            testId='DfTxTakeLoan.VaultId'
+          />
+        </AdaptiveList.Row>
+        <AdaptiveList.Row name='Address To'>
+          {(() => {
+            if (address?.address !== undefined) {
+              return (
+                <AddressLink
+                  address={address.address}
+                  className='break-all'
+                  testId='DfTxTakeLoan.Address'
+                />
+              )
+            }
+            return 'NA'
+          })()}
+        </AdaptiveList.Row>
+        {props.dftx.data.tokenAmounts.map((tokenData, i) => (
+          <AdaptiveList.Row name='Amount' key={`token-amounts-${i}`}>
+            <div className='flex flex-row'>
+              <span data-testid={`DfTxTakeLoan.toAmount-${i}`}>{tokenData.amount.toFixed(8)}</span>
+              <TokenSymbol tokenId={tokenData.token} className='ml-1' testId={`DfTxTakeLoan.toSymbol-${i}`} />
+            </div>
+          </AdaptiveList.Row>
+        ))}
+      </AdaptiveList>
+    </>
+  )
+}
