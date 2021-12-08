95936f51d24f99c8072cf7d0f609de20920ab66f
Suraj Auwal <siradjiawoual@gmail.com>
2021-12-08 11:02:13 +0100

    Move Api call to client side

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,13 +3,14 @@ import { Block } from '@defichain/whale-api-client/dist/api/blocks'
 import { PoolPairData } from '@defichain/whale-api-client/dist/api/poolpairs'
 import { Transaction } from '@defichain/whale-api-client/dist/api/transactions'
 
-import { getWhaleApiClient } from '@contexts/WhaleContext'
+import { getWhaleApiClient, useWhaleApiClient } from '@contexts/WhaleContext'
 
 import { Container } from '@components/commons/Container'
 import { IndexHeader } from '@components/index/IndexHeader'
 import { LiquidityPoolList } from '@components/index/LiquidityPoolList'
 import { BlocksList } from '@components/index/BlocksList'
 import { TransactionsList } from '@components/index/TransactionsList'
+import { useEffect, useState } from 'react'
 
 interface HomePageProps {
   blocks: Block[]
@@ -18,19 +19,65 @@ interface HomePageProps {
 }
 
 export default function HomePage (props: InferGetServerSidePropsType<typeof getServerSideProps>): JSX.Element {
+  const [data, setData] = useState<HomePageProps>({
+    blocks: [],
+    transactions: [],
+    liquidityPools: []
+  })
+
+  const [clientSide, setClientSide] = useState<boolean>(false)
+  const api = useWhaleApiClient()
+
+  useEffect(() => {
+    void fetchData()
+    const interval = setInterval(() => {
+      void fetchData()
+    }, 15000)
+    return () => clearInterval(interval)
+  }, [])
+
+  async function fetchData (): Promise<void> {
+    const blocks = await api.blocks.list(8)
+
+    let transactions: Transaction[] = []
+    await Promise.all(
+      blocks.map(async block =>
+        await api.blocks.getTransactions(block.id, 8)
+          .then(results => {
+            return results
+          })
+      )
+    ).then(results => {
+      results.map(result =>
+        transactions.push(...result)
+      )
+    })
+    transactions = transactions.slice(0, 8)
+
+    const liquidityPools = await api.poolpairs.list(8)
+
+    setData({
+      transactions,
+      blocks,
+      liquidityPools
+    })
+
+    setClientSide(true)
+  }
+
   return (
     <>
       <IndexHeader />
       <Container className='pb-20'>
         <div className='flex flex-wrap -mx-2'>
           <div className='w-full lg:w-1/2 xl:w-3/5 px-1'>
-            <TransactionsList transactions={props.transactions} />
+            <TransactionsList transactions={clientSide ? data.transactions : props.transactions} />
           </div>
           <div className='w-full lg:w-1/2 xl:w-2/5 px-1'>
-            <BlocksList blocks={props.blocks} />
+            <BlocksList blocks={clientSide ? data.blocks : props.blocks} />
           </div>
         </div>
-        <LiquidityPoolList liquidityPools={props.liquidityPools} />
+        <LiquidityPoolList liquidityPools={clientSide ? data.liquidityPools : props.liquidityPools} />
       </Container>
     </>
   )
