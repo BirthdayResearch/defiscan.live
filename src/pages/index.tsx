f51ecbb8f307002589b7811e8d86b9e414b48dd7
Suraj Auwal <siradjiawoual@gmail.com>
2021-12-12 11:39:46 +0100

    Homepage props autorefresh (#709)
    * Move Api call to client side
    
    * Cleanup
    
    * Cleaned up code
    
    Co-authored-by: Joel-David Wong <git@joeldavid.dev>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,14 +2,13 @@ import { GetServerSidePropsContext, GetServerSidePropsResult, InferGetServerSide
 import { Block } from '@defichain/whale-api-client/dist/api/blocks'
 import { PoolPairData } from '@defichain/whale-api-client/dist/api/poolpairs'
 import { Transaction } from '@defichain/whale-api-client/dist/api/transactions'
-
-import { getWhaleApiClient } from '@contexts/WhaleContext'
-
+import { getWhaleApiClient, useWhaleApiClient } from '@contexts/WhaleContext'
 import { Container } from '@components/commons/Container'
 import { IndexHeader } from '@components/index/IndexHeader'
 import { LiquidityPoolList } from '@components/index/LiquidityPoolList'
 import { BlocksList } from '@components/index/BlocksList'
 import { TransactionsList } from '@components/index/TransactionsList'
+import { useEffect, useState } from 'react'
 
 interface HomePageProps {
   blocks: Block[]
@@ -18,19 +17,59 @@ interface HomePageProps {
 }
 
 export default function HomePage (props: InferGetServerSidePropsType<typeof getServerSideProps>): JSX.Element {
+  const api = useWhaleApiClient()
+  const [data, setData] = useState<HomePageProps>({
+    blocks: props.blocks,
+    transactions: props.transactions,
+    liquidityPools: props.liquidityPools
+  })
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      void fetchData()
+    }, 15000)
+    return () => clearInterval(interval)
+  }, [])
+
+  async function fetchData (): Promise<void> {
+    const blocks = await api.blocks.list(8)
+    let transactions: Transaction[] = []
+    await Promise.all(
+      blocks.map(async block =>
+        await api.blocks.getTransactions(block.id, 8)
+          .then(results => {
+            return results
+          })
+      )
+    ).then(results => {
+      results.map(result =>
+        transactions.push(...result)
+      )
+    })
+    transactions = transactions.slice(0, 8)
+
+    const liquidityPools = await api.poolpairs.list(8)
+
+    setData({
+      transactions,
+      blocks,
+      liquidityPools
+    })
+  }
+
   return (
     <>
       <IndexHeader />
       <Container className='pb-20'>
         <div className='flex flex-wrap -mx-2'>
           <div className='w-full lg:w-1/2 xl:w-3/5 px-1'>
-            <TransactionsList transactions={props.transactions} />
+            <TransactionsList transactions={data.transactions} />
           </div>
           <div className='w-full lg:w-1/2 xl:w-2/5 px-1'>
-            <BlocksList blocks={props.blocks} />
+            <BlocksList blocks={data.blocks} />
           </div>
         </div>
-        <LiquidityPoolList liquidityPools={props.liquidityPools} />
+        <LiquidityPoolList liquidityPools={data.liquidityPools} />
       </Container>
     </>
   )
