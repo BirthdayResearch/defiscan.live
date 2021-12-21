407e524eb4d3d0379720eca18b456ad1a2d56c38
Joel-David Wong <git@joeldavid.dev>
2021-12-21 17:23:25 +0800

    Added security headers to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,39 @@
+const securityHeaders = [
+  {
+    key: 'Content-Security-Policy',
+    value: "default-src 'none'; base-uri 'none'; form-action 'none'; frame-ancestors 'none'; img-src 'self'; media-src 'self'; object-src 'none'; script-src 'self'; style-src 'self' fonts.googleapis.com; font-src fonts.gstatic.com; connect-src 'self' ocean.defichain.com;\"",
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'same-origin',
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff',
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'DENY',
+  },
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block',
+  },
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=63072000; includeSubDomains; preload',
+  }
+]
+
 module.exports = {
   reactStrictMode: true,
-  pageExtensions: ['page.tsx', 'page.ts']
+  pageExtensions: ['page.tsx', 'page.ts'],
+  async headers () {
+    return [
+      {
+        source: '/(.*)',
+        headers: securityHeaders,
+      },
+    ]
+  },
 }
