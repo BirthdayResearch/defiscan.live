0a99f8659b4c300b6f745da5d474c10391a394db
Joel-David Wong <git@joeldavid.dev>
2021-12-21 18:08:03 +0800

    Cleaned up securityHeaders

--- next.config.js
+++ next.config.js
@@ -1,17 +1,17 @@
 const securityHeaders = [
   {
     key: 'Content-Security-Policy',
-    value: 'default-src \'none\';' +
-      'base-uri \'none\';' +
-      'form-action \'none\';' +
-      'frame-ancestors \'none\';' +
-      'img-src \'self\';' +
-      'media-src \'self\';' +
-      'object-src \'none\';' +
-      `script-src 'self' ${process.env.NODE_ENV === 'development' ? '\'unsafe-eval\'' : ''};` +
-      'style-src \'self\' fonts.googleapis.com \'unsafe-inline\';' +
-      'font-src fonts.gstatic.com;' +
-      'connect-src \'self\' ocean.defichain.com;',
+    value: `default-src 'none';` +
+      `base-uri 'none';` +
+      `form-action 'none';` +
+      `frame-ancestors 'none';` +
+      `img-src 'self';` +
+      `media-src 'self';` +
+      `object-src 'none';` +
+      `script-src 'self' ${process.env.NODE_ENV === 'development' ? `'unsafe-eval'` : ''};` +
+      `style-src 'self' fonts.googleapis.com 'unsafe-inline';` +
+      `font-src fonts.gstatic.com;` +
+      `connect-src 'self' ocean.defichain.com;`
   },
   {
     key: 'Referrer-Policy',
