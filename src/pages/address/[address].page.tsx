f9438fcb239b3ad95e94e76d671ba0d8c2524992
Joel-David Wong <4586209+joeldavidw@users.noreply.github.com>
2021-12-22 00:18:35 +0800

    Updated `address` Balances UI (#764)
    * Updated CollapsibleSection
    
    * Updated AddressTokenTable.tsx
    
    * Updated AddressTransactionTable heading
    
    * Updated CollapsibleSection header font weight
    
    * Renamed AddressTokenTable to AddressBalances
    
    * Refactored vaults utils
    
    * Cleaned up Address balances
    
    * Updated Tests

--- src/pages/address/[address].page.tsx
+++ src/pages/address/[address].page.tsx
@@ -3,11 +3,12 @@ import { Container } from '@components/commons/Container'
 import { AddressHeading, AddressNotFoundHeading } from './_components/AddressHeadings'
 import { AddressSummaryTable } from './_components/AddressSummaryTable'
 import { AddressTransactionTable } from './_components/AddressTransactionTable'
-import { AddressTokenTable } from './_components/AddressTokenTable'
+import { AddressBalances } from './_components/AddressBalances'
 import { fromAddress } from '@defichain/jellyfish-address'
 import { useNetwork } from '@contexts/NetworkContext'
 import { isAlphanumeric } from '../../utils/commons/StringValidator'
 import { Head } from '@components/commons/Head'
+import { CollapsibleSection } from '@components/commons/sections/CollapsibleSection'
 
 interface AddressPageProps {
   address: string
@@ -29,8 +30,23 @@ export default function AddressPage (props: InferGetServerSidePropsType<typeof g
       <Container className='pt-12 pb-20'>
         <AddressHeading address={props.address} />
         <AddressSummaryTable address={props.address} />
-        <AddressTokenTable address={props.address} />
-        <AddressTransactionTable address={props.address} />
+
+        <CollapsibleSection
+          heading='Balances'
+          className='mt-8'
+          testId='Balances'
+        >
+          <AddressBalances address={props.address} />
+        </CollapsibleSection>
+
+        <CollapsibleSection
+          heading='Transactions'
+          className='mt-8'
+          testId='Transactions'
+        >
+          <AddressTransactionTable address={props.address} />
+        </CollapsibleSection>
+
       </Container>
     </>
   )
