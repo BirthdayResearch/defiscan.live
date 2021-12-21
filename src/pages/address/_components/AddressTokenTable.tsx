5199d4070ad31c56b80c5c19e7fc2d7a1a52a609
Joel-David Wong <git@joeldavid.dev>
2021-12-21 13:39:44 +0800

    Updated AddressTokenTable.tsx

--- src/pages/address/_components/AddressTokenTable.tsx
+++ src/pages/address/_components/AddressTokenTable.tsx
@@ -1,11 +1,10 @@
 import { AddressToken } from '@defichain/whale-api-client/dist/api/address'
 import { getAssetIcon, getTokenIcon } from '@components/icons/assets/tokens'
-import { OverflowTable } from '@components/commons/OverflowTable'
 import { useWhaleApiClient } from '@contexts/WhaleContext'
 import { useEffect, useState } from 'react'
 import { CgSpinner } from 'react-icons/cg'
-import { Link } from '@components/commons/link/Link'
 import { PoolPairSymbol } from '@components/commons/PoolPairSymbol'
+import { Link } from '@components/commons/link/Link'
 
 interface AddressTokenTableProps {
   address: string
@@ -51,8 +50,7 @@ export function AddressTokenTable (props: AddressTokenTableProps): JSX.Element {
 
   if (isInitialLoad) {
     return (
-      <div className='mt-8 flex flex-wrap' data-testid='Balances'>
-        <span className='font-medium text-xl mb-2 text-gray-800' data-testid='Balances.title'>Balances</span>
+      <div className='flex flex-wrap' data-testid='Balances'>
         <div className='flex w-full h-40 items-center justify-center rounded p-4 border border-gray-100'>
           <CgSpinner size={32} className='animate-spin text-gray-600' />
         </div>
@@ -61,26 +59,15 @@ export function AddressTokenTable (props: AddressTokenTableProps): JSX.Element {
   }
 
   return (
-    <div className='mt-8 flex flex-wrap' data-testid='Balances'>
-      <span className='font-medium text-xl mb-2 text-gray-800' data-testid='Balances.title'>Balances</span>
+    <div className='flex flex-wrap' data-testid='Balances'>
       {tokensData.length > 0 ? (
-        <OverflowTable className='w-full'>
-          <OverflowTable.Header>
-            <OverflowTable.Head title='TOKEN' />
-            <OverflowTable.Head title='AMOUNT' />
-            <OverflowTable.Head title='NAME' />
-            <OverflowTable.Head title='CATEGORY' />
-          </OverflowTable.Header>
+        <div className='flex flex-wrap w-full -m-1'>
           {tokensData.map((token) => {
             return (
-              <Link href={{ pathname: `/tokens/${token.id}` }} key={token.id}>
-                <a className='contents'>
-                  <AddressTokenTableRow token={token} />
-                </a>
-              </Link>
+              <AddressTokenTableRow token={token} key={token.id} />
             )
           })}
-        </OverflowTable>
+        </div>
       ) : (<NoTokensInfo />)}
       <ShowMoreButton isLoading={isLoading} next={next} handleGetBalances={getBalances} />
     </div>
@@ -89,13 +76,20 @@ export function AddressTokenTable (props: AddressTokenTableProps): JSX.Element {
 
 function AddressTokenTableRow (props: { token: AddressToken }): JSX.Element {
   return (
-    <OverflowTable.Row>
-      <OverflowTable.Cell className='align-middle'>
-        <div className='flex items-center'>
-          <div className='my-auto'>
+    <div className='w-full md:w-1/3 lg:w-1/4 xl:w-1/5 p-1'>
+      <div className='flex flex-wrap p-3 rounded border border-gray-200'>
+        <div className='w-full flex  justify-between'>
+          <div className='flex items-center'>
             {(() => {
               if (props.token.isLPS) {
-                return <></>
+                return (
+                  <div className='mr-11'>
+                    <PoolPairSymbol
+                      poolPairId={props.token.id} symbolSizeClassName='h-6 w-6'
+                      symbolMarginClassName='ml-3.5' textClassName='hidden'
+                    />
+                  </div>
+                )
               }
 
               if (props.token.isDAT) {
@@ -106,45 +100,37 @@ function AddressTokenTableRow (props: { token: AddressToken }): JSX.Element {
               const TokenIcon = getTokenIcon(props.token.displaySymbol)
               return <TokenIcon className='h-6 w-6 mr-1' />
             })()}
+
+            <div className='text-gray-900 hover:text-primary-500'>
+              <Link href={{ pathname: `/tokens/${props.token.id}` }}>
+                <a className='contents'>
+                  {props.token.displaySymbol}{!props.token.isDAT && `#${props.token.id}`}
+                </a>
+              </Link>
+            </div>
           </div>
-          <div className='text-primary-500 group-hover:underline'>
-            {(() => {
-              if (props.token.isLPS) {
-                return (
-                  <PoolPairSymbol
-                    poolPairId={props.token.id} symbolSizeClassName='h-6 w-6'
-                    symbolMarginClassName='ml-3.5' textClassName='ml-11'
-                  />
-                )
-              }
-              return <>{props.token.displaySymbol}{!props.token.isDAT && `#${props.token.id}`}</>
-            })()}
+
+          <div className='bg-gray-200 p-1 rounded'>
+            <div className='text-xs font-medium text-gray-600'>
+              {(() => {
+                if (props.token.isLPS) {
+                  return 'LPS'
+                }
+                if (props.token.isDAT) {
+                  return 'DAT'
+                }
+                return 'DCT'
+              })()}
+            </div>
           </div>
         </div>
-      </OverflowTable.Cell>
-      <OverflowTable.Cell>
-        {props.token.amount}
-      </OverflowTable.Cell>
-      <OverflowTable.Cell>
-        {(() => {
-          if (props.token.isDAT) {
-            return props.token.name.replace('Default Defi token', 'DeFiChain')
-          }
-          return props.token.name
-        })()}
-      </OverflowTable.Cell>
-      <OverflowTable.Cell className='align-middle'>
-        {(() => {
-          if (props.token.isLPS) {
-            return 'LPS'
-          }
-          if (props.token.isDAT) {
-            return 'DAT'
-          }
-          return 'DCT'
-        })()}
-      </OverflowTable.Cell>
-    </OverflowTable.Row>
+
+        <div className='w-full mt-2.5 text-gray-900 font-medium'>
+          {props.token.amount}
+        </div>
+      </div>
+    </div>
+
   )
 }
 
