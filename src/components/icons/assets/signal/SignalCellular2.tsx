40190b439f8fbd1be854a3afd4c6914a00b3baa8
Joel-David Wong <4586209+joeldavidw@users.noreply.github.com>
2021-12-23 18:02:28 +0800

    Fixed SVGs (#777)

--- src/components/icons/assets/signal/SignalCellular2.tsx
+++ src/components/icons/assets/signal/SignalCellular2.tsx
@@ -3,8 +3,8 @@ import { SVGProps } from 'react'
 export function SignalCellular2 (props: SVGProps<SVGSVGElement>): JSX.Element {
   return (
     <svg width='1em' height='1em' viewBox='0 0 24 24' fill='currentColor' {...props}>
-      <path d='M19.5,5.5V18.5H17.5V5.5H19.5M21,9H9V20H14V9M7,14H2V20H7V14Z' />
-      <path d='M21,4H16V20H21V4M14' fill='#cecece' />
+      <path d='M21,9H9V20H14V9M7,14H2V20H7V14Z' />
+      <path d='M21,4H16V20H21V4' fill='#cecece' />
     </svg>
   )
 }
