3fbeff81a9969628d25057913ffafb608896629c
Ahto Türkson <ahto.tyrkson2@gmail.com>
2022-01-15 21:18:45 +0200

    Updated all token symbols

---
+++ src/components/icons/assets/tokens/_TokenDefault.tsx
@@ -0,0 +1,33 @@
+import { SVGProps } from 'react'
+
+export function _TokenDefault (symbol: string): (props: SVGProps<SVGSVGElement>) => JSX.Element {
+  function TokenDefaultSymbol (props: SVGProps<SVGSVGElement>): JSX.Element {
+    const bg = '#000000'
+    const text = 'white'
+
+    return (
+      <svg width='1em' height='1em' viewBox='0 0 32 32' {...props}>
+        <path
+          fill={bg}
+          d='M16 0C7.163 0 0 7.163 0 16s7.163 16 16 16c8.838 0 16-7.163 16-16S24.838 0 16 0z'
+        />
+        <text
+          className='pointer-events-none'
+          dominantBaseline='central'
+          fill={text}
+          fontSize={8}
+          fontWeight='bolder'
+          textAnchor='middle'
+          lengthAdjust='spacingAndGlyphs'
+          textLength={30}
+          x='50%'
+          y='50%'
+        >
+          {symbol}
+        </text>
+      </svg>
+    )
+  }
+
+  return TokenDefaultSymbol
+}
