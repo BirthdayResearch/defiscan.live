5662b22d7e8cf253887e52811f1e504a1e14c5ec
Joel-David Wong <git@joeldavid.dev>
2022-01-18 14:00:14 +0800

    Removed unnecessary Transition from HoverPopover

--- src/components/commons/popover/HoverPopover.tsx
+++ src/components/commons/popover/HoverPopover.tsx
@@ -1,5 +1,4 @@
 import { JSX } from '@babel/types'
-import { Transition } from '@headlessui/react'
 import { PropsWithChildren, ReactNode, useState } from 'react'
 import classNames from 'classnames'
 import { Placement, useFloating } from '@floating-ui/react-dom'
@@ -36,41 +35,31 @@ export function HoverPopover (props: PropsWithChildren<IconPopoverProps>): JSX.E
         {props.children}
       </div>
 
-      <Transition
-        enter='transition ease-out duration-200'
-        enterFrom='opacity-0 translate-y-1'
-        enterTo='opacity-100 translate-y-0'
-        leave='transition ease-in duration-150'
-        leaveFrom='opacity-100 translate-y-0'
-        leaveTo='opacity-0 translate-y-1'
-        show
-      >
-        {(() => {
-          if (!isHover) {
-            return null
-          }
+      {(() => {
+        if (!isHover) {
+          return null
+        }
 
-          return (
-            <div
-              ref={floating}
-              style={{
-                position: strategy,
-                top: y ?? '',
-                left: x ?? ''
-              }}
-              className='p-2 z-20'
-            >
-              {typeof props.popover === 'string' ? (
-                <div
-                  className='px-4 py-3 font-normal text-sm bg-white text-left text-gray-900 rounded-lg border border-gray-100 shadow-md max-w-xs'
-                >
-                  {props.popover}
-                </div>
-              ) : props.popover}
-            </div>
-          )
-        })()}
-      </Transition>
+        return (
+          <div
+            ref={floating}
+            style={{
+              position: strategy,
+              top: y ?? '',
+              left: x ?? ''
+            }}
+            className='p-2 z-20'
+          >
+            {typeof props.popover === 'string' ? (
+              <div
+                className='px-4 py-3 font-normal text-sm bg-white text-left text-gray-900 rounded-lg border border-gray-100 shadow-md max-w-xs'
+              >
+                {props.popover}
+              </div>
+            ) : props.popover}
+          </div>
+        )
+      })()}
     </>
   )
 }
