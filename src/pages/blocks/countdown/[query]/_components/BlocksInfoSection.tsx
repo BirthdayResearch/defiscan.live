8510562115ef9d5acf0715f6bf6b6e9a1f3c9ee2
Joel-David Wong <4586209+joeldavidw@users.noreply.github.com>
2022-01-30 04:01:05 +0800

    Updated events and ui (#876)

--- src/pages/blocks/countdown/[query]/_components/BlocksInfoSection.tsx
+++ src/pages/blocks/countdown/[query]/_components/BlocksInfoSection.tsx
@@ -1,5 +1,6 @@
 import { MdStairs } from 'react-icons/md'
 import React from 'react'
+import NumberFormat from 'react-number-format'
 
 export function BlocksInfoSection (props: { remaining: number, current: number }): JSX.Element {
   return (
@@ -9,14 +10,30 @@ export function BlocksInfoSection (props: { remaining: number, current: number }
           <div className='rounded border p-2 flex items-center'>
             <MdStairs className='text-gray-400 inline-block' size={22} />
             <span className='text-gray-500 ml-1' data-testid='BlocksInfoSection.Current.Label'>Current Height:</span>
-            <span className='text-gray-900 ml-1' data-testid='BlocksInfoSection.Current.Value'>{props.current}</span>
+            <span className='text-gray-900 ml-1' data-testid='BlocksInfoSection.Current.Value'>
+              <NumberFormat
+                value={props.current}
+                displayType='text'
+                thousandSeparator
+              />
+            </span>
           </div>
         </div>
         <div className='w-full md:w-1/2 md:py-0 md:pr-2 md:pl-1'>
           <div className='rounded border p-2 flex items-center'>
             <MdStairs className='text-gray-400 inline-block' size={22} />
-            <span className='text-gray-500 ml-1' data-testid='BlocksInfoSection.Remaining.Label'>Remaining Blocks:</span>
-            <span className='text-gray-900 ml-1' data-testid='BlocksInfoSection.Remaining.Value'>{props.remaining}</span>
+            <span
+              className='text-gray-500 ml-1'
+              data-testid='BlocksInfoSection.Remaining.Label'
+            >Remaining Blocks:
+            </span>
+            <span className='text-gray-900 ml-1' data-testid='BlocksInfoSection.Remaining.Value'>
+              <NumberFormat
+                value={props.remaining}
+                displayType='text'
+                thousandSeparator
+              />
+            </span>
           </div>
         </div>
       </div>
