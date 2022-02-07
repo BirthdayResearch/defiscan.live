aa51910fb08e183728883bdafdb1a5d065aacbe1
Joel-David Wong <git@joeldavid.dev>
2022-02-07 12:21:47 +0800

    Updated block announcement

--- src/components/commons/banner/WarningBanner.tsx
+++ src/components/commons/banner/WarningBanner.tsx
@@ -1,13 +1,14 @@
+import { PropsWithChildren } from 'react'
+
 interface EmptySectionProps {
-  message: string
   className?: string
   testId?: string
 }
 
-export function WarningBanner (props: EmptySectionProps): JSX.Element {
+export function WarningBanner (props: PropsWithChildren<EmptySectionProps>): JSX.Element {
   return (
     <div className='bg-orange-100 rounded p-3 text-center' data-testid={props.testId}>
-      {props.message}
+      {props.children}
     </div>
   )
 }
