4a0c2e285fd9d97772744bb8fa5d07bc56ea2419
Suraj Auwal <siradjiawoual@gmail.com>
2022-02-21 14:18:01 +0100

    Added spacing and padding

--- src/components/index/IndexStats.tsx
+++ src/components/index/IndexStats.tsx
@@ -7,7 +7,7 @@ import { CollapsibleSection } from '@components/commons/sections/CollapsibleSect
 
 export function IndexStats (): JSX.Element {
   return (
-    <section>
+    <section className='mb-12'>
       <div className='hidden md:flex flex-row items-start'>
         <StatPriceCard />
         <div className='flex flex-wrap w-full'>
@@ -16,25 +16,41 @@ export function IndexStats (): JSX.Element {
             heading='Total DFI Minted'
             stat='711,565,082'
             suffix='/1.2B'
-          />
+          >
+            <div className='mt-auto text-gray-500'>
+              <span className='font-semibold'>56%</span> from max supply
+            </div>
+          </StatCard>
+          <StatCard
+            infodesc='Total DFI Minted  is the total number of DFI emitted to date.'
+            heading='Total DFI Minted'
+            stat='711,565,082'
+            suffix='DFI'
+          >
+            <div className='mt-auto text-gray-500'>
+              <span className='font-semibold'>56%</span> from max supply
+            </div>
+          </StatCard>
           <StatCard
             infodesc='Total DFI Minted  is the total number of DFI emitted to date.'
             heading='Total DFI Minted'
             stat='711,565,082'
             suffix='/1.2B'
-          />
+          >
+            <div className='mt-auto text-gray-500'>
+              <span className='font-semibold'>56%</span> from max supply
+            </div>
+          </StatCard>
           <StatCard
             infodesc='Total DFI Minted  is the total number of DFI emitted to date.'
             heading='Total DFI Minted'
             stat='711,565,082'
             suffix='/1.2B'
-          />
-          <StatCard
-            infodesc='Total DFI Minted  is the total number of DFI emitted to date.'
-            heading='Total DFI Minted'
-            stat='711,565,082'
-            suffix='/1.2B'
-          />
+          >
+            <div className='mt-auto text-gray-500'>
+              <span className='font-semibold'>56%</span> from max supply
+            </div>
+          </StatCard>
         </div>
       </div>
       <CollapsibleSection className='flex flex-col md:hidden' heading='Overview'>
@@ -45,25 +61,41 @@ export function IndexStats (): JSX.Element {
             heading='Total DFI Minted'
             stat='711,565,082'
             suffix='/1.2B'
-          />
+          >
+            <div className='mt-auto text-gray-500'>
+              <span className='font-semibold'>56%</span> from max supply
+            </div>
+          </StatCard>
           <StatCard
             infodesc='Total DFI Minted  is the total number of DFI emitted to date.'
             heading='Total DFI Minted'
             stat='711,565,082'
             suffix='/1.2B'
-          />
+          >
+            <div className='mt-auto text-gray-500'>
+              <span className='font-semibold'>56%</span> from max supply
+            </div>
+          </StatCard>
           <StatCard
             infodesc='Total DFI Minted  is the total number of DFI emitted to date.'
             heading='Total DFI Minted'
             stat='711,565,082'
             suffix='/1.2B'
-          />
+          >
+            <div className='mt-auto text-gray-500'>
+              <span className='font-semibold'>56%</span> from max supply
+            </div>
+          </StatCard>
           <StatCard
             infodesc='Total DFI Minted  is the total number of DFI emitted to date.'
             heading='Total DFI Minted'
             stat='711,565,082'
             suffix='/1.2B'
-          />
+          >
+            <div className='mt-auto text-gray-500'>
+              <span className='font-semibold'>56%</span> from max supply
+            </div>
+          </StatCard>
         </div>
       </CollapsibleSection>
     </section>
@@ -86,7 +118,7 @@ function StatPriceCard (): JSX.Element {
           </div>
         </div>
       </div>
-      <span className='mt-auto font-normal text-lg text-gray-200'>
+      <span className='mt-auto text-gray-500 font-normal text-lg'>
         Updated at 23:26:13 UTC+8
       </span>
     </div>
@@ -98,7 +130,7 @@ function StatCard (props: PropsWithChildren<{infodesc: string, heading: string, 
     <div className='md:pl-2 md:pb-2 w-full md:w-1/2'>
       <div className='flex flex-col border border-gray-200 py-4 px-6 h-[7.5rem]  rounded-lg'>
         <div className='flex items-center'>
-          <span className='font-normal '>{props.heading}</span>
+          <span className='font-normal mr-2'>{props.heading}</span>
           <InfoHoverPopover description={props.infodesc} />
         </div>
         <div className='flex flex-wrap items-center'>
