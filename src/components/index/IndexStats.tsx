1c7a6cb43ccc1261e1f090ba9ae9bfaba12651e7
Suraj Auwal <siradjiawoual@gmail.com>
2022-02-21 13:56:11 +0100

    Stat section implementation

---
+++ src/components/index/IndexStats.tsx
@@ -0,0 +1,117 @@
+import { DFI as DfiIcon } from '@components/icons/assets/tokens/DFI'
+import { BsArrowUp } from 'react-icons/bs'
+import { PropsWithChildren } from 'react'
+import { InfoHoverPopover } from '@components/commons/popover/InfoHoverPopover'
+import ReactNumberFormat from 'react-number-format'
+import { CollapsibleSection } from '@components/commons/sections/CollapsibleSection'
+
+export function IndexStats (): JSX.Element {
+  return (
+    <section>
+      <div className='hidden md:flex flex-row items-start'>
+        <StatPriceCard />
+        <div className='flex flex-wrap w-full'>
+          <StatCard
+            infodesc='Total DFI Minted  is the total number of DFI emitted to date.'
+            heading='Total DFI Minted'
+            stat='711,565,082'
+            suffix='/1.2B'
+          />
+          <StatCard
+            infodesc='Total DFI Minted  is the total number of DFI emitted to date.'
+            heading='Total DFI Minted'
+            stat='711,565,082'
+            suffix='/1.2B'
+          />
+          <StatCard
+            infodesc='Total DFI Minted  is the total number of DFI emitted to date.'
+            heading='Total DFI Minted'
+            stat='711,565,082'
+            suffix='/1.2B'
+          />
+          <StatCard
+            infodesc='Total DFI Minted  is the total number of DFI emitted to date.'
+            heading='Total DFI Minted'
+            stat='711,565,082'
+            suffix='/1.2B'
+          />
+        </div>
+      </div>
+      <CollapsibleSection className='flex flex-col md:hidden' heading='Overview'>
+        <StatPriceCard />
+        <div className='flex flex-wrap w-full space-y-2 mt-4'>
+          <StatCard
+            infodesc='Total DFI Minted  is the total number of DFI emitted to date.'
+            heading='Total DFI Minted'
+            stat='711,565,082'
+            suffix='/1.2B'
+          />
+          <StatCard
+            infodesc='Total DFI Minted  is the total number of DFI emitted to date.'
+            heading='Total DFI Minted'
+            stat='711,565,082'
+            suffix='/1.2B'
+          />
+          <StatCard
+            infodesc='Total DFI Minted  is the total number of DFI emitted to date.'
+            heading='Total DFI Minted'
+            stat='711,565,082'
+            suffix='/1.2B'
+          />
+          <StatCard
+            infodesc='Total DFI Minted  is the total number of DFI emitted to date.'
+            heading='Total DFI Minted'
+            stat='711,565,082'
+            suffix='/1.2B'
+          />
+        </div>
+      </CollapsibleSection>
+    </section>
+  )
+}
+
+function StatPriceCard (): JSX.Element {
+  return (
+    <div className='rounded-xl border border-gray-200 lg:shrink-0 p-8 w-full md:w-[21rem] flex flex-col h-[15.5rem]'>
+      <div className='flex flex-col space-y-3'>
+        <div className='flex items-center'>
+          <DfiIcon fontSize={32} />
+          <div className='font-normal ml-2'>$DFI Price</div>
+        </div>
+        <div className='flex items-center'>
+          <h1 className='font-semibold text-5xl'>$3.70</h1>
+          <div className='flex text-green-500 items-center text-lg font-normal ml-3'>
+            <BsArrowUp />
+            <span>1.34%</span>
+          </div>
+        </div>
+      </div>
+      <span className='mt-auto font-normal text-lg text-gray-200'>
+        Updated at 23:26:13 UTC+8
+      </span>
+    </div>
+  )
+}
+
+function StatCard (props: PropsWithChildren<{infodesc: string, heading: string, stat: string, suffix?: string}>): JSX.Element {
+  return (
+    <div className='md:pl-2 md:pb-2 w-full md:w-1/2'>
+      <div className='flex flex-col border border-gray-200 py-4 px-6 h-[7.5rem]  rounded-lg'>
+        <div className='flex items-center'>
+          <span className='font-normal '>{props.heading}</span>
+          <InfoHoverPopover description={props.infodesc} />
+        </div>
+        <div className='flex flex-wrap items-center'>
+          <ReactNumberFormat
+            value={props.stat}
+            displayType='text'
+            thousandSeparator
+            className='text-2xl font-semibold'
+          />
+          <span>{props.suffix}</span>
+        </div>
+        {props.children}
+      </div>
+    </div>
+  )
+}
--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -9,6 +9,7 @@ import { LiquidityPoolList } from '@components/index/LiquidityPoolList'
 import { BlocksList } from '@components/index/BlocksList'
 import { TransactionsList } from '@components/index/TransactionsList'
 import { useEffect, useState } from 'react'
+import { IndexStats } from '@components/index/IndexStats'
 
 interface HomePageProps {
   blocks: Block[]
@@ -61,6 +62,7 @@ export default function HomePage (props: InferGetServerSidePropsType<typeof getS
     <>
       <IndexHeader />
       <Container className='pb-20'>
+        <IndexStats />
         <div className='flex flex-wrap -mx-2'>
           <div className='w-full lg:w-3/5 xl:w-2/3 px-1'>
             <TransactionsList transactions={data.transactions} />
