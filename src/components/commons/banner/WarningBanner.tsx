d13db01b2ed1fa23dcc976a53eb79e99c7858c82
Joel-David Wong <git@joeldavid.dev>
2022-02-28 17:24:33 +0800

    Added Testnet notification

--- src/components/commons/banner/WarningBanner.tsx
+++ src/components/commons/banner/WarningBanner.tsx
@@ -1,11 +1,28 @@
 import { PropsWithChildren } from 'react'
+import { useNetwork } from '@contexts/NetworkContext'
 
-interface EmptySectionProps {
+interface WarningBannerProps {
   className?: string
   testId?: string
+  testnet?: boolean
+  mainnet?: boolean
 }
 
-export function WarningBanner (props: PropsWithChildren<EmptySectionProps>): JSX.Element {
+export function WarningBanner (props: PropsWithChildren<WarningBannerProps>): JSX.Element {
+  const network = useNetwork().name
+
+  if (props.mainnet === undefined && props.testnet === undefined) {
+    return <></>
+  }
+
+  if (network !== 'mainnet' && props.mainnet!) {
+    return <></>
+  }
+
+  if (network !== 'testnet' && props.testnet!) {
+    return <></>
+  }
+
   return (
     <div className='bg-orange-100 rounded p-3 text-center' data-testid={props.testId}>
       {props.children}
