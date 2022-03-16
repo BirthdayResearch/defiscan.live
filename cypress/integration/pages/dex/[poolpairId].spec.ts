d59b8870ecbd542190a705f6a1e06e90a3462f1e
Suraj Auwal <siradjiawoual@gmail.com>
2022-03-16 11:33:03 +0100

    Fixed Linting issue
    Signed-off-by: Suraj Auwal <siradjiawoual@gmail.com>

--- cypress/integration/pages/dex/[poolpairId].spec.ts
+++ cypress/integration/pages/dex/[poolpairId].spec.ts
@@ -16,7 +16,7 @@ context('/dex/[poolpairid] on macbook-16', () => {
   })
 
   it('should poolpair details card information', function () {
-    cy.findByTestId('PoolPairDetails').should('be.visible').within( () => {
+    cy.findByTestId('PoolPairDetails').should('be.visible').within(() => {
       cy.findByTestId('PoolPairDetails.Price').should('be.visible').within(() => {
         cy.findByTestId('Price.TokenA').should('be.visible')
         cy.findByTestId('Price.TokenB').should('be.visible')
@@ -38,35 +38,34 @@ context('/dex/[poolpairid] on macbook-16', () => {
   })
 
   it('should have OverflowTable header information', function () {
-      cy.findByTestId('OverflowTable.Header').then(ele => {
-        cy.wrap(ele).findByText('Tx ID').should('be.visible')
-        cy.wrap(ele).findByText('Age').should('be.visible')
-        cy.wrap(ele).findByText('From').should('be.visible')
-        cy.wrap(ele).findByText('To').should('be.visible')
-        cy.wrap(ele).findByText('Amount').should('be.visible')
-      })
+    cy.findByTestId('OverflowTable.Header').then(ele => {
+      cy.wrap(ele).findByText('Tx ID').should('be.visible')
+      cy.wrap(ele).findByText('Age').should('be.visible')
+      cy.wrap(ele).findByText('From').should('be.visible')
+      cy.wrap(ele).findByText('To').should('be.visible')
+      cy.wrap(ele).findByText('Amount').should('be.visible')
     })
+  })
 
-    it('should have dex info in OverflowTable', function () {
-      cy.findAllByTestId('OverflowTable.Row').eq(1).then(ele => {
-        cy.wrap(ele).children().should('have.length', 5)
-      })
+  it('should have dex info in OverflowTable', function () {
+    cy.findAllByTestId('OverflowTable.Row').eq(1).then(ele => {
+      cy.wrap(ele).children().should('have.length', 5)
     })
+  })
 
-    it('should CursorPagination.Next', function () {
-      cy.findAllByTestId('OverflowTable.Cell').then((ele) => {
-        cy.scrollTo('bottom')
-        const pageOneFirstCell = ele[0].innerText
-        cy.interceptServerSideWait(() => {
-          cy.findByTestId('CursorPagination.Next').click()
-        })
-        cy.findAllByTestId('OverflowTable.Cell').then((pageTwoCells) => {
-          expect(pageTwoCells[0].innerText).not.equals(pageOneFirstCell)
-        })
+  it('should CursorPagination.Next', function () {
+    cy.findAllByTestId('OverflowTable.Cell').then((ele) => {
+      cy.scrollTo('bottom')
+      const pageOneFirstCell = ele[0].innerText
+      cy.interceptServerSideWait(() => {
+        cy.findByTestId('CursorPagination.Next').click()
+      })
+      cy.findAllByTestId('OverflowTable.Cell').then((pageTwoCells) => {
+        expect(pageTwoCells[0].innerText).not.equals(pageOneFirstCell)
       })
     })
+  })
 })
-
 
 context('/dex/[poolpairId] on iphone-x', () => {
   before(() => {
@@ -77,35 +76,35 @@ context('/dex/[poolpairId] on iphone-x', () => {
     cy.viewport('iphone-x')
   })
 
-it('should have PoolPair Details Bar information', function () {
-  cy.findByTestId('PoolPairDetailsBar').within(() => {
-    cy.findByTestId('PoolPairSymbol').should('be.visible').should('contain.text', 'dBTC-DFI')
-    cy.findByTestId('PriceRatio').should('be.visible')
-    cy.findByTestId('TokenUsdPrice').should('be.visible')
+  it('should have PoolPair Details Bar information', function () {
+    cy.findByTestId('PoolPairDetailsBar').within(() => {
+      cy.findByTestId('PoolPairSymbol').should('be.visible').should('contain.text', 'dBTC-DFI')
+      cy.findByTestId('PriceRatio').should('be.visible')
+      cy.findByTestId('TokenUsdPrice').should('be.visible')
+    })
   })
-})
 
-it('should poolpair details card information', function () {
-  cy.findByTestId('PoolPairDetails').should('be.visible').within( () => {
-    cy.findByTestId('PoolPairDetails.Price').should('be.visible').within(() => {
-      cy.findByTestId('Price.TokenA').should('be.visible')
-      cy.findByTestId('Price.TokenB').should('be.visible')
-    })
-    cy.findByTestId('PoolPairDetails.Liquidity').should('be.visible').within(() => {
-      cy.findByTestId('24hVolume').should('be.visible')
-      cy.findByTestId('TVL').should('be.visible')
-    })
-    cy.findByTestId('PoolPairDetails.Pool').should('be.visible').within(() => {
-      cy.findByTestId('Pool.TokenA').should('be.visible')
-      cy.findByTestId('Pool.TokenB').should('be.visible')
-    })
-    cy.findByTestId('PoolPairDetails.Apr').should('be.visible').within(() => {
-      cy.findByTestId('APR').should('be.visible')
-      cy.findByTestId('Rewards').should('be.visible')
-      cy.findByTestId('Commissions').should('be.visible')
+  it('should poolpair details card information', function () {
+    cy.findByTestId('PoolPairDetails').should('be.visible').within(() => {
+      cy.findByTestId('PoolPairDetails.Price').should('be.visible').within(() => {
+        cy.findByTestId('Price.TokenA').should('be.visible')
+        cy.findByTestId('Price.TokenB').should('be.visible')
+      })
+      cy.findByTestId('PoolPairDetails.Liquidity').should('be.visible').within(() => {
+        cy.findByTestId('24hVolume').should('be.visible')
+        cy.findByTestId('TVL').should('be.visible')
+      })
+      cy.findByTestId('PoolPairDetails.Pool').should('be.visible').within(() => {
+        cy.findByTestId('Pool.TokenA').should('be.visible')
+        cy.findByTestId('Pool.TokenB').should('be.visible')
+      })
+      cy.findByTestId('PoolPairDetails.Apr').should('be.visible').within(() => {
+        cy.findByTestId('APR').should('be.visible')
+        cy.findByTestId('Rewards').should('be.visible')
+        cy.findByTestId('Commissions').should('be.visible')
+      })
     })
   })
-})
 
   it('should have CardList', function () {
     cy.findAllByTestId('SwapCard').within(() => {
@@ -132,18 +131,18 @@ it('should poolpair details card information', function () {
         cy.findByTestId('CardList.Row.Child').should('be.visible')
       })
     })
-})
+  })
 
-it('should CursorPagination.Next', function () {
-  cy.findAllByTestId('OverflowTable.Cell').then((ele) => {
-    cy.scrollTo('bottom')
-    const pageOneFirstCell = ele[0].innerText
-    cy.interceptServerSideWait(() => {
-      cy.findByTestId('CursorPagination.Next').click()
-    })
-    cy.findAllByTestId('OverflowTable.Cell').then((pageTwoCells) => {
-      expect(pageTwoCells[0].innerText).not.equals(pageOneFirstCell)
+  it('should CursorPagination.Next', function () {
+    cy.findAllByTestId('OverflowTable.Cell').then((ele) => {
+      cy.scrollTo('bottom')
+      const pageOneFirstCell = ele[0].innerText
+      cy.interceptServerSideWait(() => {
+        cy.findByTestId('CursorPagination.Next').click()
+      })
+      cy.findAllByTestId('OverflowTable.Cell').then((pageTwoCells) => {
+        expect(pageTwoCells[0].innerText).not.equals(pageOneFirstCell)
+      })
     })
   })
-})
 })
