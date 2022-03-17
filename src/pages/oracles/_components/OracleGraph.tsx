0d7ea494c8dd50240107f0ba215372a44fad9a54
Nichelle <nichelle@cakedefi.com>
2022-03-17 21:14:31 +0800

    Update to whale indexed api to fetch prices in chosen intervals

--- src/pages/oracles/_components/OracleGraph.tsx
+++ src/pages/oracles/_components/OracleGraph.tsx
@@ -1,22 +1,22 @@
 import { JSX } from '@babel/types'
 import { useWhaleApiClient } from '@contexts/WhaleContext'
-import { ApiPagedResponse, WhaleApiClient } from '@defichain/whale-api-client'
+import { WhaleApiClient } from '@defichain/whale-api-client'
 import { PriceFeed, PriceTicker } from '@defichain/whale-api-client/dist/api/prices'
 import { format } from 'date-fns'
 import React, { MouseEventHandler, useEffect, useState } from 'react'
 import { Area, AreaChart, CartesianGrid, ResponsiveContainer, Tooltip, TooltipProps, XAxis, YAxis } from 'recharts'
 import classNames from 'classnames'
 import BigNumber from 'bignumber.js'
+import { CgSpinner } from 'react-icons/cg'
 
 interface PriceGraphProps {
   price: PriceTicker
 }
 
 export enum GraphPeriod {
-  ONE_DAY = '1D',
+  ONE_DAY = '24H',
   ONE_WEEK = '7D',
   ONE_MONTH = '30D',
-  ONE_YEAR = '1Y'
 }
 
 export function OracleGraph ({
@@ -30,21 +30,21 @@ export function OracleGraph ({
   const api = useWhaleApiClient()
   const [feed, setFeed] = useState<PriceFeed[] | undefined>(undefined)
   const [graphPeriod, setGraphPeriod] = useState<GraphPeriod>(GraphPeriod.ONE_DAY)
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+  const oneHour = 60 * 60
 
   useEffect(() => {
+    setIsLoading(true)
     switch (graphPeriod) {
-      case GraphPeriod.ONE_YEAR:
-        void fetchTimeFramePriceFeed(api, token, currency, 365 * 24 * 60 * 60).then(setFeed)
-        break
       case GraphPeriod.ONE_MONTH:
-        void fetchTimeFramePriceFeed(api, token, currency, 30 * 24 * 60 * 60).then(setFeed)
+        void fetchTimeFramePriceFeed(api, token, currency, 30 * 24 * oneHour, oneHour).then(setFeed).finally(() => setIsLoading(false))
         break
       case GraphPeriod.ONE_WEEK:
-        void fetchTimeFramePriceFeed(api, token, currency, 7 * 24 * 60 * 60).then(setFeed)
+        void fetchTimeFramePriceFeed(api, token, currency, 7 * 24 * oneHour, oneHour).then(setFeed).finally(() => setIsLoading(false))
         break
       case GraphPeriod.ONE_DAY:
       default:
-        void fetchTimeFramePriceFeed(api, token, currency, 24 * 60 * 60).then(setFeed)
+        void fetchTimeFramePriceFeed(api, token, currency, 24 * oneHour).then(setFeed).finally(() => setIsLoading(false))
         break
     }
   }, [graphPeriod])
@@ -62,27 +62,27 @@ export function OracleGraph ({
         maxHeight: '80vh'
       }}
     >
-      <div className='pt-2 pr-2 md:pr-4 flex flex-col'>
-        <div
-          className='flex justify-end flex-wrap text-sm mt-8 lg:mt-0 space-x-0 lg:space-x-4 space-y-4 lg:space-y-0'
-        >
-          <div className='flex lg:max-w-max flex-wrap -mx-0.5'>
-            <GraphPeriodButton
-              current={graphPeriod} graphPeriod={GraphPeriod.ONE_DAY}
-              onClick={() => setGraphPeriod(GraphPeriod.ONE_DAY)}
-            />
-            <GraphPeriodButton
-              current={graphPeriod} graphPeriod={GraphPeriod.ONE_WEEK}
-              onClick={() => setGraphPeriod(GraphPeriod.ONE_WEEK)}
-            />
-            <GraphPeriodButton
-              current={graphPeriod} graphPeriod={GraphPeriod.ONE_MONTH}
-              onClick={() => setGraphPeriod(GraphPeriod.ONE_MONTH)}
-            />
-          </div>
+      <div
+        className='flex justify-end flex-wrap text-sm mt-4 mr-10 space-x-0 lg:space-x-4 space-y-4 lg:space-y-0'
+      >
+        <div className='flex lg:max-w-max flex-wrap'>
+          <GraphPeriodButton
+            current={graphPeriod} graphPeriod={GraphPeriod.ONE_DAY}
+            onClick={() => setGraphPeriod(GraphPeriod.ONE_DAY)}
+          />
+          <GraphPeriodButton
+            current={graphPeriod} graphPeriod={GraphPeriod.ONE_WEEK}
+            onClick={() => setGraphPeriod(GraphPeriod.ONE_WEEK)}
+          />
+          <GraphPeriodButton
+            current={graphPeriod} graphPeriod={GraphPeriod.ONE_MONTH}
+            onClick={() => setGraphPeriod(GraphPeriod.ONE_MONTH)}
+          />
         </div>
       </div>
-      <PriceAreaChart feed={feed} current={graphPeriod} />
+      {isLoading
+        ? (<Spinner />)
+        : (<PriceAreaChart feed={feed} current={graphPeriod} />)}
     </div>
   )
 }
@@ -98,7 +98,7 @@ function GraphPeriodButton ({
 }): JSX.Element {
   return (
     <div
-      className={classNames('rounded p-2 border cursor-pointer mx-0.5 mt-1 lg:mt-0', graphPeriod === current ? 'text-white bg-primary-500 border-primary-500' : 'border-gray-300 text-gray-900 hover:bg-primary-50')}
+      className={classNames('rounded p-2 border cursor-pointer mx-0.5 mt-1 lg:mt-0', graphPeriod === current ? 'text-primary-500 bg-primary-100 border-primary-100' : 'text-gray-900 bg-gray-200 border-gray-200  hover:bg-primary-50 hover:border-primary-50')}
       onClick={onClick}
       key={graphPeriod}
     >
@@ -118,8 +118,6 @@ function PriceAreaChart ({
 
   function formatXAxis (tickItem): string {
     switch (current) {
-      case GraphPeriod.ONE_YEAR:
-        return format(tickItem, 'MM')
       case GraphPeriod.ONE_MONTH:
         return format(tickItem, 'dd/MM')
       case GraphPeriod.ONE_WEEK:
@@ -141,7 +139,7 @@ function PriceAreaChart ({
 
     return `$${
       value.dividedBy(Math.pow(1000, places))
-        .toFormat(1, {
+        .toFormat(2, {
           decimalSeparator: '.',
           suffix: units[places * 3]
         })
@@ -229,17 +227,26 @@ function TooltipDialog ({ payload }: TooltipProps<any, any>): JSX.Element | null
   )
 }
 
-async function fetchTimeFramePriceFeed (api: WhaleApiClient, token: string, currency: string, timeRange: number): Promise<PriceFeed[]> {
+function Spinner (): JSX.Element {
+  return (
+    <div className='flex w-full justify-center grid h-full content-center'>
+      <CgSpinner size={54} className='animate-spin text-primary-500' />
+    </div>
+  )
+}
+
+async function fetchTimeFramePriceFeed (api: WhaleApiClient, token: string, currency: string, timeRange: number, interval?: number): Promise<PriceFeed[]> {
   const prices: PriceFeed[] = []
   const after = (Date.now() / 1000) - timeRange
+  interval ??= 15 * 60 // default to 15 mins if undefined
+  const dataPoints = timeRange / interval
 
-  let response: ApiPagedResponse<PriceFeed> = await api.prices.getFeed(token, currency, 60)
+  let response = await api.prices.getFeedWithInterval(token, currency, interval, dataPoints < 200 ? dataPoints : 200)
   prices.push(...response)
-
-  while (response.hasNext && response[response.length - 1].block.time > after) {
-    response = await api.paginate(response)
+  while (response.hasNext && prices.length < dataPoints) {
+    response = await api.prices.getFeedWithInterval(token, currency, interval, 200, response.nextToken)
     prices.push(...response)
   }
 
-  return prices.reverse()
+  return prices.filter(p => p.block.medianTime > after).reverse()
 }
