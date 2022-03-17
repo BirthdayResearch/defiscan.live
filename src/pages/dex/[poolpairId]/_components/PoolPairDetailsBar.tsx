28d792057f6f4f2ab26741ff68f0e47a2d7c8996
Suraj Auwal <siradjiawoual@gmail.com>
2022-03-17 03:09:38 +0100

    Fixed PoolPairDetailsBar
    Signed-off-by: Suraj Auwal <siradjiawoual@gmail.com>

--- src/pages/dex/[poolpairId]/_components/PoolPairDetailsBar.tsx
+++ src/pages/dex/[poolpairId]/_components/PoolPairDetailsBar.tsx
@@ -15,29 +15,27 @@ export function PoolPairDetailsBar (props: {poolpair: PoolPairData}): JSX.Elemen
         textClassName='ml-7 md:ml-16 font-normal text-lg md:text-xl'
         testId='PoolPairSymbol'
       />
-      <div className='mt-6 md:ml-auto md:mt-0'>
-        <div className='flex text-primary-900 lg:items-baseline' data-testid='PriceRatio'>
-          <span className='mr-2 text-sm font-normal'>
-            {`1${props.poolpair.tokenA.displaySymbol}  =`}
-          </span>
-          <div className='flex flex-col'>
-            <ReactNumberFormat
-              value={props.poolpair.priceRatio.ba}
-              suffix={` ${props.poolpair.tokenB.displaySymbol}`}
-              displayType='text'
-              thousandSeparator
-              className='text-sm font-medium md:text-2xl'
-              decimalScale={Number(props.poolpair.priceRatio.ba) > 1 ? 2 : 8}
-              fixedDecimalScale
-            />
-            <ReactNumberFormat
-              className='text-sm flex justify-end text-gray-400'
-              displayType='text'
-              thousandSeparator
-              prefix='≈$'
-              value={getTokenPrice(props.poolpair.tokenB.displaySymbol, props.poolpair.priceRatio.ba).toFixed(3)}
-            />
-          </div>
+      <div className='flex text-primary-900 md:items-baseline mt-6 md:ml-auto md:mt-0' data-testid='PriceRatio'>
+        <span className='mr-2 text-sm font-normal'>
+          {`1${props.poolpair.tokenA.displaySymbol}  =`}
+        </span>
+        <div className='flex flex-col'>
+          <ReactNumberFormat
+            value={props.poolpair.priceRatio.ba}
+            suffix={` ${props.poolpair.tokenB.displaySymbol}`}
+            displayType='text'
+            thousandSeparator
+            className='text-sm font-medium md:text-2xl'
+            decimalScale={Number(props.poolpair.priceRatio.ba) > 1 ? 2 : 8}
+            fixedDecimalScale
+          />
+          <ReactNumberFormat
+            className='text-sm flex justify-end text-gray-400'
+            displayType='text'
+            thousandSeparator
+            prefix='≈$'
+            value={getTokenPrice(props.poolpair.tokenB.displaySymbol, props.poolpair.priceRatio.ba).toFixed(3)}
+          />
         </div>
       </div>
     </div>
