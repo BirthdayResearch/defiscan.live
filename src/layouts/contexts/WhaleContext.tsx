01184a6295370dc308ff00d399678f2e2154de9d
Joel-David Wong <4586209+joeldavidw@users.noreply.github.com>
2022-03-18 15:39:03 +0800

    Bumped whale to 0.31 (#1051)

--- src/layouts/contexts/WhaleContext.tsx
+++ src/layouts/contexts/WhaleContext.tsx
@@ -70,14 +70,14 @@ function newWhaleClient (connection?: string | NetworkConnection): WhaleApiClien
       return new WhaleApiClient({
         url: 'https://ocean.defichain.com',
         network: 'testnet',
-        version: 'v0.27'
+        version: 'v0.31'
       })
     case NetworkConnection.MainNet:
     default:
       return new WhaleApiClient({
         url: 'https://ocean.defichain.com',
         network: 'mainnet',
-        version: 'v0.27'
+        version: 'v0.31'
       })
   }
 }
@@ -89,12 +89,12 @@ function newRpcClient (connection?: string | NetworkConnection): WhaleRpcClient 
     case NetworkConnection.RemotePlayground:
       return new WhaleRpcClient('https://playground.defichain.com/v0/regtest/rpc')
     case NetworkConnection.TestNet: {
-      const version = 'v0.27'
+      const version = 'v0.31'
       return new WhaleRpcClient(`https://ocean.defichain.com/${version}/testnet/rpc`)
     }
     case NetworkConnection.MainNet:
     default: {
-      const version = 'v0.27'
+      const version = 'v0.31'
       return new WhaleRpcClient(`https://ocean.defichain.com/${version}/mainnet/rpc`)
     }
   }
