c837a573cae17cd731fa8541ebb9b4039492f65f
Nichelle <8347352+nichellekoh@users.noreply.github.com>
2022-03-21 19:19:20 +0800

    Added graph period button for 1D, 7D & 30D (#1042)
    * Added graph period button for 1D, 7D & 30D
    
    * Update to whale indexed api to fetch prices in chosen intervals
    
    * Update to display bucket start time for graph hover over
    
    * Update Oracles Graph tests
    
    * Update Oracles Graph to show 90d and change x-axis format to dd MMM
    
    * Update tests and y-axis for Oracles Graph
    
    * converted data point to number
    
    * Fix graph range for min & max
    
    * Updated graph popover
    
    Co-authored-by: Joel-David Wong <git@joeldavid.dev>

--- src/pages/oracles/_components/OracleGraph.tsx
+++ src/pages/oracles/_components/OracleGraph.tsx
@@ -1,13 +1,24 @@
 import { JSX } from '@babel/types'
 import { useWhaleApiClient } from '@contexts/WhaleContext'
-import { ApiPagedResponse, WhaleApiClient } from '@defichain/whale-api-client'
-import { PriceFeed, PriceTicker } from '@defichain/whale-api-client/dist/api/prices'
+import { WhaleApiClient } from '@defichain/whale-api-client'
+import { PriceFeedInterval, PriceTicker } from '@defichain/whale-api-client/dist/api/prices'
 import { format } from 'date-fns'
-import { useEffect, useState } from 'react'
+import React, { MouseEventHandler, useEffect, useState } from 'react'
 import { Area, AreaChart, CartesianGrid, ResponsiveContainer, Tooltip, TooltipProps, XAxis, YAxis } from 'recharts'
+import classNames from 'classnames'
+import BigNumber from 'bignumber.js'
+import { CgSpinner } from 'react-icons/cg'
+import ReactNumberFormat from 'react-number-format'
 
 interface PriceGraphProps {
   price: PriceTicker
+}
+
+export enum GraphPeriod {
+  ONE_DAY = '24H',
+  ONE_WEEK = '7D',
+  ONE_MONTH = '30D',
+  THREE_MONTHS = '90D',
 }
 
 export function OracleGraph ({
@@ -19,11 +30,30 @@ export function OracleGraph ({
   }
 }: PriceGraphProps): JSX.Element {
   const api = useWhaleApiClient()
-  const [feed, setFeed] = useState<PriceFeed[] | undefined>(undefined)
+  const [feed, setFeed] = useState<PriceFeedInterval[] | undefined>(undefined)
+  const [graphPeriod, setGraphPeriod] = useState<GraphPeriod>(GraphPeriod.THREE_MONTHS)
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+  const oneHour = 60 * 60
+  const oneDay = 24 * 60 * 60
 
   useEffect(() => {
-    void fetch24HourPriceFeed(api, token, currency).then(setFeed)
-  }, [])
+    setIsLoading(true)
+    switch (graphPeriod) {
+      case GraphPeriod.THREE_MONTHS:
+        void fetchTimeFramePriceFeedInterval(api, token, currency, 90 * 24 * oneHour, oneDay).then(setFeed).finally(() => setIsLoading(false))
+        break
+      case GraphPeriod.ONE_MONTH:
+        void fetchTimeFramePriceFeedInterval(api, token, currency, 30 * 24 * oneHour, oneHour).then(setFeed).finally(() => setIsLoading(false))
+        break
+      case GraphPeriod.ONE_WEEK:
+        void fetchTimeFramePriceFeedInterval(api, token, currency, 7 * 24 * oneHour, oneHour).then(setFeed).finally(() => setIsLoading(false))
+        break
+      case GraphPeriod.ONE_DAY:
+      default:
+        void fetchTimeFramePriceFeedInterval(api, token, currency, 24 * oneHour).then(setFeed).finally(() => setIsLoading(false))
+        break
+    }
+  }, [graphPeriod])
 
   if (feed === undefined) {
     return (
@@ -32,17 +62,105 @@ export function OracleGraph ({
   }
 
   return (
-    <PriceAreaChart feed={feed} />
+    <div
+      className='rounded-lg flex flex-col bg-gray-50' style={{
+        height: '32rem',
+        maxHeight: '80vh'
+      }}
+    >
+      <div
+        className='flex justify-end flex-wrap text-sm mt-4 mr-10 space-x-0 lg:space-x-4 space-y-4 lg:space-y-0'
+      >
+        <div className='flex lg:max-w-max flex-wrap'>
+          <GraphPeriodButton
+            current={graphPeriod} graphPeriod={GraphPeriod.ONE_DAY}
+            onClick={() => setGraphPeriod(GraphPeriod.ONE_DAY)}
+          />
+          <GraphPeriodButton
+            current={graphPeriod} graphPeriod={GraphPeriod.ONE_WEEK}
+            onClick={() => setGraphPeriod(GraphPeriod.ONE_WEEK)}
+          />
+          <GraphPeriodButton
+            current={graphPeriod} graphPeriod={GraphPeriod.ONE_MONTH}
+            onClick={() => setGraphPeriod(GraphPeriod.ONE_MONTH)}
+          />
+          <GraphPeriodButton
+            current={graphPeriod} graphPeriod={GraphPeriod.THREE_MONTHS}
+            onClick={() => setGraphPeriod(GraphPeriod.THREE_MONTHS)}
+          />
+        </div>
+      </div>
+      {isLoading
+        ? (<Spinner />)
+        : (<PriceAreaChart feed={feed} current={graphPeriod} />)}
+    </div>
   )
 }
 
-function PriceAreaChart ({ feed }: { feed: PriceFeed[] }): JSX.Element {
+function GraphPeriodButton ({
+  current,
+  graphPeriod,
+  onClick
+}: {
+  current: GraphPeriod
+  graphPeriod: GraphPeriod
+  onClick: MouseEventHandler<HTMLDivElement>
+}): JSX.Element {
+  return (
+    <div
+      data-testid='Oracles.GraphPeriodButton'
+      className={classNames('rounded p-2 border cursor-pointer mx-0.5 mt-1 lg:mt-0', graphPeriod === current ? 'text-primary-500 bg-primary-100 border-primary-100' : 'text-gray-900 bg-gray-200 border-gray-200  hover:bg-primary-50 hover:border-primary-50')}
+      onClick={onClick}
+      key={graphPeriod}
+    >
+      {graphPeriod}
+    </div>
+  )
+}
+
+function PriceAreaChart ({
+  feed,
+  current
+}: { feed: PriceFeedInterval[], current: GraphPeriod }): JSX.Element {
   const data = feed.map(value => ({
     feed: value,
-    time: value.block.medianTime * 1000
+    data: Number(value.aggregated.amount),
+    time: value.aggregated.time.start * 1000
   }))
+
+  function formatXAxis (tickItem): string {
+    switch (current) {
+      case GraphPeriod.THREE_MONTHS:
+        return format(tickItem, 'dd MMM')
+      case GraphPeriod.ONE_MONTH:
+        return format(tickItem, 'dd MMM')
+      case GraphPeriod.ONE_WEEK:
+        return format(tickItem, 'dd MMM')
+      case GraphPeriod.ONE_DAY:
+      default:
+        return format(tickItem, 'hh aa')
+    }
+  }
+
+  function formatYAxis (tickItem): string {
+    const units = {
+      3: 'k',
+      6: 'm',
+      9: 'b'
+    }
+    let value = new BigNumber(tickItem)
+    const places = Math.floor(value.e! / 3)
+    if (value.isGreaterThanOrEqualTo(new BigNumber(1))) {
+      value = value.dividedBy(Math.pow(1000, places))
+    }
+    return `$${value.toFormat(2, {
+      decimalSeparator: '.',
+      suffix: units[places * 3]
+    })}`
+  }
+
   return (
-    <ResponsiveContainer width='100%' height='100%' className='bg-gray-50 rounded-md'>
+    <ResponsiveContainer width='100%' height='100%' className='rounded-md'>
       <AreaChart
         width={600}
         height={400}
@@ -71,26 +189,27 @@ function PriceAreaChart ({ feed }: { feed: PriceFeed[] }): JSX.Element {
           tickMargin={12}
           scale='time'
           domain={['dataMin', 'dataMax']}
-          tickFormatter={time => format(time, 'hh:mm aa')}
+          tickFormatter={formatXAxis}
         />
         <YAxis
-          dataKey='feed.aggregated.amount'
+          dataKey='data'
           type='number'
           allowDataOverflow
           tickMargin={12}
           scale='linear'
-          domain={[m => (m * 0.99).toPrecision(3), m => (m * 1.01).toPrecision(3)]}
-          tickFormatter={value => value.toPrecision(4)}
+          domain={[dataMin => (dataMin * 0.99).toPrecision(3), dataMax => (dataMax * 1.01).toPrecision(3)]}
+          tickFormatter={formatYAxis}
         />
 
         <Tooltip content={props => <TooltipDialog {...props} />} />
 
         <Area
           type='monotone'
-          dataKey='feed.aggregated.amount'
+          dataKey='data'
           stroke='#ff00af'
           strokeWidth={2}
           fill='url(#gradient)'
+          id='oraclesGraphArea'
         />
       </AreaChart>
     </ResponsiveContainer>
@@ -98,41 +217,66 @@ function PriceAreaChart ({ feed }: { feed: PriceFeed[] }): JSX.Element {
 }
 
 function TooltipDialog ({ payload }: TooltipProps<any, any>): JSX.Element | null {
-  const feed = payload?.[0]?.payload.feed as PriceFeed
+  const feed = payload?.[0]?.payload.feed as PriceFeedInterval
   if (feed === undefined) {
     return null
   }
 
   function Row (props: { title: string, content: any }): JSX.Element {
     return (
-      <div className='table-row'>
-        <div className='table-cell opacity-60 pr-3 py-0.5'>{props.title}:</div>
-        <div className='table-cell'>{props.content}</div>
+      <div className='flex flex-wrap mt-1.5 text-gray-900'>
+        <div className='w-full text-gray-500 text-sm'>{props.title}</div>
+        <div className='font-medium'>{props.content}</div>
       </div>
     )
   }
 
   return (
-    <div className='table p-5 rounded shadow-lg bg-white ring-1 ring-gray-500 ring-opacity-5'>
-      <Row title='Block' content={feed.block.height} />
-      <Row title='Date' content={format(feed.block.medianTime * 1000, 'MMM dd, hh:mm:ss aa')} />
-      <Row title='Price' content={feed.aggregated.amount} />
-      <Row title='Oracles' content={`${feed.aggregated.oracles.active}/${feed.aggregated.oracles.total} responded`} />
+    <div className='table px-4 py-3 rounded shadow-lg bg-white ring-1 ring-gray-500 ring-opacity-5'>
+      <div className='font-medium text-gray-900'>
+        {format(feed.aggregated.time.start * 1000, 'MMM dd, hh:mm:ss aa')}
+      </div>
+      <Row
+        title='Price' content={
+          <ReactNumberFormat
+            displayType='text'
+            thousandSeparator
+            value={feed.aggregated.amount}
+            decimalScale={2}
+            prefix='$'
+            suffix=' USD'
+            data-testid='LiquidityCardStat.Liquidity.Value'
+          />
+}
+      />
+      <Row
+        title='Oracles'
+        content={`${Math.round(feed.aggregated.oracles.active * 100) / 100}/${feed.aggregated.oracles.total} responded`}
+      />
     </div>
   )
 }
 
-async function fetch24HourPriceFeed (api: WhaleApiClient, token: string, currency: string): Promise<PriceFeed[]> {
-  const after = (Date.now() / 1000) - (24 * 60 * 60)
-  const prices: PriceFeed[] = []
+function Spinner (): JSX.Element {
+  return (
+    <div className='flex w-full justify-center grid h-full content-center' data-testid='Oracles.Spinner'>
+      <CgSpinner size={54} className='animate-spin text-primary-500' />
+    </div>
+  )
+}
 
-  let response: ApiPagedResponse<PriceFeed> = await api.prices.getFeed(token, currency, 60)
+async function fetchTimeFramePriceFeedInterval (api: WhaleApiClient, token: string, currency: string, timeRange: number, interval?: number): Promise<PriceFeedInterval[]> {
+  const prices: PriceFeedInterval[] = []
+  const after = (Date.now() / 1000) - timeRange
+  interval ??= 15 * 60 // default to 15 mins if undefined
+  const dataPoints = timeRange / interval
+
+  let response = await api.prices.getFeedWithInterval(token, currency, interval, dataPoints < 200 ? dataPoints : 200)
   prices.push(...response)
-
-  while (response.hasNext && response[response.length - 1].block.time > after) {
-    response = await api.paginate(response)
+  while (response.hasNext && prices.length < dataPoints) {
+    response = await api.prices.getFeedWithInterval(token, currency, interval, 200, response.nextToken)
     prices.push(...response)
   }
 
-  return prices.reverse()
+  return prices.filter(p => p.block.medianTime > after).reverse()
 }
