ab5f764a2c170564fc8c7952461aaa26c2c96e89
Harsh <harshrathi.dev@gmail.com>
2022-06-14 08:43:44 +0530

    chore(ui-ux): replaced div with next image tag

--- src/components/index/IndexHeader.tsx
+++ src/components/index/IndexHeader.tsx
@@ -1,6 +1,7 @@
 import { Container } from '@components/commons/Container'
 import { useSelector } from 'react-redux'
 import { RootState } from '@store/index'
+import Image from 'next/image'
 import { UnitSuffix } from '@components/commons/UnitSuffix'
 import ReactNumberFormat from 'react-number-format'
 import React, { PropsWithChildren } from 'react'
@@ -14,9 +15,13 @@ export function IndexHeader (): JSX.Element {
   return (
     <>
       <Stats />
-      <HeaderWrapper bgImage={theme === 'dark' ? 'url(\'/assets/hero/fortcanningDark.svg\')' : 'linear-gradient(180deg, rgba(255, 255, 255, 0) 25.4%, #FFFFFF 94.76%), url(\'/assets/hero/fortcanning.svg\')'}>
+      <HeaderWrapper
+        title='header-image'
+        gradient={theme === 'dark' ? 'linear-gradient(rgba(23, 23, 23, 0) 25.4%, rgb(23, 23, 23) 94.76%)' : 'linear-gradient(rgba(255, 255, 255, 0) 25.4%, rgb(255, 255, 255) 94.76%)'}
+        bgImage={theme === 'dark' ? '/assets/hero/fortcanningDark.svg' : '/assets/hero/fortcanning.svg'}
+      >
         <Container className='h-full'>
-          <div className='h-full flex flex-wrap items-center justify-center mt-14 mb-16'>
+          <div className='h-full flex flex-wrap items-center justify-center pt-14 pb-16'>
             <h1 className='text-4xl lg:text-5xl w-full mb-6 font-medium dark:text-dark-gray-900 text-center' data-testid='Header.title'>
               DeFiChain <span className='font-normal dark:text-gray-100'>Explorer</span>
             </h1>
@@ -28,13 +33,24 @@ export function IndexHeader (): JSX.Element {
   )
 }
 
-function HeaderWrapper (props: PropsWithChildren<{bgImage: string}>): JSX.Element {
+function HeaderWrapper (props: PropsWithChildren<{bgImage: string, title: string, gradient: string}>): JSX.Element {
   return (
-    <div
-      className='flex flex-col items-center pb-24 -mb-24'
-      style={{ backgroundImage: props.bgImage, backgroundSize: 'cover', backgroundPosition: 'right center' }}
-    >
-      {props.children}
+    <div className='relative pb-24 -mb-24'>
+      <Image
+        layout='fill'
+        alt={props.title}
+        src={props.bgImage}
+        className='object-center object-cover pointer-events-none'
+      />
+      <div
+        className='absolute h-full w-full'
+        style={{
+          background: props.gradient
+        }}
+      />
+      <div className='relative z-1'>
+        {props.children}
+      </div>
     </div>
   )
 }
