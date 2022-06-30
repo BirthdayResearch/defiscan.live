b8d5d9bfb235b42fab697cf0a54cfb9c297e6241
JJ Adonis <josejaimecareers@gmail.com>
2022-06-30 16:42:32 +0800

    chore(release): rc/1.2.0  (#1283)
    * feat(ui-ux): add number format on token amount (#1280)
    
    feature(ui-ux): add number format on token amount
    
    * feat(ui-ux): `DEX` - Pool Pair page - swap history - add padding left on token symbol (#1282)
    
    feat(ui-ux): DEX - Pool Pair page - swap history - add padding left on token symbol
    
    * chore(deps): updated next sitemap
    
    * bump(deps): bump @prismicio/client from 6.5.1 to 6.6.1 (#1276)
    
    Bumps [@prismicio/client](https://github.com/prismicio/prismic-client) from 6.5.1 to 6.6.1.
    - [Release notes](https://github.com/prismicio/prismic-client/releases)
    - [Changelog](https://github.com/prismicio/prismic-client/blob/master/CHANGELOG.md)
    - [Commits](https://github.com/prismicio/prismic-client/compare/v6.5.1...v6.6.1)
    
    ---
    updated-dependencies:
    - dependency-name: "@prismicio/client"
      dependency-type: direct:production
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * bump(deps-dev): bump @netlify/plugin-nextjs from 4.7.1 to 4.9.3 (#1284)
    
    Bumps [@netlify/plugin-nextjs](https://github.com/netlify/netlify-plugin-nextjs) from 4.7.1 to 4.9.3.
    - [Release notes](https://github.com/netlify/netlify-plugin-nextjs/releases)
    - [Changelog](https://github.com/netlify/netlify-plugin-nextjs/blob/main/CHANGELOG.md)
    - [Commits](https://github.com/netlify/netlify-plugin-nextjs/compare/plugin-nextjs-v4.7.1...plugin-nextjs-v4.9.3)
    
    ---
    updated-dependencies:
    - dependency-name: "@netlify/plugin-nextjs"
      dependency-type: direct:development
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * chore(deps): update next
    
    * breaking e2e tests
    
    * update packages
    
    * chore(ui-ux): replaced div with next image tag for header component (#1252)
    
    * chore(ui-ux): replaced div with next image tag
    
    * updated image type and used import for caching the images
    
    * updated bg image
    
    * updated bg images
    
    * add svgs
    
    Co-authored-by: thedoublejay <josejaimecareers@gmail.com>
    
    * bump docker to 2.49.0
    
    * chore(deps): bump packages
    
    Co-authored-by: Chloe <44501120+chloezxyy@users.noreply.github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: Harsh R <53080940+fullstackninja864@users.noreply.github.com>

--- src/components/index/IndexHeader.tsx
+++ src/components/index/IndexHeader.tsx
@@ -1,6 +1,7 @@
 import { Container } from '@components/commons/Container'
 import { useSelector } from 'react-redux'
 import { RootState } from '@store/index'
+import Image, { StaticImageData } from 'next/image'
 import { UnitSuffix } from '@components/commons/UnitSuffix'
 import ReactNumberFormat from 'react-number-format'
 import React, { PropsWithChildren } from 'react'
@@ -8,16 +9,25 @@ import { StatItem } from '@components/commons/stats/StatItem'
 import { StatsBar } from '@components/commons/stats/StatsBar'
 import { SearchBar } from '@components/commons/searchbar/SearchBar'
 import { useTheme } from '@contexts/ThemeContext'
+import FortCanningDark from '@public/assets/hero/fortcanningDark.svg'
+import FortCanning from '@public/assets/hero/fortcanning.svg'
 
 export function IndexHeader (): JSX.Element {
   const { theme } = useTheme()
   return (
     <>
       <Stats />
-      <HeaderWrapper bgImage={theme === 'dark' ? 'url(\'/assets/hero/fortcanningDark.svg\')' : 'linear-gradient(180deg, rgba(255, 255, 255, 0) 25.4%, #FFFFFF 94.76%), url(\'/assets/hero/fortcanning.svg\')'}>
+      <HeaderWrapper
+        title='header-image'
+        gradient={theme === 'dark' ? 'linear-gradient(rgba(23, 23, 23, 0) 25.4%, rgb(23, 23, 23) 94.76%)' : 'linear-gradient(rgba(255, 255, 255, 0) 25.4%, rgb(255, 255, 255) 94.76%)'}
+        bgImage={theme === 'dark' ? FortCanningDark : FortCanning}
+      >
         <Container className='h-full'>
-          <div className='h-full flex flex-wrap items-center justify-center mt-14 mb-16'>
-            <h1 className='text-4xl lg:text-5xl w-full mb-6 font-medium dark:text-dark-gray-900 text-center' data-testid='Header.title'>
+          <div className='h-full flex flex-wrap items-center justify-center pt-14 pb-16'>
+            <h1
+              className='text-4xl lg:text-5xl w-full mb-6 font-medium dark:text-dark-gray-900 text-center'
+              data-testid='Header.title'
+            >
               DeFiChain <span className='font-normal dark:text-gray-100'>Explorer</span>
             </h1>
             <SearchBar atHeader={false} />
@@ -28,13 +38,26 @@ export function IndexHeader (): JSX.Element {
   )
 }
 
-function HeaderWrapper (props: PropsWithChildren<{bgImage: string}>): JSX.Element {
+function HeaderWrapper (props: PropsWithChildren<{ bgImage: StaticImageData, title: string, gradient: string }>): JSX.Element {
   return (
-    <div
-      className='flex flex-col items-center pb-24 -mb-24'
-      style={{ backgroundImage: props.bgImage, backgroundSize: 'cover', backgroundPosition: 'right center' }}
-    >
-      {props.children}
+    <div className='relative pb-24 -mb-24'>
+      <Image
+        layout='fill'
+        alt={props.title}
+        src={props.bgImage}
+        objectFit='cover'
+        objectPosition='center'
+        className='pointer-events-none'
+      />
+      <div
+        className='absolute h-full w-full'
+        style={{
+          background: props.gradient
+        }}
+      />
+      <div className='relative z-1'>
+        {props.children}
+      </div>
     </div>
   )
 }
