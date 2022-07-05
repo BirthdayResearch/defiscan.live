1c61383668e9e519546a05185b8657f00847a5a2
Chloezxyy <chloezhengxy@gmail.com>
2022-07-05 14:13:14 +0800

    feat(ui-ux): code refactor for warning banner

--- src/components/commons/banner/WarningBanner.tsx
+++ src/components/commons/banner/WarningBanner.tsx
@@ -17,11 +17,11 @@ export function WarningBanner (props: PropsWithChildren<WarningBannerProps>): JS
   const deFiChainStatusUrl = 'https://status.defichain.com/'
 
   const blockchainIsDownContent: Announcement = {
-    content: 'We are currently investigating a syncing issue on the blockchain. ',
+    content: 'We are currently investigating a syncing issue on the blockchain.',
     url: deFiChainStatusUrl
   }
   const oceanIsDownContent: Announcement = {
-    content: 'We are currently investigating connection issues on Ocean API. ',
+    content: 'We are currently investigating connection issues on Ocean API.',
     url: deFiChainStatusUrl
   }
 
@@ -44,23 +44,21 @@ export function WarningBanner (props: PropsWithChildren<WarningBannerProps>): JS
   }
 
   return (
-    <div className='bg-orange-100 rounded p-3 text-center flex-row text-center' data-testid='warning_banner'>
-      <div>
-        {displayAnnouncement.content}
+    <div className='bg-orange-100 rounded p-3 flex justify-center' data-testid='warning_banner'>
+      {displayAnnouncement.content}
 
-        {displayAnnouncement.url !== undefined && (
-          <>
-            View more details on the
-            <a
-              href={`${displayAnnouncement.url}`}
-              className='text-primary-500 hover:text-primary-600 font-medium'
-              target='_blank' rel='noreferrer'
-            >
-              <span> DeFiChain Status </span> Page
-            </a>
-          </>
-        )}
-      </div>
+      {displayAnnouncement.url !== undefined && (
+        <div className='pl-1'>
+          View more details on the
+          <a
+            href={`${displayAnnouncement.url}`}
+            className='text-primary-500 hover:text-primary-600 font-medium'
+            target='_blank' rel='noreferrer'
+          >
+            <span> DeFiChain Status </span> Page
+          </a>
+        </div>
+      )}
     </div>
   )
 }
