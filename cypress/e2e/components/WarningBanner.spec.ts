15ecd6e7c2a839fb8523b43ec4d3d1ca84282d04
Chloezxyy <chloezhengxy@gmail.com>
2022-07-06 09:51:48 +0800

    update e2e to retrieve data using body

--- cypress/e2e/components/WarningBanner.spec.ts
+++ cypress/e2e/components/WarningBanner.spec.ts
@@ -1,4 +1,4 @@
-context('Warning banner on desktop', () => {
+context('Warning banner on desktop - Blockchain and Ocean warning messages', () => {
   const outage = {
     status: {
       description: 'outage'
@@ -9,6 +9,70 @@ context('Warning banner on desktop', () => {
       description: 'operational'
     }
   }
+  const sampleNetworkData = {
+    data: {
+      count: {
+        blocks: 8895,
+        prices: 15,
+        tokens: 32,
+        masternodes: 10
+      },
+      burned: {
+        address: 0,
+        fee: 15,
+        auction: 0,
+        payback: 0.50574055,
+        emission: 334807.59827419,
+        total: 334822.59827419
+      },
+      tvl: {
+        dex: 222050572.00136113,
+        masternodes: 840000,
+        loan: 28631944.99842,
+        total: 251522516.99978113
+      },
+      price: {
+        usd: 10000,
+        usdt: 10000
+      },
+      masternodes: {
+        locked: [{
+          weeks: 0,
+          count: 10,
+          tvl: 840000
+        }]
+      },
+      loan: {
+        count: {
+          collateralTokens: 12,
+          loanTokens: 5,
+          openAuctions: 0,
+          openVaults: 4,
+          schemes: 6
+        },
+        value: {
+          collateral: 28631944.99842,
+          loan: 118101.94192486
+        }
+      },
+      emission: {
+        masternode: 134.999832,
+        dex: 103.08268,
+        community: 19.887464,
+        anchor: 0.081008,
+        burned: 146.989016,
+        total: 405.04
+      },
+      net: {
+        version: 2090000,
+        subversion: "/DeFiChain:2.9.0/",
+        protocolversion: 70029
+      },
+      blockchain: {
+        difficulty: 4.656542373906925e-10
+      }
+    }
+  }
 
   beforeEach(() => {
     cy.visit('/?network=Playground')
@@ -16,25 +80,25 @@ context('Warning banner on desktop', () => {
   })
 
   it('should not display warning banner if nothing is down', function () {
-    // useApiStatus.getBlockStatus has condition to check for synced blocks
+    cy.intercept('**/blockchain', {
+      statusCode: 200,
+      body: operational
+    })
+    cy.intercept('**/overall', {
+      statusCode: 200,
+      body: operational
+    })
     cy.intercept('**/stats', {
       statusCode: 200,
-      data: {
-        lastSync: new Date().toString(),
-        lastSuccessfulSync: new Date().toString()
-      }
+      body: sampleNetworkData
     }).as('getStats')
     cy.wait('@getStats').then(() => {
-      cy.wait(5000)
-    cy.findByTestId('warning_banner').should('not.exist')
+      cy.wait(6000)
+      cy.findByTestId('warning_banner').should('not.exist')
     })
   })
 
   it('should display blockchain is down warning banner after preset interval', () => {
-    // to check timing for blockchain sync
-    let lastSuccessfulSync = new Date();
-    lastSuccessfulSync.setSeconds(lastSuccessfulSync.getSeconds() - 5) // 5 secs interval for playground
-
     cy.intercept('**/blockchain', {
       statusCode: 200,
       body: outage
@@ -45,23 +109,16 @@ context('Warning banner on desktop', () => {
     })
     cy.intercept('**/stats', {
       statusCode: 200,
-      data: {
-        lastSync: new Date().toString(),
-        lastSuccessfulSync: lastSuccessfulSync
-      }
+      body: undefined
     }).as('getStats')
     cy.wait('@getStats').then(() => {
-      cy.wait(3000)
+      cy.wait(6000)
       cy.findByTestId('warning_banner').should('exist')
       cy.findByTestId('warning_banner').should('contain', 'We are currently investigating a syncing issue on the blockchain.')
     })
   })
 
   it('should display ocean is down warning banner', () => {
-    // to check timing for blockchain sync
-    let lastSuccessfulSync = new Date();
-    lastSuccessfulSync.setSeconds(lastSuccessfulSync.getSeconds() - 5) // 5 secs interval for playground
-
     cy.intercept('**/blockchain', {
       statusCode: 200,
       body: operational
@@ -72,13 +129,10 @@ context('Warning banner on desktop', () => {
     })
     cy.intercept('**/stats', {
       statusCode: 200,
-      data: {
-        lastSync: new Date().toString(),
-        lastSuccessfulSync: lastSuccessfulSync
-      }
+      body: undefined
     }).as('getStats')
     cy.wait('@getStats').then(() => {
-      cy.wait(3000)
+      cy.wait(6000)
       cy.findByTestId('warning_banner').should('exist')
       cy.findByTestId('warning_banner').should('contain', 'We are currently investigating connection issues on Ocean API.')
     })
@@ -93,6 +147,7 @@ context('Warning banner on desktop', () => {
       statusCode: 200,
       body: outage
     })
+    // if stats' body is undefined or has error = only lastSync will be updated = blockchain is down
     cy.intercept('**/stats', {
       statusCode: 404,
       body: '404 Not Found!',
@@ -101,7 +156,7 @@ context('Warning banner on desktop', () => {
       }
     }).as('getStats')
     cy.wait('@getStats').then(() => {
-      cy.wait(3000)
+      cy.wait(6000)
       cy.findByTestId('warning_banner').should('exist')
       cy.findByTestId('warning_banner').should('contain', 'We are currently investigating a syncing issue on the blockchain.')
     })
