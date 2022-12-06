f66dc91ea040a8ab864820773a130037c9af4cba
pierregee <pierre.gabaldon@gmail.com>
2022-12-06 14:42:23 +0800

    feature(core): add consortium in header

--- src/layouts/components/Header.tsx
+++ src/layouts/components/Header.tsx
@@ -35,59 +35,71 @@ export function Header(): JSX.Element {
   }, []);
 
   return (
-    <header
-      className={classNames(
-        "bg-white z-50 sticky top-0 md:shadow-none md:static",
-        { "shadow-lg": !atTop }
-      )}
-    >
-      <div className="hidden md:block border-b border-gray-100 bg-primary-700 dark:bg-gray-800 dark:border-0">
-        <Container className="py-1">
-          <div className="flex items-center justify-between h-8">
-            <HeaderCountBar className="h-full flex" />
-            <HeaderNetworkMenu />
-          </div>
-        </Container>
-      </div>
-
-      <div className="border-b border-gray-100 dark:bg-gray-900 dark:border-gray-800">
-        <Container className="py-4 md:py-8">
-          <div className="flex items-center justify-between">
-            <div className="flex w-full">
-              <Link href={{ pathname: "/" }} passHref>
-                <a className="flex items-center cursor-pointer hover:text-primary-500">
-                  <DeFiChainLogo className="w-32 md:hidden lg:block lg:w-40 h-full" />
-                </a>
-              </Link>
-              <DesktopNavbar />
+    <>
+      <header
+        className={classNames(
+          "sticky top-0 z-50 bg-white md:static md:shadow-none",
+          { "shadow-lg": !atTop }
+        )}
+      >
+        <div className="hidden border-b border-gray-100 bg-primary-700 dark:border-0 dark:bg-gray-800 md:block">
+          <Container className="py-1">
+            <div className="flex h-8 items-center justify-between">
+              <HeaderCountBar className="flex h-full" />
+              <HeaderNetworkMenu />
             </div>
+          </Container>
+        </div>
+
+        <div className="border-b border-gray-100 dark:border-gray-800 dark:bg-gray-900">
+          <Container className="py-4 md:py-8">
+            <div className="flex items-center justify-between">
+              <div className="flex w-full">
+                <Link href={{ pathname: "/" }} passHref>
+                  <a className="flex cursor-pointer items-center hover:text-primary-500">
+                    <DeFiChainLogo className="h-full w-32 sm:hidden md:block lg:w-40" />
+                  </a>
+                </Link>
+                <DesktopNavbar />
+              </div>
+              <div className="lg:hidden">
+                {menu ? (
+                  <MdClose
+                    className="h-6 w-6 text-primary-500"
+                    onClick={() => setMenu(false)}
+                    data-testid="Header.CloseMenu"
+                  />
+                ) : (
+                  <MdMenu
+                    className="h-6 w-6 text-primary-500"
+                    onClick={() => setMenu(true)}
+                    data-testid="Header.OpenMenu"
+                  />
+                )}
+              </div>
+            </div>
+          </Container>
+        </div>
+      </header>
+      <>
+        {menu && (
+          <>
             <div className="md:hidden">
-              {menu ? (
-                <MdClose
-                  className="h-6 w-6 text-primary-500"
-                  onClick={() => setMenu(false)}
-                  data-testid="Header.CloseMenu"
-                />
-              ) : (
-                <MdMenu
-                  className="h-6 w-6 text-primary-500"
-                  onClick={() => setMenu(true)}
-                  data-testid="Header.OpenMenu"
-                />
-              )}
+              <MobileMenu />
             </div>
-          </div>
-        </Container>
-      </div>
-
-      <>{menu && <MobileMenu />}</>
-    </header>
+            <div className="hidden md:block md:fixed md:z-50">
+              <TabletMenu toggleMenu={() => setMenu(false)} />
+            </div>
+          </>
+        )}
+      </>
+    </>
   );
 }
 
 function DesktopNavbar(): JSX.Element {
   return (
-    <div className="hidden md:flex ml-2 lg:ml-8 md:w-full md:justify-between items-center text-gray-600 dark:text-dark-gray-900">
+    <div className="ml-2 hidden items-center text-gray-600 dark:text-dark-gray-900 md:w-full md:justify-between lg:ml-8 lg:flex">
       <div className="hidden md:flex">
         <HeaderLink
           className="ml-1 lg:ml-4"
@@ -131,6 +143,12 @@ function DesktopNavbar(): JSX.Element {
           pathname="/masternodes"
           testId="Desktop.HeaderLink.Masternodes"
         />
+        <HeaderLink
+          className="ml-1 lg:ml-4"
+          text="Consortium"
+          pathname="/consortium/asset_breakdown"
+          testId="Desktop.HeaderLink.Consortium"
+        />
       </div>
       <div
         className="hidden w-1/4 md:block"
@@ -142,63 +160,52 @@ function DesktopNavbar(): JSX.Element {
   );
 }
 
+function TabletMenu({ toggleMenu }: { toggleMenu: () => void }): JSX.Element {
+  return (
+    <div
+      className="flex flex-col float-right h-screen w-5/12 bg-white dark:bg-gray-900 lg:hidden"
+      data-testid="TabletMenu"
+    >
+      <div className="flex flex-row justify-between m-4">
+        <Link href={{ pathname: "/" }} passHref>
+          <a className="hover:text-primary-500">
+            <DeFiChainLogo className="h-full w-32" />
+          </a>
+        </Link>
+        <MdClose
+          className="h-6 w-6 text-primary-500"
+          onClick={() => toggleMenu()}
+          data-testid="Header.CloseMenu"
+        />
+      </div>
+      <div className="flex h-full flex-col justify-between">
+        <Container className="pt-2 pb-4 text-gray-600 dark:text-dark-gray-900">
+          <MenuItems />
+        </Container>
+        <div className="flex flex-wrap bg-primary-700 p-4 dark:bg-gray-900">
+          <HeaderCountBar className="flex w-full flex-wrap" />
+          <div className="mt-4 w-full">
+            <HeaderNetworkMenu />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
 function MobileMenu(): JSX.Element {
   return (
-    <div className="md:hidden dark:bg-gray-900" data-testid="MobileMenu">
-      <Container className="pt-2 pb-4 border-b border-gray-100 shadow-sm text-gray-600 dark:text-dark-gray-900">
-        <div className="flex flex-col">
-          <HeaderLink
-            className="flex justify-center border-b border-gray-100 dark:border-gray-700"
-            text="DEX"
-            pathname="/dex"
-            testId="Mobile.HeaderLink.DEX"
-          />
-          <HeaderLink
-            className="flex justify-center border-b border-gray-100 dark:border-gray-700"
-            text="Blocks"
-            pathname="/blocks"
-            testId="Mobile.HeaderLink.Blocks"
-          />
-          <HeaderLink
-            className="flex justify-center border-b border-gray-100 dark:border-gray-700"
-            text="Vaults"
-            pathname="/vaults"
-            testId="Mobile.HeaderLink.Vaults"
-          />
-          <HeaderLink
-            className="flex justify-center border-b border-gray-100 dark:border-gray-700"
-            text="Auctions"
-            pathname="/auctions"
-            testId="Mobile.HeaderLink.Auctions"
-          />
-          <HeaderLink
-            className="flex justify-center border-b border-gray-100 dark:border-gray-700"
-            text="Oracles"
-            pathname="/oracles"
-            testId="Mobile.HeaderLink.Oracles"
-          />
-          <HeaderLink
-            className="flex justify-center border-b border-gray-100 dark:border-gray-700"
-            text="Tokens"
-            pathname="/tokens"
-            testId="Mobile.HeaderLink.Tokens"
-          />
-          <HeaderLink
-            className="flex justify-center border-b border-gray-100 dark:border-gray-700"
-            text="Masternodes"
-            pathname="/masternodes"
-            testId="Mobile.HeaderLink.Masternodes"
-          />
-        </div>
-
+    <div className=" dark:bg-gray-900 md:hidden" data-testid="MobileMenu">
+      <Container className="border-b border-gray-100 pt-2 pb-4 text-gray-600 shadow-sm dark:text-dark-gray-900">
+        <MenuItems />
         <div className="mt-4" data-testid="Mobile.HeaderSearchBar">
           <SearchBar atHeader={false} />
         </div>
       </Container>
 
-      <div className="p-2 bg-primary-700 dark:bg-gray-900 flex flex-wrap p-4 md:p-0">
-        <HeaderCountBar className="w-full flex flex-wrap" />
-        <div className="w-full mt-4">
+      <div className="flex flex-wrap bg-primary-700 p-2 p-4 dark:bg-gray-900 md:p-0">
+        <HeaderCountBar className="flex w-full flex-wrap" />
+        <div className="mt-4 w-full">
           <HeaderNetworkMenu />
         </div>
       </div>
@@ -206,7 +213,7 @@ function MobileMenu(): JSX.Element {
   );
 }
 
-function HeaderLink(props: {
+export function HeaderLink(props: {
   text: string;
   pathname: string;
   className: string;
@@ -216,17 +223,24 @@ function HeaderLink(props: {
   return (
     <Link href={{ pathname: props.pathname }}>
       <a
-        className={classNames(props.className, {
-          "text-primary-500 dark:text-dark-50":
-            router.pathname === props.pathname,
-        })}
+        className={classNames(
+          props.className,
+          {
+            "dark:text-dark-50 text-primary-500":
+              router.pathname === props.pathname,
+          },
+          {
+            "text-gray-900 dark:text-dark-gray-900":
+              router.pathname !== props.pathname,
+          }
+        )}
         data-testid={props.testId}
       >
         <div
           className={classNames(
-            "inline m-2 text-lg pb-0.5 hover:text-primary-500 dark:hover:text-dark-50  cursor-pointer",
+            "dark:hover:text-dark-50 m-2 inline cursor-pointer pb-0.5 text-lg  hover:text-primary-500",
             {
-              "border-b-2 border-primary-500 dark:border-dark-50":
+              "dark:border-dark-50 border-b-2 border-primary-500":
                 router.pathname === props.pathname,
             }
           )}
@@ -235,5 +249,60 @@ function HeaderLink(props: {
         </div>
       </a>
     </Link>
+  );
+}
+
+function MenuItems(): JSX.Element {
+  return (
+    <div className="flex flex-col">
+      <HeaderLink
+        className="flex justify-center border-b border-gray-100 dark:border-gray-700 md:p-1.5"
+        text="DEX"
+        pathname="/dex"
+        testId="Mobile.HeaderLink.DEX"
+      />
+      <HeaderLink
+        className="flex justify-center border-b border-gray-100 dark:border-gray-700 md:p-1.5"
+        text="Blocks"
+        pathname="/blocks"
+        testId="Mobile.HeaderLink.Blocks"
+      />
+      <HeaderLink
+        className="flex justify-center border-b border-gray-100 dark:border-gray-700 md:p-1.5"
+        text="Vaults"
+        pathname="/vaults"
+        testId="Mobile.HeaderLink.Vaults"
+      />
+      <HeaderLink
+        className="flex justify-center border-b border-gray-100 dark:border-gray-700 md:p-1.5"
+        text="Auctions"
+        pathname="/auctions"
+        testId="Mobile.HeaderLink.Auctions"
+      />
+      <HeaderLink
+        className="flex justify-center border-b border-gray-100 dark:border-gray-700 md:p-1.5"
+        text="Oracles"
+        pathname="/oracles"
+        testId="Mobile.HeaderLink.Oracles"
+      />
+      <HeaderLink
+        className="flex justify-center border-b border-gray-100 dark:border-gray-700 md:p-1.5"
+        text="Tokens"
+        pathname="/tokens"
+        testId="Mobile.HeaderLink.Tokens"
+      />
+      <HeaderLink
+        className="flex justify-center border-b border-gray-100 dark:border-gray-700 md:p-1.5"
+        text="Masternodes"
+        pathname="/masternodes"
+        testId="Mobile.HeaderLink.Masternodes"
+      />
+      <HeaderLink
+        className="flex justify-center border-b border-gray-100 dark:border-gray-700 md:p-1.5"
+        text="Consortium"
+        pathname="/consortium/asset_breakdown"
+        testId="Mobile.HeaderLink.Consortium"
+      />
+    </div>
   );
 }
