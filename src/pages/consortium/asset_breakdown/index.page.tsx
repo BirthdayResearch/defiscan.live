67baf772f211e0b1115ab0d6efdb0a14b3226126
pierregee <pierre.gabaldon@gmail.com>
2022-12-07 09:52:59 +0800

    chore(consortium): add consortium e2e

--- src/pages/consortium/asset_breakdown/index.page.tsx
+++ src/pages/consortium/asset_breakdown/index.page.tsx
@@ -56,10 +56,6 @@ export default function AssetBreakdown({
     setFilteredAssets(searchedAssets);
   }, [searchText]);
 
-  // function handleDropdownFilter(token: string): void {
-  //   setSelectedToken(token);
-  // }
-
   return (
     <Container>
       <div className="mx-0 mt-14 flex flex-col items-center justify-center lg:mx-40 xl:flex-row">
@@ -76,49 +72,11 @@ export default function AssetBreakdown({
       <ConsortiumLayout className="pb-20">
         <Head title="Consortium" />
         <div className="my-6">
-          {/* <TokenMenu
-          selectedToken={selectedToken}
-          handleDropdownFilter={handleDropdownFilter}
-        /> */}
-
-          {/* <div className="relative flex flex-col border-b border-gray-200 md:flex-row"> */}
-          {/* <AssetBreakdownPieChart /> */}
-          {/* <AssetBreakdownPercentageChart
-            selectedToken="All Tokens"
-            totalBreakdown={{
-              mintedColor: "bg-green-500",
-              backedColor: "bg-green-100",
-              percentage: 60,
-              header: "Total",
-              backedTokens: "12312.412342142",
-              mintedTokens: "131234.12313122",
-            }}
-            assetBreakdown={[
-              {
-                mintedColor: "bg-primary-600",
-                backedColor: "bg-primary-100",
-                percentage: 30.12,
-                header: "Cake",
-                backedTokens: "12312.412342142",
-                mintedTokens: "131234.12313122",
-              },
-              {
-                mintedColor: "bg-hyperlink-focused",
-                backedColor: "bg-blue-300",
-                percentage: 69.88,
-                header: "Birthday Research",
-                backedTokens: "12312.412342142",
-                mintedTokens: "131234.12313122",
-              },
-            ]}
-          /> */}
-          {/* </div> */}
           <div className="flex flex-row rounded-md border md:w-2/4 xl:w-2/12 border-gray-200 dark:bg-gray-800 dark:border-gray-700">
             <IoSearchSharp
               size={22}
               className="ml-4 dark:text-gray-100 text-gray-600 self-center"
             />
-
             <input
               className="inline-block h-10 px-2 outline-0 dark:text-white dark:bg-gray-800"
               placeholder="Search"
@@ -133,7 +91,6 @@ export default function AssetBreakdown({
             <AssetBreakdownTable assets={filteredAssets} />
           </div>
         </div>
-
         <div className="my-6 md:hidden">
           <AssetBreakdownCards assets={filteredAssets} />
         </div>
@@ -219,67 +176,6 @@ export async function getServerSideProps(
     },
   };
 }
-
-// export function TokenMenu({
-//   selectedToken,
-//   handleDropdownFilter,
-// }: {
-//   selectedToken: string;
-//   handleDropdownFilter: (token: string) => void;
-// }): JSX.Element {
-//   const tokens = ["All Tokens", "dDOGE", "dETH", "dBTC"];
-//   return (
-//     <div className="mb-6 flex items-center">
-//       <div className="mr-2 text-lg dark:text-dark-gray-900">Show: </div>
-//       <Menu as="div" className="relative flex w-full">
-//         <Menu.Button className="flex items-center rounded border border-gray-300 bg-white py-3 px-4 dark:bg-dark-gray-200 dark:text-dark-gray-900">
-//           <div className="ml-2 font-medium leading-none text-gray-900 dark:text-dark-gray-100">
-//             {selectedToken}
-//           </div>
-//           <MdArrowDropDown className="ml-1 h-6 w-6 text-gray-400 dark:text-dark-gray-100" />
-//         </Menu.Button>
-
-//         <Transition
-//           as={Fragment}
-//           enter="transition ease-out duration-100"
-//           enterFrom="transform opacity-0 scale-95"
-//           enterTo="transform opacity-100 scale-100"
-//           leave="transition ease-in duration-75"
-//           leaveFrom="transform opacity-100 scale-100"
-//           leaveTo="transform opacity-0 scale-95"
-//         >
-//           <Menu.Items className="absolute left-0 top-14 z-10 w-36 rounded border border-gray-200 bg-white shadow-lg dark:bg-dark-gray-200">
-//             <div className="py-1">
-//               {tokens.map((item) => (
-//                 <Menu.Item key={item}>
-//                   {({ active }) => (
-//                     <div
-//                       className="flex cursor-pointer items-end justify-between px-4 py-2 text-sm dark:text-dark-gray-900"
-//                       onClick={() => handleDropdownFilter(item)}
-//                     >
-//                       <div
-//                         className={
-//                           active || selectedToken === item
-//                             ? "font-medium text-primary-500"
-//                             : ""
-//                         }
-//                       >
-//                         {item}
-//                       </div>
-//                       {selectedToken === item && (
-//                         <BiCheck className="ml-1 text-xl text-primary-500" />
-//                       )}
-//                     </div>
-//                   )}
-//                 </Menu.Item>
-//               ))}
-//             </div>
-//           </Menu.Items>
-//         </Transition>
-//       </Menu>
-//     </div>
-//   );
-// }
 
 function isSubstringOfText(keyword: string, text: string): boolean {
   return text.toLowerCase().trim().includes(keyword.toLowerCase().trim());
