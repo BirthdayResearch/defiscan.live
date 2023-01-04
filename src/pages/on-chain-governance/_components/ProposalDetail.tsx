3e5122878f326606f7fa091ed8c32a4829757916
veralygit <57183851+veralygit@users.noreply.github.com>
2023-01-04 18:07:13 +0800

    update line height for proposal type

--- src/pages/on-chain-governance/_components/ProposalDetail.tsx
+++ src/pages/on-chain-governance/_components/ProposalDetail.tsx
@@ -36,7 +36,7 @@ export function ProposalDetail({
     <div className="md:border md:p-6 border-gray-200 rounded-lg">
       <div className="flex mb-2">
         <div className="p-2 bg-gray-100 dark:bg-gray-800 rounded">
-          <div className="text-sm font-medium text-gray-900 dark:text-gray-100 tracking-[0.015em]">
+          <div className="text-sm font-medium text-gray-900 dark:text-gray-100 tracking-[0.015em] leading-4">
             {ProposalDisplayName[proposal.type]}
           </div>
         </div>
