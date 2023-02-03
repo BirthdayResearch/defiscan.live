75a9233f706cddb45108e89d225e9e09d35c2a70
veralygit <57183851+veralygit@users.noreply.github.com>
2023-02-03 18:56:33 +0800

    add comment for future changes

--- src/pages/governance/shared/getTotalVotes.ts
+++ src/pages/governance/shared/getTotalVotes.ts
@@ -5,6 +5,7 @@ export function getVotePercentage(
   numNo: number,
   numNeutral: number
 ) {
+  // to remove neutral votes from total votes tabulation count after bc released fix for neutral vote tabulation
   const totalVotes = numYes + numNo + numNeutral;
   const percYes = BigNumber(numYes).div(totalVotes).multipliedBy(100);
   const percNo = BigNumber(numNo).div(totalVotes).multipliedBy(100);
