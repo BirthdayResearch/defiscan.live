fdc6be350e455779fcb8b394fb463793bbb40d47
Keng Ye <kengyeleow@gmail.com>
2023-02-22 13:20:06 +0800

    refactor: unused function

--- src/pages/governance/_components/VotingResult.tsx
+++ src/pages/governance/_components/VotingResult.tsx
@@ -364,17 +364,11 @@ function Progress({
       >
         <div
           style={{ width: `${yesValue}%` }}
-          className={classNames(
-            "absolute top-0 left-0 h-3",
-            "bg-green-600 dark:bg-[#21E529]"
-          )}
+          className="absolute top-0 left-0 h-3 bg-green-600 dark:bg-[#21E529]"
         />
         <div
           style={{ width: `${noValue}%` }}
-          className={classNames(
-            "absolute top-0 right-0 h-3",
-            "bg-red-600 dark:bg-[#FF483D]"
-          )}
+          className="absolute top-0 right-0 h-3 bg-red-600 dark:bg-[#FF483D]"
         />
       </div>
     </div>
