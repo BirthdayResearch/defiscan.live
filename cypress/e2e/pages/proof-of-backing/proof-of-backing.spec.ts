cbfe10de8f1a77cf0a2c859907ba4f33c4a6a1f4
Mikhail <mykhailo@cakedefi.com>
2023-04-04 14:03:34 +0800

    added backing addresses test data; net supply format validation;

---
+++ cypress/e2e/pages/proof-of-backing/proof-of-backing.spec.ts
@@ -0,0 +1,95 @@
+import {
+  cakeBackingAddresses,
+  quantumBackingAddresses,
+} from "../../../fixtures/proof-of-backing/backingAddresses";
+
+const pobUrlMainnet = "/proof-of-backing?network=MainNet";
+
+context("/proof-of-backing on macbook-16", () => {
+  before(() => {
+    cy.visit(pobUrlMainnet);
+  });
+
+  beforeEach(() => {
+    cy.viewport("macbook-16");
+  });
+
+  it("should have title and description", () => {
+    cy.findByTestId("title-header")
+      .should("be.visible")
+      .and("have.text", "Proof of Backing");
+    cy.findByTestId("title-description")
+      .should("be.visible")
+      .and(
+        "have.text",
+        "All tokens have backed collateral from which they are minted. See proof of the backed amount on the addresses below."
+      );
+  });
+
+  it("should have Tokens displayed", () => {
+    cakeBackingAddresses.forEach((address) => {
+      cy.findByTestId(`symbol-${address.token}`).should("be.visible");
+    });
+  });
+
+  it("should verify Net supply values", () => {
+    cakeBackingAddresses.forEach((address) => {
+      cy.getNetSupplyValueBySymbol(address.token).should(
+        "match",
+        /[-+]?[0-9]*(\.[0-9]+)/
+      );
+    });
+  });
+
+  it("should verify Cake: Backing address values", () => {
+    cakeBackingAddresses.forEach((backingAddress) => {
+      cy.findByTestId(`backedAddress-cake-${backingAddress.token}`)
+        .should("be.visible")
+        .and("have.attr", "href", backingAddress.link);
+    });
+  });
+
+  it("should verify Quantum: Backing address values", () => {
+    quantumBackingAddresses.forEach((backingAddress) => {
+      cy.findByTestId(`backedAddress-quantum-filled-${backingAddress.token}`)
+        .should("be.visible")
+        .and("have.attr", "href", backingAddress.link);
+    });
+  });
+});
+
+context("/proof-of-backing on iphone-x", () => {
+  before(() => {
+    cy.visit(pobUrlMainnet);
+  });
+
+  beforeEach(() => {
+    cy.viewport("iphone-x");
+  });
+
+  it("should have Tokens links be displayed", () => {
+    cakeBackingAddresses.forEach((address) => {
+      cy.findByTestId(`mobile-viewLink-${address.token}`).should(
+        "have.attr",
+        "href",
+        `/tokens/${address.token}`
+      );
+      cy.findByTestId(`mobile-viewLink-${address.token}`)
+        .parent()
+        .should("be.visible");
+    });
+  });
+
+  it("should expand each the Symbol card in mobile view", () => {
+    cakeBackingAddresses.forEach((address) => {
+      cy.findByTestId(`mobile-selectBlock-${address.token}`)
+        .find("svg")
+        .click();
+    });
+    cy.findByTestId(
+      `mobile-netSupply-${
+        cakeBackingAddresses[cakeBackingAddresses.length - 1].token
+      }`
+    ).should("be.visible");
+  });
+});
---
+++ cypress/fixtures/proof-of-backing/backingAddresses.ts
@@ -0,0 +1,57 @@
+export const cakeBackingAddresses = [
+  {
+    token: "dBTC",
+    link: "https://www.blockchain.com/btc/address/38pZuWUti3vSQuvuFYs8Lwbyje8cmaGhrT",
+  },
+  {
+    token: "dETH",
+    link: "https://etherscan.io/address/0x94fa70d079d76279e1815ce403e9b985bccc82ac",
+  },
+  {
+    token: "dUSDT",
+    link: "https://etherscan.io/address/0x94fa70d079d76279e1815ce403e9b985bccc82ac",
+  },
+  {
+    token: "dUSDC",
+    link: "https://etherscan.io/address/0x94fa70d079d76279e1815ce403e9b985bccc82ac",
+  },
+  {
+    token: "dEUROC",
+    link: "https://etherscan.io/address/0x94fa70d079d76279e1815ce403e9b985bccc82ac",
+  },
+  {
+    token: "dLTC",
+    link: "https://live.blockcypher.com/ltc/address/MLYQxJfnUfVqRwfYXjDJfmLbyA77hqzSXE",
+  },
+  {
+    token: "dBCH",
+    link: "https://www.blockchain.com/bch/address/38wFczGqaaGLRub2U7CWeWkMuPDwhMVMRf",
+  },
+  {
+    token: "dDOGE",
+    link: "https://dogechain.info/address/D7jrXDgPYck8jL9eYvRrc7Ze8n2e2Loyba",
+  },
+];
+
+export const quantumBackingAddresses = [
+  {
+    token: "dBTC",
+    link: "https://etherscan.io/address/0x11901fd641f3a2d3a986d6745a2ff1d5fea988eb",
+  },
+  {
+    token: "dETH",
+    link: "https://etherscan.io/address/0x11901fd641f3a2d3a986d6745a2ff1d5fea988eb",
+  },
+  {
+    token: "dUSDT",
+    link: "https://etherscan.io/address/0x11901fd641f3a2d3a986d6745a2ff1d5fea988eb",
+  },
+  {
+    token: "dUSDC",
+    link: "https://etherscan.io/address/0x11901fd641f3a2d3a986d6745a2ff1d5fea988eb",
+  },
+  {
+    token: "dEUROC",
+    link: "https://etherscan.io/address/0x11901fd641f3a2d3a986d6745a2ff1d5fea988eb",
+  },
+];
