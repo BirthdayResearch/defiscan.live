4ce0ee79193d61e3df475d0214b2bdb36e6e5066
JJ Adonis <work@josejai.me>
2023-06-22 12:36:44 +0800

    chore(ops): update deps and node 18 (#1814)
    * chore(ops): update deps and node 18
    
    * chore(ops): update deps and node 18
    
    * chore(ops): update deps and node 18

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  preset: "@birthdayresearch/sticky-jest",
+  preset: "@stickyjs/jest",
 };
--- next.config.js
+++ next.config.js
@@ -19,8 +19,7 @@ const securityHeaders = [
         process.env.NODE_ENV === "development"
           ? `ws://localhost:3000/_next/webpack-hmr`
           : ""
-      };` +
-      `prefetch-src 'self';`,
+      };`
   },
   {
     key: "Referrer-Policy",
