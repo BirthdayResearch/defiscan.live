af2b2b4c6ee53c1b7694fc57704279178004e58f
elocin <elocinnat99@gmail.com>
2024-10-10 20:47:43 +0800

    fix(ui-ux): fix NaN bug in homepage (#1939)
    * return 0% if divided by 0
    
    * remove added ,
    
    * remove console
    
    * lint
    
    * refine CalculatePercentage
    
    ---------
    
    Co-authored-by: canonbrother <w.canonbrother@gmail.com>

--- src/utils/index/CalculatePercentage.ts
+++ src/utils/index/CalculatePercentage.ts
@@ -1,10 +1,15 @@
 export function CalculatePercentage(
   value1: number | undefined,
-  value2: number | undefined
+  value2: number | undefined,
 ): string {
   if (value1 === undefined || value2 === undefined) {
     return "";
   }
 
-  return `${((value1 / value2) * 100).toFixed(2)}%`;
+  const result = value1 / value2;
+  if (isNaN(result)) {
+    return "0.00%";
+  }
+
+  return `${(result * 100).toFixed(2)}%`;
 }
